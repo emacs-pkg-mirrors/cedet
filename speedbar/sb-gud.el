b66ecc382b6f6ea66ae132aa366d6760ff6995b2
zappo <zappo>
1998-03-06 16:31:26 +0000

    Removed dependency on speedbspec Added comment about the autoload

--- speedbar/sb-gud.el
+++ speedbar/sb-gud.el
@@ -5,7 +5,7 @@
 ;; Author: Eric M. Ludlam <zappo@gnu.ai.mit.edu>
 ;; Version: 0.1
 ;; Keywords: tools, gud
-;; X-RCS: $Id: sb-gud.el,v 1.3 1998/01/07 02:04:29 zappo Exp $
+;; X-RCS: $Id: sb-gud.el,v 1.4 1998/03/06 16:31:26 zappo Exp $
 ;;
 ;; This file is part of GNU Emacs.
 ;;
@@ -36,20 +36,15 @@
 ;; files are displayed.  These functions provide gud specific support,
 ;; showing stacks, files, and the like in the side-bar.
 ;;
-;;   To provide special service to all the modes supported by this file,
-;; put the following in your .emacs file.
-;;
-;; (require 'speedbspec)
-;;
-;;   This will load in the known functions, and the mode-enabling code
-;; into 'change-major-mode-hook.
+;;   To enable in emacs 20.2 or earlier, add this to your .emacs file.
+;;   (autoload 'gud-speedbar-buttons "sb-gud"
+;;             "GUD specific speedbar button generator.")
 ;;
 ;;   This file requires speedbar.
 
 ;;; Change log:
-;; 0.1 - Revision
-
-(require 'speedbspec)
+;; 0.1   - First revision
+;; 0.1.1 - Removed dependency on speedbspec.
 
 ;;; Code:
 (defvar gud-last-speedbar-buffer nil
