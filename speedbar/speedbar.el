8be5c7e877f898f82ed82646b615ceb6cdd45a43
zappo <zappo>
1998-06-14 12:59:25 +0000

    Version number

--- speedbar/speedbar.el
+++ speedbar/speedbar.el
@@ -3,9 +3,9 @@
 ;;; Copyright (C) 1996, 97, 98 Free Software Foundation
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
-;; Version: 0.7g
+;; Version: 0.7
 ;; Keywords: file, tags, tools
-;; X-RCS: $Id: speedbar.el,v 1.110 1998/06/12 11:34:12 zappo Exp $
+;; X-RCS: $Id: speedbar.el,v 1.111 1998/06/14 12:59:25 zappo Exp $
 
 ;; This file is part of GNU Emacs.
 
