d8e68abef59802797d6bd19885b251bdf167fc7a
zappo <zappo>
1998-10-22 20:37:40 +0000

    Added lots of project minor mode commands (empty) Object data base builder is now based on a rescan method which will incidentally create new stuff along the way. Added named target objects (yea!) Added a name program (to get the name from an obj, maybe delete later) Fixed up speedbar stuff to use right variables (left overs) Makefiles w/ one target in speedbar skip the intermediate target view. Fixed some depth problems.

--- ede/project-am.el
+++ ede/project-am.el
@@ -51,6 +51,10 @@
 (defvar project-am-constructing nil
   "Non nil when constructing a project hierarchy.")
 
+(defvar project-am-deep-rescan nil
+  "Non nil means scan down a tree, otherwise rescans are top level only.
+Do not set this to non-nil globally.  It is used internally.")
+
 (defclass project-am-target ()
   ((name :initarg :name :docstring "Name of this target.")
    (path :initarg :path :docstring "The path to this target.")
@@ -77,6 +81,10 @@
 	    :initform nil
 	    :docstring "Additional texinfo included in this one."))
   "A top level texinfo file to build.")
+
+(defclass project-am-man (project-am-target)
+  nil
+  "A top level man file to build.")
 
 (defclass project-am-makefile ()
   ((file :initarg :file
@@ -127,8 +135,15 @@
     (easy-menu-define
      project-am-minor-menu project-am-minor-keymap "Project Minor Mode Menu"
      '("Project"
-       [ "Load a project" project-am t ]
+       [ "Load a project" project-am-load-project-file t ]
        [ "Speedbar Project" project-speedbar t ]
+       "---"
+       [ "Create New Target" project-am-new-target t ]
+       [ "Add to Target" project-am-add-file (not project-am-object) ]
+       [ "Remove from Targets" project-am-remove-file project-am-object ]
+       [ "Modify Target" project-am-edit-file-target project-am-object ]
+       "---"
+       [ "Rescan Project Files" project-am-rescan-toplevel t ]
        )))
 
 ;; Allow re-insertion of a new keymap
@@ -154,6 +169,59 @@ mode.  nil means to toggle the mode."
 		 (<= (prefix-numeric-value arg) 0))))
   (if (not project-am-constructing)
       (project-am-load default-directory)))
+
+;;; Minor Mode commands
+;;
+(defun project-am-load-project-file (file)
+  "Find and load a project FILE.
+A project file is Makefile.am"
+  (interactive "FAutomake File: ")
+  (if (string-match "Makefile\\.am")
+      (find-file file)
+    (error "Not an automake file")))
+
+(defun project-am-add-file ()
+  "Add the current buffer into a project."
+  (interactive)
+  )
+
+(defun project-am-remove-file ()
+  "Remove the current buffer from any project targets."
+  (interactive)
+  )
+
+(defun project-am-edit-file-target ()
+  "Edit the target associated w/ this file."
+  (interactive)
+  )
+
+(defun project-am-new-target (name type)
+  "Create a new target named NAME."
+  (interactive (list
+		(read-string "Name: "
+			     (if project-am-object
+				 (project-am-name project-am-object)
+			       ""))
+		(completing-read "Type: "
+				 '(("bin") ("sbin") ("lib")
+				   ("texinfo") ("man"))
+				 nil t
+				 (cond ((eq major-mode 'texinfo-mode)
+					"texinfo")
+				       ((eq major-mode 'nroff-mode)
+					"man")
+				       (t "bin")))))
+  )
+
+(defun project-am-rescan-toplevel ()
+  "Rescan all projects in which the current buffer resides."
+  (interactive)
+  (let* ((tlof (project-am-find-topmost-level default-directory))
+	 (tlo (project-am-load tlof))
+	 (project-am-deep-rescan t))  ; scan deep in this case.
+    ;; tlo is the top level object for whatever file we are in
+    ;; or nil.  If we have an object, call the rescan method.
+    (if tlo (project-am-rescan tlo))))
 
 ;;; Project loading and saving
 ;;
@@ -213,57 +281,104 @@ It does not check for existing project objects.  Use `project-am-load'."
 	    (if (and project-am-object (project-am-makefile-p project-am-object))
 		project-am-object
 	      (let ((ampf (make-instance project-am-makefile
-					 :file fn
-					 :targets
-					 (append
-					  (mapcar 'project-am-scan-program
-						  (makefile-macro-file-list
-						   "bin_PROGRAMS"))
-					  (mapcar 'project-am-scan-program
-						  (makefile-macro-file-list
-						   "sbin_PROGRAMS"))
-					  (mapcar 'project-am-scan-lib
-						  (makefile-macro-file-list
-						   "noinst_LIBRARIES"))
-					  (mapcar 'project-am-scan-texinfo
-						  (makefile-macro-file-list
-						   "info_TEXINFOS"))
-					  ))))
-		(project-am-load-subtree ampf)
+					 :file fn)))
+		(project-am-rescan ampf)
 		(make-local-variable 'project-am-object)
 		(setq project-am-object ampf)
 		ampf))
 	  ;; If the buffer was not already loaded, kill it.
 	  (if (not kb) (kill-buffer (current-buffer))))))))
 
-(defun project-am-scan-program (program)
-  "Create a object of CLASS based on NAME.
-Argument PROGRAM is the name of the program to create."
-  (make-instance project-am-program
-		 :name program
-		 :path (expand-file-name default-directory)
-		 :source (makefile-macro-file-list
-			  (concat program "_SOURCES"))
-		 :ldadd (makefile-macro-file-list
-			  (concat program "_LDADD"))))
+;;; Methods:
+(defmethod project-am-rescan ((this project-am-makefile))
+  "Rescan the makefile for all targets and sub targets."
+  (let ((osubproj (oref this :subproj))
+	(otargets (oref this :targets))
+	(csubproj (or
+		   ;; If DIST_SUBDIRS doesn't exist, then go for the
+		   ;; static list of SUBDIRS.  The DIST version should
+		   ;; contain SUBDIRS plus extra stuff.
+		   (makefile-macro-file-list "DIST_SUBDIRS")
+		   (makefile-macro-file-list "SUBDIRS")))
+	(nsubproj nil)
+	;; Targets are excluded here because they require
+	;; special attention.
+	(ntargets nil)
+	(tmp nil)
+	;; Here are target prefixes as strings
+	(tp '(("bin_PROGRAMS" . project-am-program) 
+	      ("sbin_PROGRAMS" . project-am-program)
+	      ("noinst_LIBRARIES" . project-am-lib)
+	      ("info_TEXINFOS" . project-am-texinfo)
+	      ("man_MANS" . project-am-man)))
+	(path (expand-file-name default-directory))
+	)
+    (mapcar
+     ;; Map all tye different types
+     (lambda (typecar)
+       ;; Map all the found objects
+       (mapcar (lambda (lstcar)
+		 (setq tmp (object-assoc lstcar :name otargets))
+		 (if (not tmp)
+		     (setq tmp (apply (cdr typecar) lstcar
+				      :name lstcar
+				      :path path nil)))
+		 (project-am-rescan tmp)
+		 (setq ntargets (cons tmp ntargets)))
+	       (makefile-macro-file-list (car typecar))))
+     tp)
+    ;; Now that we have this new list, chuck the old targets
+    ;; and replace it with the new list of targets I just created.
+    (oset this :targets ntargets)
+    ;; We still have a list of targets.  For all buffers, make sure
+    ;; their object still exists!
 
-(defun project-am-scan-lib (program)
-  "Create a object of CLASS based on NAME.
-Argument PROGRAM is the name of the library to create."
-  (make-instance project-am-lib
-		 :name program
-		 :path (expand-file-name default-directory)
-		 :source (makefile-macro-file-list
-			  (concat program "_SOURCES"))))
+    ;; FIGURE THIS OUT
 
-(defun project-am-scan-texinfo (program)
-  "Create a object of CLASS based on NAME.
-Argument PROGRAM is the name of the texinfo file."
-  (make-instance project-am-texinfo
-		 :name program
-		 :path (expand-file-name default-directory)
-		 :include (makefile-macro-file-list
-			   (concat program "_TEXINFOS"))))
+    ;; Ok, now lets look at all our sub-projects.
+    (mapcar (lambda (sp)
+	      ;; For each project id found, see if we need to recycle,
+	      ;; and if we do not, then make a new one.  Check the deep
+	      ;; rescan value for behavior patterns.
+	      (setq tmp (object-assoc
+			 (concat default-directory sp "/Makefile.am")
+			 :file osubproj))
+	      (if (not tmp)
+		  ;; No tmp?  Create a new one.  Don't bother with
+		  ;; non-deep business since we need this object.
+		  (setq tmp (project-am-load-makefile
+			     (concat (file-name-directory (oref this :file))
+				     sp "/")))
+		;; If we have tmp, then rescan it only if deep mode.
+		(if project-am-deep-rescan
+		    (project-am-rescan tmp)))
+	      ;; Tac tmp onto our list of things to keep
+	      (setq nsubproj (cons tmp nsubproj)))
+	    csubproj)
+    (oset this :subproj nsubproj)
+    ;; All elements should be updated now.
+    ))
+
+(defmethod project-am-rescan ((this project-am-program))
+  "Rescan object THIS."
+  (oset this :source (makefile-macro-file-list
+		      (concat (oref this :name) "_SOURCES")))
+  (oset this :ldadd (makefile-macro-file-list
+		     (concat (oref this :name) "_LDADD"))))
+
+(defmethod project-am-rescan ((this project-am-lib))
+  "Rescan object THIS."
+  (oset this :source (makefile-macro-file-list
+		      (concat (oref this :name) "_SOURCES"))))
+
+(defmethod project-am-rescan ((this project-am-texinfo))
+  "Rescan object THIS."
+  (oset this :include (makefile-macro-file-list
+		       (concat (oref this :name) "_TEXINFOS"))))
+
+(defmethod project-am-rescan ((this project-am-man))
+  "Rescan object THIS."
+  )
 
 (defun project-am-buffer-project (buffer)
   "Return the project associated with BUFFER."
@@ -271,7 +386,7 @@ Argument PROGRAM is the name of the texinfo file."
 			     (set-buffer buffer)
 			     default-directory)))
 
-;;; Methods:
+
 (defmethod project-am-set-buffer ((ampf project-am-makefile))
   "Set the current buffer to this project file."
   (set-buffer (find-file-noselect (oref ampf :file))))
@@ -298,6 +413,17 @@ Argument PROGRAM is the name of the texinfo file."
 (defmethod project-am-subtree ((ampf project-am-makefile) subpath)
   "Return the sub project in AMPF specified by SUBPATH."
   (object-assoc (expand-file-name subpath) :file (oref ampf subproj)))
+
+(defmethod project-am-name ((ampf project-am-makefile))
+  "Return a short-name for this project file.
+Do this by extracting the lowest directory name."
+  (let ((s (file-name-directory (oref this :file))))
+    (string-match "/\\([a-zA-Z0-9_]+\\)/$" s)
+    (match-string 1 s)))
+
+(defmethod project-am-name ((this project-am-target))
+  "Return the name of this targt."
+  (oref this :name))
 
 ;;; Speedbar support mode
 ;;
@@ -356,10 +482,7 @@ expansion depth."
   (speedbar-make-tag-line 'angle ?+
 			  'project-am-object-expand
 			  this
-			  (let ((s (file-name-directory
-				    (oref this :file))))
-			    (string-match "/\\([a-zA-Z0-9_]+\\)/$" s)
-			    (match-string 1 s))
+			  (project-am-name this)
 			  'project-am-file-find
 			  (oref this :file)
 			  'speedbar-directory-face depth))
@@ -368,7 +491,7 @@ expansion depth."
   "Create a speedbar button for object THIS at DEPTH."
   (speedbar-make-tag-line 'angle ?+
 			  'project-am-object-expand
-			  this (oref this :name)
+			  this (project-am-name this)
 			  nil nil  ; nothing to jump to
 			  'speedbar-file-face depth))
 
@@ -376,7 +499,7 @@ expansion depth."
   "Create a speedbar button for object THIS at DEPTH."
   (speedbar-make-tag-line 'angle ?+
 			  'project-am-object-expand
-			  this (oref this :name)
+			  this (project-am-name this)
 			  nil nil  ; nothing to jump to
 			  'speedbar-file-face depth))
 
@@ -385,7 +508,16 @@ expansion depth."
   (speedbar-make-tag-line 'bracket ?+
 			  'project-am-object-expand
 			  this
-			  (oref this :name)
+			  (project-am-name this)
+			  'project-am-file-find
+			  (concat (oref this :path)
+				  (oref this :name))
+			  'speedbar-file-face depth))
+
+(defmethod project-am-sb-button ((this project-am-man) depth)
+  "Create a speedbar button for object THIS at DEPTH."
+  (speedbar-make-tag-line 'bracket ?? nil nil
+			  (project-am-name this)
 			  'project-am-file-find
 			  (concat (oref this :path)
 				  (oref this :name))
@@ -416,27 +548,34 @@ method for the actual text."
 	 (targets nil))
     (setq subproj (oref this :subproj))
     (setq targets (oref this :targets))
-    (while subproj
-      (project-am-sb-button (car subproj) depth)
-      (setq subproj (cdr subproj)))
-    (while targets
-      (project-am-sb-button (car targets) depth)
-      (setq targets (cdr targets)))))
+    (if (and (= 1 (+ (length subproj) (length targets)))
+	     (string= (project-am-name this)
+		      (project-am-name (or (car subproj)
+					   (car targets)))))
+	;; If there is only one target, and it has the same name
+	;; as the directory, then expand that target instead.
+	(project-am-sb-expand (or (car subproj) (car targets)) depth)
+      (while subproj
+	(project-am-sb-button (car subproj) depth)
+	(setq subproj (cdr subproj)))
+      (while targets
+	(project-am-sb-button (car targets) depth)
+	(setq targets (cdr targets))))))
 
 (defmethod project-am-sb-expand ((this project-am-objectcode) depth)
   "Expand node describing something built into objectcode.
 TEXT is the text clicked on.  TOKEN is the object we are expanding from.
 INDENT is the current indentatin level."
-  (let ((sources (oref token :source)))
+  (let ((sources (oref this :source)))
     (while sources
       (speedbar-make-tag-line 'bracket ?+
 			      'project-am-tag-file
-			      (concat (oref token :path)
+			      (concat (oref this :path)
 				      (car sources))
 			      (car sources)
 			      'project-am-file-find
 			      (concat
-			       (oref token :path)
+			       (oref this :path)
 			       (car sources))
 			      'speedbar-file-face depth)
       (setq sources (cdr sources)))))
@@ -445,23 +584,23 @@ INDENT is the current indentatin level."
   "Expand node describing a texinfo manual.
 TEXT is the text clicked on.  TOKEN is the object we are expanding from.
 INDENT is the current indentatin level."
-  (let ((includes (oref token :include)))
+  (let ((includes (oref this :include)))
     (while includes
       (speedbar-make-tag-line 'bracket ?+
 			      'project-am-tag-file
-			      (concat (oref token :path)
+			      (concat (oref this :path)
 				      (car includes))
 			      (car includes)
 			      'project-am-file-find
 			      (concat
-			       (oref token :path)
+			       (oref this :path)
 			       (car includes))
 			      'speedbar-file-face depth)
       (setq includes (cdr includes)))
     ;; Not only do we show the included files (for future expansion)
     ;; but we also want to display tags for this file too.
-    (project-am-create-tag-buttons (concat (oref token :path) 
-					   (oref token :name))
+    (project-am-create-tag-buttons (concat (oref this :path) 
+					   (oref this :name))
 				   depth)))
 
 (defun project-am-file-find (text token indent)
@@ -493,10 +632,11 @@ Etags does not support this feature.  TEXT will be the button
 string.  TOKEN will be the list, and INDENT is the current indentation
 level."
   (cond ((string-match "+" text)	;we have to expand this file
+	 (speedbar-change-expand-button-char ?-)
 	 (speedbar-with-writable
 	   (save-excursion
 	     (end-of-line) (forward-char 1)
-	     (project-am-create-tag-buttons token indent))))
+	     (project-am-create-tag-buttons token (1+ indent)))))
 	((string-match "-" text)	;we have to contract this node
 	 (speedbar-change-expand-button-char ?+)
 	 (speedbar-delete-subblock indent))
