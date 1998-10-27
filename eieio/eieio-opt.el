49cc4892bd3d490e97f20da72050b0fa888a5eb2
zappo <zappo>
1998-10-27 18:51:50 +0000

    Ran through checkdoc.

--- eieio/eieio-opt.el
+++ eieio/eieio-opt.el
@@ -1,49 +1,49 @@
 ;;; eieio-opt.el -- eieio optional functions (debug, printing, etc)
 
-;;;
-;;; Copyright (C) 1996 Eric M. Ludlam
-;;;
-;;; Author: <zappo@gnu.ai.mit.edu>
-;;; RCS: $Id: eieio-opt.el,v 1.2 1996/12/12 03:37:24 zappo Exp $
-;;; Keywords: OO, lisp
-;;;                                                                          
-;;; This program is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2, or (at your option)
-;;; any later version.
-;;;           
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with this program; if not, you can either send email to this
-;;; program's author (see below) or write to:
-;;;
-;;;              The Free Software Foundation, Inc.
-;;;              675 Mass Ave.
-;;;              Cambridge, MA 02139, USA. 
-;;;
-;;; Please send bug reports, etc. to zappo@gnu.ai.mit.edu.
-;;;
-;;; Updates can be found at:
-;;;    ftp://ftp.ultranet.com/pub/zappo
+;;; Copyright (C) 1996, 1998 Eric M. Ludlam
+;;
+;; Author: <zappo@gnu.org>
+;; RCS: $Id: eieio-opt.el,v 1.3 1998/10/27 18:51:50 zappo Exp $
+;; Keywords: OO, lisp
+;;                                                                          
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;           
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, you can either send email to this
+;; program's author (see below) or write to:
+;;
+;;              The Free Software Foundation, Inc.
+;;              675 Mass Ave.
+;;              Cambridge, MA 02139, USA.
+;;
+;; Please send bug reports, etc. to zappo@gnu.org
+;;
+;; Updates can be found at:
+;;    ftp://ftp.ultranet.com/pub/zappo
 
-;;;
 ;;; Commentary:
-;;;
-;;;   This contains support functions to eieio.  These functions contain
-;;; some small class browser and class printing functions.
-;;;
+;;
+;;   This contains support functions to eieio.  These functions contain
+;; some small class browser and class printing functions.
+;;
 
 (require 'eieio)
 
+;;; Code:
 (defun eieio-browse (&optional root-class)
-  "Create an object browser window which shows all objects starting
-with root-class, or `eieio-default-superclass' if none is given."
+  "Create an object browser window to show all objects.
+If optional ROOT-CLASS, then start with that, otherwise start with
+variable `eieio-default-superclass'."
   (interactive (if current-prefix-arg
-		   (list (read (completing-read "Class: " 
+		   (list (read (completing-read "Class: "
 						(eieio-build-class-alist)
 						nil t)))
 		 nil))
@@ -58,8 +58,10 @@ with root-class, or `eieio-default-superclass' if none is given."
     ))
 
 (defun eieio-browse-tree (this-root prefix ch-prefix)
-  "Recursive part of browser, draws the children of the given class on
-the screen."
+  "Recursively, draws the children of the given class on the screen.
+Argument THIS-ROOT is the local root of the tree.
+Argument PREFIX is the character prefix to use.
+Argument CH-PREFIX is another character prefix to display."
   (if (not (class-p (eval this-root))) (signal 'wrong-type-argument (list 'class-p this-root)))
   (let ((myname (symbol-name this-root))
 	(chl (aref (class-v this-root) class-children))
@@ -75,9 +77,9 @@ the screen."
     ))
 
 (defun eieio-thing-to-string (thing)
-  "Convert THING into a string.  If THING is an object, use
-`object-print' instead, if THING is a class, then use `class-name'
-instead, if THING is a list of stuff, try those."
+  "Convert THING into a string.
+If THING is an object, use `object-print' instead, if THING is a class,
+then use `class-name' instead, if THING is a list of stuff, try those."
   (if (object-p thing) (object-print thing)
     (if (class-p thing) (class-name thing)
       (if (and thing (listp thing))
@@ -90,13 +92,13 @@ instead, if THING is a list of stuff, try those."
   )
 
 (defun eieio-describe-class (class)
-  "Describe a CLASS defined by a string or symbol.  If CLASS is actually
-an object, then also display current values of that obect."
-  (interactive (list 
+  "Describe a CLASS defined by a string or symbol.
+If CLASS is actually an object, then also display current values of that obect."
+  (interactive (list
 		(completing-read "Class: " (eieio-build-class-alist) nil t)))
   (switch-to-buffer (get-buffer-create "*EIEIO OBJECT DESCRIBE*"))
   (erase-buffer)
-  (let* ((cv (cond ((stringp class) (if (string= class "") 
+  (let* ((cv (cond ((stringp class) (if (string= class "")
 					(class-v eieio-default-superclass)
 				      (class-v (read class))))
 		   ((symbolp class) (class-v class))
@@ -122,7 +124,7 @@ an object, then also display current values of that obect."
     (insert "\n")
     (while priva
       (let ((dvs (eieio-thing-to-string (car privd))))
-	(insert (symbol-name (car priva)) "\t" 
+	(insert (symbol-name (car priva)) "\t"
 		(if (< (length (symbol-name (car priva))) 8) "\t" "")
 		(if (< (length (symbol-name (car priva))) 16) "\t" "")
 		dvs
@@ -164,7 +166,9 @@ an object, then also display current values of that obect."
 (defalias 'describe-class 'eieio-describe-class)
 
 (defun eieio-build-class-alist (&optional class buildlist)
-  "Returns an alist of all currently active classes for completion purposes."
+  "Return an alist of all currently active classes for completion purposes.
+Optional argument CLASS is the class to start with.
+Optional argument BUILDLIST is more list to attach."
   (let* ((cc (or class eieio-default-superclass))
 	 (sublst (aref (class-v cc) class-children)))
     (setq buildlist (cons (cons (symbol-name cc) 1) buildlist))
@@ -173,5 +177,6 @@ an object, then also display current values of that obect."
       (setq sublst (cdr sublst)))
     buildlist))
 
-;;; end of lisp
 (provide 'eieio-opt)
+
+;;; eieio-opt.el ends here
