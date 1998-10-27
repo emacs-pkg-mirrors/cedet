dff11709e45318e82fddb628512ac4771343c9b3
zappo <zappo>
1998-10-27 17:44:18 +0000

    Fixed for lack of widget stuff.

--- eieio/eieiocomp.el
+++ eieio/eieiocomp.el
@@ -1,30 +1,30 @@
-;;; compile the eieio distribution
+;;; eieiocomp.el --- compile the eieio distribution
 
-;;;
-;;; Copyright (C) 1996, 1997 Eric M. Ludlam
-;;;
-;;; This program is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2, or (at your option)
-;;; any later version.
-;;;
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with this program; if not, you can either send email to this
-;;; program's author (see below) or write to:
-;;;
-;;;              The Free Software Foundation, Inc.
-;;;              675 Mass Ave.
-;;;              Cambridge, MA 02139, USA. 
-;;;
-;;; Please send bug reports, etc. to zappo@gnu.ai.mit.edu.
-;;;
-;;; $Id: eieiocomp.el,v 1.1 1997/01/29 02:19:15 zappo Exp $
+;; Copyright (C) 1996, 1997, 1998 Eric M. Ludlam
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, you can either send email to this
+;; program's author (see below) or write to:
+;;
+;;              The Free Software Foundation, Inc.
+;;              675 Mass Ave.
+;;              Cambridge, MA 02139, USA.
+;;
+;; Please send bug reports, etc. to zappo@gnu.ai.mit.edu.
+;;
+;; $Id: eieiocomp.el,v 1.2 1998/10/27 17:44:18 zappo Exp $
 
+;;; Code:
 (load-library "bytecomp")
 
 (let* ((cdir (expand-file-name default-directory))
@@ -35,16 +35,18 @@
 
   (message "PATH:\n%s" load-path)
 
-  ;; Now some object things.  This one file will load eieio, dialog,
-  ;; and widget stuff
-  (load-library "dlg-config.el")
-  ;; Load each eieio user for compiling
+  ;; Load each eieio client for compiling
   (load-library "tree.el")
   (load-library "call-tree.el")
-  (load-library "dialog-tree.el")
   (load-library "chart.el")
+  (load-library "dbif.el")
+  (load-library "psql.el")
 
   (message "Calling recompile on %s" udir)
 
   (byte-recompile-directory udir 0)
 )
+
+(provide 'eieiocomp)
+
+;;; eieiocomp.el ends here
