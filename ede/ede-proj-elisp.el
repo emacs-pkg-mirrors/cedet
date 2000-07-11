eb8745bfc483deefd52072c8c58a29265538096d
zappo <zappo>
2000-07-11 23:14:17 +0000

    Eval and compile requires. Removed load path and requiresments.  Replaced with aux-packages. Added ede-proj-elisp-packages-to-loadpath (see above) Replaced path/requirements usage to above two comments. Added `ede-proj-elisp-add-path', `ede-proj-tweek-autoconf' `ede-proj-flush-autoconf'

--- ede/ede-proj-elisp.el
+++ ede/ede-proj-elisp.el
@@ -4,7 +4,7 @@
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: project, make
-;; RCS: $Id: ede-proj-elisp.el,v 1.5 2000/07/03 14:01:20 zappo Exp $
+;; RCS: $Id: ede-proj-elisp.el,v 1.6 2000/07/11 23:14:17 zappo Exp $
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -27,32 +27,38 @@
 ;;
 ;; Handle Emacs Lisp in and EDE Project file.
 
+(eval-and-compile (require 'ede-proj))
+
 ;;; Code:
 (defclass ede-proj-target-elisp (ede-proj-target-makefile)
   ((menu :initform nil)
    (keybindings :initform nil)
-   (load-path :initarg :load-path
-	      :initform nil
-	      :type list
-	      :custom (repeat string)
-	      :documentation "Additional load-path arguments.
-When compiling from the command line, these are added to the makefile.
-When compiling from within emacs, these are ignored.")
-   (requirements :initarg :requirements
+   (aux-packages :initarg :aux-packages
 		 :initform nil
 		 :type list
 		 :custom (repeat string)
-		 :documentation
-		 "Additional packages that should be loaded before building.
-When using eieio, tools generally need to be loaded before you can compile
-them safely.")
-   )
+		 :documentation "Additional packages needed.
+There should only be one toplevel package per auxiliary tool needed.
+These packages location is found, and added to the compile time
+load path."
+   ))
   "This target consists of a group of lisp files.
 A lisp target may be one general program with many separate lisp files in it.")
 
 (defmethod ede-want-file-p ((obj ede-proj-target-elisp) file)
   "Return t if OBJ wants to own FILE."
   (string-match "\\.el$" file))
+
+(defun ede-proj-elisp-packages-to-loadpath (packages)
+  "Convert a list of PACKAGES, to a list of load path."
+  (let ((paths nil))
+    (while packages
+      (if (not (locate-library (car packages)))
+	  (error "Cannot find package %s" (car packages)))
+      (setq paths (cons (file-name-directory (locate-library (car packages)))
+			paths)
+	    packages (cdr packages)))
+    paths))
 
 (defmethod project-compile-target ((obj ede-proj-target-elisp))
   "Compile all sources in a Lisp target OBJ."
@@ -78,8 +84,11 @@ A lisp target may be one general program with many separate lisp files in it.")
   "Insert variables needed by target THIS."
   (call-next-method this)
   (insert "EMACS=" (file-name-nondirectory (car command-line-args)) "\n")
-  (if (oref this load-path)
-      (insert "LOADPATH=" (mapconcat (lambda (a) a) (oref this load-path) " ")
+  (if (oref this aux-packages)
+      (insert "LOADPATH=" (mapconcat (lambda (a) a)
+				     (ede-proj-elisp-packages-to-loadpath
+				      (oref this aux-packages))
+				     " ")
 	      "\n")))
 
 (defmethod ede-proj-makefile-garbage-patterns ((this ede-proj-target-elisp))
@@ -94,7 +103,7 @@ These are removed with make clean."
   (insert (ede-name this) ":\n"
 	  "\t@echo \"(add-to-list 'load-path \\\"$(PWD)\\\")\" > "
 	  (ede-name this) "-compile-script\n")
-  (if (oref this load-path)
+  (if (oref this aux-packages)
       (progn
 	(insert "\t@for loadpath in ${LOADPATH}; do \\\n")
 	(insert "\t  echo \"(add-to-list 'load-path \\\"$$loadpath\\\")\" >> "
@@ -102,25 +111,71 @@ These are removed with make clean."
 	(insert "\t  done\n")))
   (insert "\t@echo \"(setq debug-on-error t)\" >> "
 	  (ede-name this) "-compile-script\n")
-;  (let ((lp (oref this load-path)))
-;    (while lp
-;      (insert "\t@echo \"(add-to-list 'load-path \\\"" (car lp) "\\\")\" >> "
-;	      (ede-name this) "-comp\n")
-;      (setq lp (cdr lp))))
-  (let ((ar (oref this requirements)))
-    (while ar
-      (insert "\t@echo \"(require '")
-      (if (not (string-match "\\.el$" (car ar)))
-	  (insert (car ar))
-	;; This adds a string param indicating that we should load from
-	;; the .el file, not an existing .elc file.
-	(insert (substring (car ar) 0 (match-beginning 0))
-		" \\\"" (car ar) "\\\""))
-      (insert ")\" >> " (ede-name this) "-compile-script\n")
-      (setq ar (cdr ar))))
   (insert "\t$(EMACS) -batch -l " (ede-name this) "-compile-script "
 	  "-f batch-byte-compile  $(" (ede-proj-makefile-sourcevar this)
 	  ")\n"))
+
+(defun ede-proj-elisp-add-path (path)
+  "Add path PATH into the file if it isn't already there."
+  (goto-char (point-min))
+  (if (re-search-forward (concat "(cons \\\""
+				 (regexp-quote path))
+			 nil t)
+      nil;; We have it already
+    (if (re-search-forward "(cons nil" nil t)
+	(progn
+	  ;; insert stuff here
+	  (end-of-line)
+	  (insert "\n"
+		  "   echo \"(setq load-path (cons \\\""
+		  path
+		  "\\\" load-path))\" >> script")
+	  )
+      (error "Don't know how to update load path"))))
+
+(defmethod ede-proj-tweek-autoconf ((this ede-proj-target-elisp))
+  "Tweek the configure file (current buffer) to accomodate THIS."
+  (autoconf-insert-new-macro "AM_PATH_LISPDIR")
+  ;; Ok, now we have to tweek the autoconf provided `elisp-comp' program.
+  (let ((ec (ede-expand-filename this "elisp-comp")))
+    (if (not (file-exists-p ec))
+	(message "There may be compile errors.  Rerun a second time.")
+      (save-excursion
+	(if (file-symlink-p ec)
+	    (progn
+	      ;; Desymlinkafy
+	      (rename-file ec (concat ec ".tmp"))
+	      (copy-file (concat ec ".tmp") ec)
+	      (delete-file (concat ec ".tmp"))))
+	(set-buffer (find-file-noselect ec t))
+	(ede-proj-elisp-add-path "..")
+	(let ((paths (ede-proj-elisp-packages-to-loadpath
+		      (oref this aux-packages))))
+	  ;; Add in the current list of paths
+	  (while paths
+	    (ede-proj-elisp-add-path (car paths))
+	    (setq paths (cdr paths))))
+	(save-buffer)) )))
+
+(defmethod ede-proj-flush-autoconf ((this ede-proj-target-elisp))
+  "Flush the configure file (current buffer) to accomodate THIS."
+  ;; Remove crufty old paths from elisp-compile
+  (let ((ec (ede-expand-filename this "elisp-comp"))
+	(paths (ede-proj-elisp-packages-to-loadpath
+		(oref this aux-packages))))
+    (if (file-exists-p ec)
+	(save-excursion
+	  (set-buffer (find-file-noselect ec t))
+	  (goto-char (point-min))
+	  (while (re-search-forward "(cons \\([^ ]+\\) load-path)"
+				    nil t)
+	    (let ((path (match-string 1)))
+	      (if (string= path "nil")
+		  nil
+		(delete-region (save-excursion (beginning-of-line) (point))
+			       (save-excursion (end-of-line)
+					       (forward-char 1)
+					       (point))))))))))
 
 (provide 'ede-proj-elisp)
 
