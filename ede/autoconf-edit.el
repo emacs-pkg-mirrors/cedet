115ce6dddef406470fc6c92f90b206f4eaa2ae06
zappo <zappo>
2000-07-22 12:40:05 +0000

    Removed autoconf into autoconf compat.

--- ede/autoconf-edit.el
+++ ede/autoconf-edit.el
@@ -4,7 +4,7 @@
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: project
-;; RCS: $Id: autoconf-edit.el,v 1.3 2000/07/11 23:10:20 zappo Exp $
+;; RCS: $Id: autoconf-edit.el,v 1.4 2000/07/22 12:40:05 zappo Exp $
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -35,83 +35,33 @@
 	(require 'autoconf)
       (error (require 'autoconf-mode "autoconf")))
 
-;; Only use this copy of autoconf.el in Emacs21 if it isn't already
-;; available.
-;;
-;;  Originally written by Dave Love for Emacs 21
-(defvar autoconf-mode-map (make-sparse-keymap))
-
-(defvar autoconf-mode-hook nil
-  "Hook run by `autoconf-mode'.")
-
-(defconst autoconf-font-lock-syntactic-keywords
-  '(("\\<dnl\\>" 0 '(11))))
-
-(defconst autoconf-definition-regexp
-  "AC_\\(SUBST\\|DEFINE\\(_UNQUOTED\\)?\\)(\\(\\sw+\\)")
-
-(defvar autoconf-font-lock-keywords
-  `(("A[CM]_\\sw+" . font-lock-keyword-face)
-    (,autoconf-definition-regexp
-     3 font-lock-function-name-face)
-    ;; Are any other M4 keywords really appropriate for configure.in,
-    ;; given that we do `dnl'?
-    ("changequote" . font-lock-keyword-face)))
-
-(defvar autoconf-mode-syntax-table
-  (let ((table (make-syntax-table)))
-    (modify-syntax-entry ?\" "." table)
-    (modify-syntax-entry ?\n ">" table)
-    (modify-syntax-entry ?# "<" table)
-    table))
-
-(defvar autoconf-imenu-generic-expression
-  (list (list nil autoconf-definition-regexp 3)))
-
-;; It's not clear how best to implement this.
-(defun autoconf-current-defun-function ()
-  "Function to use for `add-log-current-defun-function' in Autoconf mode.
-This version looks back for an AC_DEFINE or AC_SUBST.  It will stop
-searching backwards at another AC_... command."
-  (save-excursion
-    (with-syntax-table autoconf-mode-syntax-table
-      (modify-syntax-entry ?_ "w")
-      (if (re-search-backward autoconf-definition-regexp
-			      (save-excursion (beginning-of-defun) (point))
-			      t)
-	  (match-string-no-properties 3)))))
-
-;;;###autoload
-(defun autoconf-mode ()
-  "Major mode for editing Autoconf configure.in files."
-  (interactive)
-  (kill-all-local-variables)
-  (use-local-map autoconf-mode-map)
-  (setq major-mode 'autoconf-mode)
-  (setq mode-name "Autoconf")
-  (set-syntax-table autoconf-mode-syntax-table)
-  (set (make-local-variable 'parens-require-spaces) nil) ; for M4 arg lists
-  (set (make-local-variable 'defun-prompt-regexp)
-       "^[ \t]*A[CM]_\\(\\sw\\|\\s_\\)+")
-  (set (make-local-variable 'comment-start) "dnl ")
-  (set (make-local-variable 'comment-start-skip) "\\(\\<dnl\\|#\\) +")
-  (set (make-local-variable 'font-lock-syntactic-keywords)
-       autoconf-font-lock-syntactic-keywords)
-  (set (make-local-variable 'font-lock-defaults)
-       `(autoconf-font-lock-keywords nil nil (("_" . "w"))))
-  (set (make-local-variable 'imenu-generic-expression)
-       autoconf-imenu-generic-expression)
-  (set (make-local-variable 'imenu-syntax-alist) '(("_" . "w")))
-  (set (make-local-variable 'indent-line-function) #'indent-relative)
-  (set (make-local-variable 'add-log-current-defun-function)
-	#'autoconf-current-defun-function)
-  (run-hooks 'autoconf-mode-hook))
+  (eval-and-compile (if (not (fboundp 'with-syntax-table))
+			
+;; Copied from Emacs 21 for compatibility with released Emacses.
+(defmacro with-syntax-table (table &rest body)
+  "Evaluate BODY with syntax table of current buffer set to a copy of TABLE.
+The syntax table of the current buffer is saved, BODY is evaluated, and the
+saved table is restored, even in case of an abnormal exit.
+Value is what BODY returns."
+  (let ((old-table (make-symbol "table"))
+	(old-buffer (make-symbol "buffer")))
+    `(let ((,old-table (syntax-table))
+	   (,old-buffer (current-buffer)))
+       (unwind-protect
+	   (progn
+	     (set-syntax-table (copy-syntax-table ,table))
+	     ,@body)
+	 (save-current-buffer
+	   (set-buffer ,old-buffer)
+	   (set-syntax-table ,old-table))))))
 
 )
 
+(require 'autoconf-compat)
+
 ;; This part is not in autoconf.el
 (add-to-list 'auto-mode-alist '("\\<configure\\.in$" . autoconf-mode))
-
+)
 
 (defvar autoconf-new-automake-string
   "dnl Process this file with autoconf to produce a configure script
@@ -139,7 +89,7 @@ configure the initial configure script using `autoconf-new-automake-string'"
     (if (not (string-match "\\(/\\|\\\\)$" rootdir))
 	(setq rootdir (concat rootdir "/")))
     (let ((cf (concat rootdir "configure.in")))
-      (if (and (file-exists cf)
+      (if (and (file-exists-p cf)
 	       (not (y-or-n-p (format "File %s exists.  Start Over? " cf))))
 	  (error "Quit"))
       (find-file cf)))
