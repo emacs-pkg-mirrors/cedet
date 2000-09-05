3468d03297cfb99f64a670c22e988830e3ec9947
zappo <zappo>
2000-09-05 00:40:33 +0000

    Removed some stuff from the header. For `expandtag' button type, make sure it is three characters long, otherwise the expand/contract code can get a little confused when working with images.

--- speedbar/speedbar.el
+++ speedbar/speedbar.el
@@ -4,7 +4,7 @@
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: file, tags, tools
-;; X-RCS: $Id: speedbar.el,v 1.181 2000/09/02 02:36:39 zappo Exp $
+;; X-RCS: $Id: speedbar.el,v 1.182 2000/09/05 00:40:33 zappo Exp $
 
 (defvar speedbar-version "0.13beta1"
   "The current version of speedbar.")
@@ -31,43 +31,6 @@
 ;;   The speedbar provides a frame in which files, and locations in
 ;; files are displayed.  These items can be clicked on with mouse-2
 ;; in order to make the last active frame display that file location.
-;;
-;; Starting Speedbar:
-;;
-;;   If speedbar came to you as a part of Emacs, simply type
-;; `M-x speedbar', and it will be autoloaded for you.
-;;
-;;   If speedbar is not a part of your distribution, then add
-;; this to your .emacs file:
-;;
-;;   (autoload 'speedbar-frame-mode "speedbar" "Popup a speedbar frame" t)
-;;   (autoload 'speedbar-get-focus "speedbar" "Jump to speedbar frame" t)
-;;
-;;   If you want to choose it from a menu, such as "Tools", you can do this:
-;;
-;;   Emacs:
-;;   (define-key-after (lookup-key global-map [menu-bar tools])
-;;      [speedbar] '("Speedbar" . speedbar-frame-mode) [calendar])
-;;
-;;   XEmacs:
-;;   (add-menu-button '("Tools")
-;;		      ["Speedbar" speedbar-frame-mode
-;;		       :style toggle
-;;		       :selected (and (boundp 'speedbar-frame)
-;;				      (frame-live-p speedbar-frame)
-;;				      (frame-visible-p speedbar-frame))]
-;;		      "--")
-;;
-;;   If you want to access speedbar using only the keyboard, do this:
-;;
-;;   (global-set-key [(f4)] 'speedbar-get-focus)
-;;
-;;   This will let you hit f4 (or whatever key you choose) to jump
-;; focus to the speedbar frame.  Pressing it again will bring you back
-;; to the attached frame.  Pressing RET or e to jump to a file
-;; or tag will move you back to the attached frame.  The command
-;; `speedbar-get-focus' will also create a speedbar frame if it does
-;; not exist.
 ;;
 ;; Customizing Speedbar:
 ;;
@@ -1925,8 +1888,8 @@ position to insert a new item, and that the new item will end with a CR"
     (let* ((exp-button (cond ((eq exp-button-type 'bracket) "[%c]")
 			     ((eq exp-button-type 'angle) "<%c>")
 			     ((eq exp-button-type 'curly) "{%c}")
-			     ((eq exp-button-type 'expandtag) "%c>")
-			     ((eq exp-button-type 'statictag) "=>")
+			     ((eq exp-button-type 'expandtag) " %c>")
+			     ((eq exp-button-type 'statictag) " =>")
 			     (t ">")))
 	   (buttxt (format exp-button exp-button-char))
 	   (start (point))
