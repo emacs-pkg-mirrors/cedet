16d561030e6c37f90c6a354c704ab3d29e32556e
zappo <zappo>
2000-09-24 15:32:22 +0000

    Copyright update. Added `ede-proj-makefile-insert-source-variables' which produces a different variable between Makefile and Makefile.am mode.

--- ede/ede-proj-archive.el
+++ ede/ede-proj-archive.el
@@ -4,16 +4,14 @@
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: project, make
-;; RCS: $Id: ede-proj-archive.el,v 1.5 2000/07/22 12:41:44 zappo Exp $
+;; RCS: $Id: ede-proj-archive.el,v 1.6 2000/09/24 15:32:22 zappo Exp $
 
-;; This file is NOT part of GNU Emacs.
-
-;; GNU Emacs is free software; you can redistribute it and/or modify
+;; This software is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This software is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
@@ -34,6 +32,16 @@
   (ede-proj-target-makefile-objectcode)
   ()
   "This target generates an object code archive.")
+
+
+(defmethod ede-proj-makefile-insert-source-variables :BEFORE
+  ((this ede-proj-target-makefile-archive))
+  "Insert bin_PROGRAMS variables needed by target THIS.
+We aren't acutally inserting SOURCE details, but this is used by the
+Makefile.am generator, so use it to add this important bin program."
+  (ede-pmake-insert-variable-shared
+      (concat "lib" (ede-name this) "_a_LIBRARIES")
+    (insert (concat "lib" (ede-name this) ".a"))))
 
 (defmethod ede-proj-makefile-insert-rules
   ((this ede-proj-target-makefile-archive))
