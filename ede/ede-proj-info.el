cd4e8bd08f054c37230cc4a34244ae9e6524e622
zappo <zappo>
2000-09-24 15:37:39 +0000

    Copyright update. Removed eval-and-compile restriction. Added availablecompilers and sourcetype defaults Added `ede-makeinfo-source' and `ede-makeinfo-compiler'. Removed garbage-patters, and insert-rules. Added `ede-proj-makefile-target-name' and `object-write' methods.

--- ede/ede-proj-info.el
+++ ede/ede-proj-info.el
@@ -4,16 +4,14 @@
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: project, make
-;; RCS: $Id: ede-proj-info.el,v 1.6 2000/07/11 23:14:27 zappo Exp $
+;; RCS: $Id: ede-proj-info.el,v 1.7 2000/09/24 15:37:39 zappo Exp $
 
-;; This file is NOT part of GNU Emacs.
-
-;; GNU Emacs is free software; you can redistribute it and/or modify
+;; This software is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This software is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
@@ -27,12 +25,14 @@
 ;;
 ;; Handle texinfo in and EDE Project file.
 
-(eval-and-compile (require 'ede-proj))
+(require 'ede-pmake)
 
 ;;; Code:
 (defclass ede-proj-target-makefile-info (ede-proj-target-makefile)
   ((menu :initform nil)
    (keybindings :initform nil)
+   (availablecompilers :initform (ede-makeinfo-compiler))
+   (sourcetype :initform (ede-makeinfo-source))
    (mainmenu :initarg :mainmenu
 	     :initform ""
 	     :type string
@@ -41,9 +41,26 @@
 All other sources should be included independently."))
   "Target for a single info file.")
 
-(defmethod ede-want-file-p ((obj ede-proj-target-makefile-info) file)
-  "Return t if OBJ wants to own FILE."
-  (string-match "\\.texi?$" file))
+(defvar ede-makeinfo-source
+  (ede-sourcecode "ede-makeinfo-source"
+		  :name "Texinfo"
+		  :sourcepattern "\\.texi?$"
+		  :garbagepattern '("*.info"))
+  "Texinfo source code definition.")
+
+(defvar ede-makeinfo-compiler
+  (ede-compiler
+   "ede-makeinfo-compiler"
+   :name "makeinfo"
+   :variables '(("MAKEINFO" . "makeinfo"))
+   :commands '("makeinfo -o $@ $<")
+   :autoconf '(("AC_CHECK_PROG" . "MAKEINFO, makeinfo"))
+   :sourcetype '(ede-makeinfo-source)
+   ;; the extention keys off if we want intermediate files or not,
+   ;; not what the actual extention is.
+   :objectextention ".info"
+   )
+  "Compile texinfo files into info files.")
 
 ;;; Makefile generation
 ;;
@@ -51,21 +68,24 @@ All other sources should be included independently."))
   "Return the variable name for THIS's sources."
   (concat (ede-pmake-varname this) "_INFOS"))
 
-(defmethod ede-proj-makefile-garbage-patterns
-  ((this ede-proj-target-makefile-info))
-  "Return a list of patterns that are considred garbage to THIS.
-These are removed with make clean."
-  '("*.info")
-  )
+(defmethod ede-proj-makefile-target-name ((this ede-proj-target-makefile-info))
+  "Return the name of the main target for THIS target."
+  (let ((n (ede-name this)))
+    (if (string-match "\\.info$" n)
+	n
+      (concat n ".info"))))
 
-(defmethod ede-proj-makefile-insert-rules ((this ede-proj-target-makefile-info))
-  "Insert rules to build THIS set of texinfo documentation files."
-  (call-next-method)
-  (let ((mm (oref this mainmenu)))
-    (if (or (string= mm "") (not mm))
-	(setq mm (car (oref this source))))
-    (insert "\n" (ede-name this) ": $(" (ede-pmake-varname this) "_INFOS)\n"
-	    "\tmakeinfo " mm "\n")))
+(defmethod object-write ((this ede-proj-target-makefile-info))
+  "Before commiting any change to THIS, make sure the mainmenu is first."
+   (let ((mm (oref this mainmenu))
+	 (s (oref this source))
+	 (nl nil))
+     (if (or (string= mm "") (not mm) (string= mm (car s)))
+	 nil
+       ;; Make sure that MM is first in the list of items.
+       (setq nl (cons mm (delq mm s)))
+       (oset this source nl)))
+   (call-next-method))
 
 (defmethod ede-documentation ((this ede-proj-target-makefile-info))
   "Return a list of files that provides documentation.
