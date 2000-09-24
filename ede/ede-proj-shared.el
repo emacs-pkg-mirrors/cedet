deaff3d998713264ac05cf7b5eea45ccec5da366
zappo <zappo>
2000-09-24 15:51:45 +0000

    Copyright update. Removed libtool stuff from class. Added defaults for availablecompilers. Added `ede-gcc-shared-compiler', `ede-gcc-libtool-shared-compiler', with some cruft ifed out. Added `ede-proj-configure-add-missing',  `ede-proj-makefile-insert-automake-pre-variables',  `ede-proj-makefile-insert-automake-post-variables',  `ede-proj-makefile-target-name', and  `ede-proj-makefile-sourcevar (for automake stuff)

--- ede/ede-proj-shared.el
+++ ede/ede-proj-shared.el
@@ -4,16 +4,14 @@
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: project, make
-;; RCS: $Id: ede-proj-shared.el,v 1.4 2000/07/12 14:09:48 zappo Exp $
+;; RCS: $Id: ede-proj-shared.el,v 1.5 2000/09/24 15:51:45 zappo Exp $
 
-;; This file is NOT part of GNU Emacs.
-
-;; GNU Emacs is free software; you can redistribute it and/or modify
+;; This software is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This software is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
@@ -28,27 +26,102 @@
 ;; Handle shared object libraries in and EDE Project file.
 ;; Tries to deal with libtool and non-libtool situations.
 
-(eval-and-compile (require 'ede-proj-prog))
+(require 'ede-proj-prog)
 
 ;;; Code:
 (defclass ede-proj-target-makefile-shared-object
   (ede-proj-target-makefile-program)
-  ((ldflags :custom (repeat (string :tag "Libtool flag"))
+  ((availablecompilers :initform (ede-gcc-shared-compiler
+				  ede-gcc-libtool-shared-compiler))
+   (ldflags :custom (repeat (string :tag "Libtool flag"))
 	    :documentation
-	    "Additional flags to add when linking this target with libtool.
+	    "Additional flags to add when linking this shared library.
 Use ldlibs to add addition libraries.")
-   (libtool :initarg :libtool
-	    :initform nil
-	    :custom boolean
-	    :documentation
-	    "Non-nil if libtool should be used to generate the library.")
    )
-  "This target generates a shared library using libtool.")
+  "This target generates a shared library.")
 
-; This variable is used in automake for listing active libraries.
-;(defmethod ede-proj-makefile-sourcevar ((this ede-proj-target-makefile-shared-object))
-;  "Return the variable name for THIS's sources."
-;  (concat (oref this name) "_LTLIBRARIES"))
+(defvar ede-gcc-shared-compiler
+  (clone ede-gcc-compiler
+	 "ede-c-shared-compiler"
+	 :name "gcc -shared"
+	 :variables '(("CC_SHARED" . "gcc")
+		      ("C_SHARED_COMPILE" .
+		       "$(CC_SHARED) -shared $(DEFS) $(INCLUDES) $(CPPFLAGS) $(CFLAGS)"))
+	 :linkvariables '(("C_SHARED_LINK" .
+			   "$(CC_SHARED) -shared $(CFLAGS) $(LDFLAGS) -L. -o $@ $^")
+			  )
+	 :commands '("$(C_SHARED_LINK) %s")
+	 :autoconf '("AM_PROG_LIBTOOL")
+	 )
+  "Compiler for C sourcecode.")
+
+(defvar ede-gcc-libtool-shared-compiler
+  (clone ede-gcc-shared-compiler
+	 "ede-c-shared-compiler-libtool"
+	 :name "libtool"
+	 :variables '(("LIBTOOL" . "$(SHELL) libtool")
+		      ("LTCOMPILE" . "$(LIBTOOL) --mode=compile $(CC) $(DEFS) $(INCLUDES) $(CPPFLAGS) $(CFLAGS)")
+		      ("LTLINK" . "$(LIBTOOL) --mode=link $(CC) $(CFLAGS) $(LDFLAGS) -L. -o $@")
+		      )
+	 :commands '("$(LTLINK) $^"
+		     )
+	 :autoconf '("AM_PROG_LIBTOOL")
+	 )
+  "Compiler for C sourcecode.")
+
+(when nil
+  		 
+	       
+  (insert;; These C to O rules create dependencies
+   "%.o: %.c\n"
+   "\t@echo '$(COMPILE) -c $<'; \\\n"
+   "\t$(COMPILE)"
+   (if (oref this automatic-dependencies)
+       " -Wp,-MD,.deps/$(*F).P"
+     "")
+   " -c $<\n\n")
+  (if have-libtool
+      (insert;; These C to shared o rules create pic code.
+       "%.lo: %.c\n"
+       "\t@echo '$(LTCOMPILE) -c $<'; \\\n"
+       "\t$(LTCOMPILE) -Wp,-MD,.deps/$(*F).p -c $<\n"
+       "\t@-sed -e 's/^\([^:]*\)\.o:/\1.lo \1.o:/' \\\n"
+       "\t      < .deps/$(*F).p > .deps/$(*F).P\n"
+       "\t@-rm -f .deps/$(*F).p\n\n"))
+  )
+
+(defmethod ede-proj-configure-add-missing ((this ede-proj-target))
+  "Query if any files needed by THIS provided by automake are missing.
+Results in --add-missing being passed to automake."
+  (not (and (ede-expand-filename (ede-toplevel) "ltconfig")
+	    (ede-expand-filename (ede-toplevel) "ltmain.sh"))))
+
+(defmethod ede-proj-makefile-insert-automake-pre-variables
+  ((this ede-proj-target-makefile-shared-object))
+  "Insert bin_PROGRAMS variables needed by target THIS.
+We aren't acutally inserting SOURCE details, but this is used by the
+Makefile.am generator, so use it to add this important bin program."
+  (ede-pmake-insert-variable-shared "lib_LTLIBRARIES"
+     (insert (concat "lib" (ede-name this) ".la"))))
+
+(defmethod ede-proj-makefile-insert-automake-post-variables
+  ((this ede-proj-target-makefile-shared-object))
+  "Insert bin_PROGRAMS variables needed by target THIS.
+We need to override -program which has an LDADD element."
+  nil)
+
+(defmethod ede-proj-makefile-target-name ((this ede-proj-target-makefile-shared-object))
+  "Return the name of the main target for THIS target."
+  ;; We need some platform gunk to make the .so change to .sl, or .a,
+  ;; depending on the platform we are going to compile against.
+  (concat "lib" (ede-name this) ".so"))
+
+(defmethod ede-proj-makefile-sourcevar ((this ede-proj-target-makefile-shared-object))
+  "Return the variable name for THIS's sources."
+  (if (eq (oref (ede-target-parent this) makefile-type) 'Makefile.am)
+      (concat "lib" (oref this name) "_la_SOURCES")
+    (call-next-method)))
+  
 
 (provide 'ede-proj-shared)
 
