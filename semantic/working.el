1206bd47bb2029c795a95dba2d9c63c9566a735f
zappo <zappo>
2000-09-29 03:22:26 +0000

    Fixed copyright.

--- semantic/working.el
+++ semantic/working.el
@@ -11,7 +11,7 @@
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
