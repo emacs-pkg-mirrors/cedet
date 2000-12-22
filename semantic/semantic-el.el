c2ce3584ffd8020388e153f1de8b4668d9cd907d
zappo <zappo>
2000-12-22 16:09:07 +0000

    Dump parsing of the lisp functions, and use a magic read function to pep up the speed of lisp parsing.
    Eric

--- semantic/semantic-el.el
+++ semantic/semantic-el.el
@@ -3,7 +3,7 @@
 ;;; Copyright (C) 1999, 2000 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
-;; X-RCS: $Id: semantic-el.el,v 1.36 2000/10/06 03:18:09 zappo Exp $
+;; X-RCS: $Id: semantic-el.el,v 1.37 2000/12/22 16:09:07 zappo Exp $
 
 ;; This file is not part of GNU Emacs.
 
@@ -34,140 +34,83 @@
   `((bovine-toplevel
      (semantic-list
       ,(lambda (vals start end)
-	 (let ((i (semantic-bovinate-from-nonterminal
-		   start end 'extract-toplevel nil
-		   ;; NOTE, currently the longest item we have is 6 long,
-		   ;; so only ask the flexer to go out 6 tokens.
-		   6)))
-	   (append (nreverse (cdr (cdr (reverse i))))
-		   (list start end)))))
-     (extract-toplevel))
-    ;; When parsing at depth 0, we need to extract elements from semantic
-    ;; lists at bovine-toplevel.  This symbol provides the needed redirection.
-    (extract-toplevel
-     (function)
-     (variable)
-     (type)
-     (include)
-     (package)
-     (method)
-     (advice)
-     (code)
-     (comment) )
-    ;; A type is defined by extended tools like CL, or EIEIO
-    (type
-     (open-paren symbol "defclass" symbol arg-list
-		 field-list doc-string
-		 ,(semantic-lambda
-		   (list (nth 2 vals) 'type
-			 "class"
-			 (nth 4 vals) (nth 3 vals) nil
-			 (car-safe (nth 5 vals))))))
-    ;; A function is anything that starts with a (defun
-    (function
-     (open-paren symbol "defun\\|defmacro\\|defsubst" symbol arg-list doc-string
-		 ,(semantic-lambda
-		    (list (nth 2 vals) 'function nil (nth 3 vals) nil
-			  (car-safe (nth 4 vals))))))
-    (method
-     (open-paren symbol "defmethod\\|defgeneric" symbol opt-label arg-list
-		 doc-string
-		 ,(semantic-lambda
-		    (list (nth 2 vals) 'function nil (nth 4 vals) nil
-			  (car-safe (nth 5 vals))))))
-    (advice
-     (open-paren symbol "defadvice" symbol arg-list
-		 doc-string
-		 ,(semantic-lambda
-		   (list (nth 2 vals) 'function nil (nth 3 vals) nil
-			 (car-safe (nth 4 vals))))))
-    ;; A variable can be a defvar or defconst.
-    (variable
-     (open-paren symbol "defvar\\|defconst\\|defcustom\\|defface\\|defimage"
-		 symbol expression doc-string
-		 ,(semantic-lambda
-		    (list (nth 2 vals) 'variable nil
-			  (if (string= (nth 1 vals) "defconst") t nil)
-			  nil nil (car-safe (nth 4 vals))))))
-    ;; In elisp, an include is just the require statement.
-    (include
-     (open-paren symbol "require" quote symbol
-		 ,(semantic-lambda
-		    (list (nth 3 vals) 'include nil nil))))
-    ;; in elisp, a package statement is the same as the provide token.
-    (package
-     (open-paren symbol "provide" quote symbol opt-filestring close-paren
-		 ,(semantic-lambda
-		    (list (nth 3 vals) 'package (nth 4 vals) nil))))
-    (opt-filestring
-     (string)
-     ( ,(lambda (vals start end) (list nil))))
-    ;; Some random code stuck in there.
-    (code
-     (open-paren symbol
-		 ,(semantic-lambda
-		    (let ((sym (if (nth 1 vals) (intern-soft (nth 1 vals)))))
-		      (if (and sym (fboundp sym))
-			  (list (nth 1 vals) 'code))))))
-    ;; Doc strings are sometimes optional, and always just return the
-    ;; start position.
-    (doc-string
-     (string ,(lambda (vals start end) (list start start end)))
-     (comment ,(lambda  (vals start end) (list start start end)))
-     ())
-    ;; Quotes are oft optional in some cases
-    (quote (punctuation "'"))
-    ;; Backquotes are also optional for macro type thingies
-    (backquote (punctuation "`"))
-    ;; Something that can be evaluated out to something.
-    (expression
-     (quote expression ,(semantic-lambda (list (car (cdr vals)))))
-     (backquote expression ,(semantic-lambda (list (car (cdr vals)))))
-     (semantic-list) (symbol) (string))
-    ;; An argument list to a function
-    (arg-list
-     (symbol "nil" ,(lambda (vals start end) (list nil)))
-     (semantic-list ,(lambda (vals start end)
-		       (semantic-bovinate-from-nonterminal start end 'argsyms)
-		       ))
-     ;; If it's already opened, what to do??
-     )
-    (argsyms
-     (open-paren close-paren ,(semantic-lambda
-				(list nil)))
-     (open-paren argsyms ,(semantic-lambda (car (cdr vals))))
-     (symbol argsyms ,(semantic-lambda
-			(append (cons (car vals) (car (cdr vals))))))
-     (semantic-list argsyms
-		    ,(semantic-lambda
-		      (let ((e (read (buffer-substring (car (nth 0 vals))
-						      (cdr (nth 0 vals))))))
-			(cons (symbol-name (car e))
-			      (car (cdr vals))))))
-     (symbol close-paren ,(semantic-lambda (list (car vals))))
-     (semantic-list close-paren
-		    ,(semantic-lambda
-		      (let ((e (read (buffer-substring (car (nth 0 vals))
-						       (cdr (nth 0 vals))))))
-			(list (symbol-name (car e)))))))
-    ;; This guys is some number of argument symbols...
-    (field-list
-     (semantic-list
-      ,(lambda (vals start end)
-	 (semantic-bovinate-from-nonterminal-full start end 'fieldsyms)
-	 )))
-    (fieldsyms
-     (semantic-list ,(semantic-lambda
-		      (let ((e (read (buffer-substring (car (nth 0 vals))
-						       (cdr (nth 0 vals))))))
-			(list (symbol-name (car e))))))
-     )
-    ;; Labels
-    (opt-label
-     (symbol "^:" ,(semantic-lambda (car vals)))
-     ())
+	 (append (semantic-elisp-use-read (car vals))
+		 (list start end)))))
     )
   "Top level bovination table for elisp.")
+
+(defun semantic-elisp-desymbolify (arglist)
+  "Convert symbols to strings for ARGLIST."
+  (let ((out nil))
+    (while arglist
+      (setq out
+	    (cons
+	     (if (symbolp (car arglist))
+		 (symbol-name (car arglist))
+	       (if (and (listp (car arglist))
+			(symbolp (car (car arglist))))
+		   (symbol-name (car (car arglist)))
+		 (format "%S" (car arglist))))
+	     out)
+	    arglist (cdr arglist)))
+    (nreverse out)))
+
+(defun semantic-elisp-use-read (sl)
+  "Use `read' on the semantic list SL.
+Return a bovination list to use."
+  (let* ((rt (read (buffer-substring (car sl) (cdr sl)))) ; read text
+	 (ts (car rt)) ; type symbol
+	 (ss (if (listp (nth 1 rt))
+		 (nth 1 (nth 1 rt))
+		(nth 1 rt))) ; symbol name
+	 (sn (format "%S" ss))
+	 )
+    (cond
+     ((or (eq ts 'defvar)
+	  (eq ts 'defconst)
+	  (eq ts 'defcustom)
+	  (eq ts 'defface)
+	  (eq ts 'defimage))
+      ;; Variables and constants
+      (list sn 'variable nil
+	    (if (eq ts 'defconst) t nil)
+	    nil nil (nth 3 rt))
+      )
+     ((or (eq ts 'defun)
+	  (eq ts 'defsubst)
+	  (eq ts 'defmacro))
+      ;; functions and macros
+      (list sn 'function nil (semantic-elisp-desymbolify (nth 2 rt))
+	    nil (nth 3 rt))
+      )
+     ((or (eq ts 'defmethod)
+	  (eq ts 'defgeneric))
+      ;; methods
+      (let* ((a2 (nth 2 rt))
+	     (a3 (nth 3 rt))
+	     (args (if (listp a2) a2 a3))
+	     (doc (nth (if (listp a2) 3 4) rt)))
+	(list sn 'function nil (semantic-elisp-desymbolify args) doc)
+	))
+     ((eq ts 'defadvice)
+      ;; Advice
+      (list sn 'function nil (semantic-elisp-desymbolify (nth 2 rt))
+	    nil (nth 3 rt)))
+     ((eq ts 'defclass)
+      ;; classes and structs (types)
+      (list sn 'type "class" (semantic-elisp-desymbolify (nth 3 rt))
+	    (semantic-elisp-desymbolify (nth 2 rt))
+	    nil (nth 4 rt))
+      )
+     ;; Now for other stuff
+     ((eq ts 'require)
+      (list sn 'include nil nil))
+     ((eq ts 'provide)
+      (list sn 'package (nth 3 rt) nil))
+     (t
+      ;; Other stuff
+      (list (symbol-name ts) 'code)
+      ))))
 
 (defun semantic-elisp-find-dependency (token)
   "Find the file BUFFER depends on described by TOKEN."
