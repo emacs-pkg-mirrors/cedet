3cf7f30c63b2fd3b7fc1038fcb1d1f8468b3a8be
zappo <zappo>
2001-03-10 01:55:34 +0000

    Added scm load. Added which-func turn on. Removed load path hacks.

--- semantic/semantic-load.el
+++ semantic/semantic-load.el
@@ -3,7 +3,7 @@
 ;;; Copyright (C) 1999, 2000, 2001 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
-;; X-RCS: $Id: semantic-load.el,v 1.1 2001/02/21 22:55:14 zappo Exp $
+;; X-RCS: $Id: semantic-load.el,v 1.2 2001/03/10 01:55:34 zappo Exp $
 
 ;; Semantic is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -26,14 +26,8 @@
 
 ;;; Code:
 
-;; Hack this into the load path.
-(if load-file-name
-    (add-to-list 'load-path (file-name-directory load-file-name)))
-
 (autoload 'semantic-bnf-mode "semantic-bnf" "Mode for Bovine Normal Form." t)
 (add-to-list 'auto-mode-alist '("\\.bnf$" . semantic-bnf-mode))
-
-(add-hook 'speedbar-load-hook (lambda () (require 'semantic-sb)))
 
 (autoload 'semantic-default-c-setup "semantic-c")
 (add-hook 'c-mode-hook 'semantic-default-c-setup)
@@ -41,6 +35,9 @@
 
 (autoload 'semantic-default-elisp-setup "semantic-el")
 (add-hook 'emacs-lisp-mode-hook 'semantic-default-elisp-setup)
+
+(autoload 'semantic-default-scheme-setup "semantic-scm")
+(add-hook 'scheme-mode-hook 'semantic-default-scheme-setup)
 
 (autoload 'semantic-default-make-setup "semantic-make")
 (add-hook 'makefile-mode-hook 'semantic-default-make-setup)
@@ -52,16 +49,18 @@
 (add-hook 'java-mode-hook 'semantic-default-java-setup)
 
 (autoload 'senator-minor-mode "senator" nil t)
-(autoload 'semantic-minor-mode "semantic-mode" nil t)
 
 (autoload 'global-semanticdb-minor-mode "semanticdb" nil t)
 (autoload 'semantic-show-dirty-mode "semantic-util" nil t)
 
 (defvar semantic-load-turn-everything-on nil
-  "Non-nil means turn on all semantic features.")
+  "Non-nil means turn on all features in the semantic package.")
 
 (when semantic-load-turn-everything-on
   (add-hook 'semantic-init-hooks 'senator-minor-mode)
+  (add-hook 'semantic-init-hooks 'turn-on-eldoc-mode)
+  (if (fboundp 'which-func-mode)
+      (add-hook 'semantic-init-hooks 'which-funct-mode))
 
   (when (eq window-system 'x)
     (add-hook 'semantic-init-hooks (lambda ()
@@ -69,7 +68,8 @@
 
   (semantic-show-dirty-mode 1)
   (global-semanticdb-minor-mode 1)
-  
+
+  (add-hook 'speedbar-load-hook (lambda () (require 'semantic-sb)))
   )
 
 (provide 'semantic-load)
