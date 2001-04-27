aaf1e9dfddeed33b63116a33f3552cdaef290d40
ponced <ponced>
2001-04-27 15:29:30 +0000

    (working-status, working-dynamic-status): No more log progress messages on Xemacs.

--- semantic/working.el
+++ semantic/working.el
@@ -229,7 +229,8 @@ macro `working-status-forms'."
 		(floor (* 100.0 (/ (float (point)) (point-max))))))
 	 (m1 (apply 'format working-message args))
 	 (m2 (funcall working-status-percentage-type (length m1) p))
-	 (message-log-max))
+	 (log-message-filter-function #'ignore) ; No logging (XEmacs)
+	 (message-log-max))             ; No logging (Emacs)
     (message "%s%s" m1 m2)))
 
 (defun working-dynamic-status (&optional number &rest args)
@@ -241,7 +242,8 @@ macro `working-status-forms'."
   (let* ((n (or number working-ref1))
 	 (m1 (apply 'format working-message args))
 	 (m2 (funcall working-status-dynamic-type (length m1) n))
-	 (message-log-max))
+	 (log-message-filter-function #'ignore) ; No logging (XEmacs)
+	 (message-log-max))             ; No logging (Emacs)
     (message "%s%s" m1 m2)
     (setq working-ref1 (1+ working-ref1))))
 
