314efabbbd3a2f67480cf41bbbf9d169fe6aba29
zappo <zappo>
2001-08-17 21:28:19 +0000

    Update version (speedbar-find-file-in-frame): fixed `other-frame' typo.

--- speedbar/speedbar.el
+++ speedbar/speedbar.el
@@ -4,9 +4,9 @@
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: file, tags, tools
-;; X-RCS: $Id: speedbar.el,v 1.204 2001/08/08 01:06:58 zappo Exp $
+;; X-RCS: $Id: speedbar.el,v 1.205 2001/08/17 21:28:19 zappo Exp $
 
-(defvar speedbar-version "0.14beta1"
+(defvar speedbar-version "0.14beta2"
   "The current version of speedbar.")
 
 ;; This file is part of GNU Emacs.
@@ -3220,7 +3220,7 @@ frame instead."
       (if dframe-power-click
 	  (let ((pop-up-frames t)) (select-window (display-buffer buff)))
 	(if (numberp speedbar-select-frame-method)
-	    (other frame speedbar-select-frame-method)
+	    (other-frame speedbar-select-frame-method)
 	  (dframe-select-attached-frame speedbar-frame))
 	(switch-to-buffer buff))))
  )
