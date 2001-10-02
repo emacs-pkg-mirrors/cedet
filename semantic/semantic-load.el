38dd84bd1dd512e56698ec35dfe14b435afa3d70
ponced <ponced>
2001-10-02 18:50:12 +0000

    Updated minor mode autoloads. Use `global-senator-minor-mode', `global-semantic-show-dirty-mode' and `global-semantic-show-unmatched-syntax-mode' to globally turn on minor modes when `semantic-load-turn-everything-on' is non-nil.

--- semantic/semantic-load.el
+++ semantic/semantic-load.el
@@ -3,7 +3,7 @@
 ;;; Copyright (C) 1999, 2000, 2001 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
-;; X-RCS: $Id: semantic-load.el,v 1.7 2001/09/27 03:29:13 zappo Exp $
+;; X-RCS: $Id: semantic-load.el,v 1.8 2001/10/02 18:50:12 ponced Exp $
 
 ;; Semantic is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -48,15 +48,100 @@
 (autoload 'semantic-default-java-setup "semantic-java")
 (add-hook 'java-mode-hook 'semantic-default-java-setup)
 
-(autoload 'senator-minor-mode "senator" nil t)
-
-(autoload 'global-semanticdb-minor-mode "semanticdb" nil t)
-(autoload 'semantic-show-dirty-mode "semantic-util" nil t)
-(autoload 'semantic-change-function-mark-dirty "semantic-util")
-(autoload 'semantic-show-unmatched-syntax-mode "semantic-util" nil t)
-
 (autoload 'semantic-chart-nonterminals-by-token "semantic-chart" nil t)
 
+
+;;; Minor modes
+;;
+
+;; semanticdb
+(autoload 'global-semanticdb-minor-mode "semanticdb" nil t)
+
+;; senator
+(autoload 'senator-minor-mode "senator"
+  "Toggle senator minor mode.
+With prefix argument ARG, turn on if positive, otherwise off.  The
+minor mode can be turned on only if semantic feature is available and
+the current buffer was set up for parsing.  Return non-nil if the
+minor mode is enabled.
+
+\\{senator-mode-map}"
+  t nil)
+
+(defvar global-senator-minor-mode nil
+  "*If non-nil enable global use of senator minor mode.")
+
+(custom-add-to-group 'senator
+                     'global-senator-minor-mode
+                     'custom-variable)
+
+(custom-add-load 'global-senator-minor-mode 'senator)
+
+(autoload 'global-senator-minor-mode "senator"
+  "Toggle global use of senator minor mode.
+If ARG is positive, enable, if it is negative, disable.
+If ARG is nil, then toggle."
+  t nil)
+
+;; semantic-show-dirty
+(autoload 'semantic-show-dirty-mode
+  "semantic-util-modes"
+  "Minor mode to display of dirty tokens.
+With prefix argument ARG, turn on if positive, otherwise off.  The
+minor mode can be turned on only if semantic feature is available and
+the current buffer was set up for parsing.  Return non-nil if the
+minor mode is enabled.
+
+\\{semantic-show-dirty-mode-map}"
+  t nil)
+
+(defvar global-semantic-show-dirty-mode nil
+  "*If non-nil enable global use of show-dirty mode.")
+
+(custom-add-to-group 'semantic
+                     'global-semantic-show-dirty-mode
+                     'custom-variable)
+
+(custom-add-load 'global-semantic-show-dirty-mode
+                 'semantic-util-modes)
+
+(autoload 'global-semantic-show-dirty-mode
+  "semantic-util-modes"
+  "Toggle global use of show-dirty mode.
+If ARG is positive, enable, if it is negative, disable.
+If ARG is nil, then toggle."
+  t nil)
+
+;; semantic-show-unmatched-syntax
+(autoload 'semantic-show-unmatched-syntax-mode
+  "semantic-util-modes"
+  "Minor mode to display of unmatched-syntax tokens.
+With prefix argument ARG, turn on if positive, otherwise off.  The
+minor mode can be turned on only if semantic feature is available and
+the current buffer was set up for parsing.  Return non-nil if the
+minor mode is enabled.
+
+\\{semantic-show-unmatched-syntax-mode-map}"
+  t nil)
+
+(defvar global-semantic-show-unmatched-syntax-mode nil
+  "*If non-nil enable global use of show-unmatched-syntax mode.")
+
+(custom-add-to-group 'semantic
+                     'global-semantic-show-unmatched-syntax-mode
+                     'custom-variable)
+
+(custom-add-load 'global-semantic-show-unmatched-syntax-mode
+                 'semantic-util-modes)
+
+(autoload 'global-semantic-show-unmatched-syntax-mode
+  "semantic-util-modes"
+  "Toggle global use of show-unmatched-syntax mode'.
+If ARG is positive, enable, if it is negative, disable.
+If ARG is nil, then toggle."
+  t nil)
+
+
 ;; This turns on semantic partial reparsing
 (add-hook 'semantic-change-hooks #'semantic-change-function-mark-dirty)
 
@@ -64,8 +149,6 @@
   "Non-nil means turn on all features in the semantic package.")
 
 (when semantic-load-turn-everything-on
-  (add-hook 'semantic-init-hooks (lambda ()
-				   (senator-minor-mode 1)))
   (add-hook 'semantic-init-hooks #'turn-on-eldoc-mode)
   (if (fboundp #'which-func-mode)
       (add-hook 'semantic-init-hooks (lambda ()
@@ -76,8 +159,9 @@
     (add-hook 'semantic-init-hooks (lambda ()
 				     (imenu-add-to-menubar "TOKENS"))))
 
-  (semantic-show-dirty-mode 1)
-  (semantic-show-unmatched-syntax-mode 1)
+  (global-semantic-show-dirty-mode 1)
+  (global-semantic-show-unmatched-syntax-mode 1)
+  (global-senator-minor-mode 1)
   (global-semanticdb-minor-mode 1)
 
   (add-hook 'speedbar-load-hook (lambda () (require 'semantic-sb)))
