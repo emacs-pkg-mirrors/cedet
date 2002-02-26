bb4d76390d957d07466aa19ede281cee5f49430c
ponced <ponced>
2002-02-26 18:50:17 +0000

    (wisent-calc-parser-tables): Renamed to `wisent-calc-automaton'. Generated from wisent-calc.wy. (wisent-calc-keywords): Removed. (wisent-calc-tokens): Generated from wisent-calc.wy. (wisent-calc-default-setup): Renamed to `wisent-calc-setup-parser'. Generated from wisent-calc.wy. (wisent-calc-number-regexp): Removed. (wisent-calc-lexer-input-stream): Removed. (wisent-calc-lexer): Removed. (wisent-calc): Use `wisent-flex' via a temporary buffer.

--- semantic/wisent/wisent-calc.el
+++ semantic/wisent/wisent-calc.el
@@ -1,12 +1,12 @@
 ;;; wisent-calc.el --- Infix notation calculator
 
-;; Copyright (C) 2001 David Ponce
+;; Copyright (C) 2001, 2002 David Ponce
 
 ;; Author: David Ponce <david@dponce.com>
 ;; Maintainer: David Ponce <david@dponce.com>
 ;; Created: 11 Sep 2001
 ;; Keywords: syntax
-;; X-RCS: $Id: wisent-calc.el,v 1.1 2001/09/12 08:19:10 ponced Exp $
+;; X-RCS: $Id: wisent-calc.el,v 1.2 2002/02/26 18:50:17 ponced Exp $
 
 ;; This file is not part of GNU Emacs.
 
@@ -30,7 +30,7 @@
 ;; This is a port of the Bison 1.28d Infix Calc sample program to the
 ;; elisp LALR parser Wisent.  It illustrates usage of operator
 ;; precedence and contextual precedence.  The grammar is generated
-;; from the BNF file wisent-calc.bnf.
+;; from the WY file wisent-calc.wy.
 ;;
 ;; To run the calculator use M-x wisent-calc and at "calc:" prompt
 ;; enter expressions separated by semicolons.  Here is a sample run of
@@ -53,125 +53,96 @@
 ;;; Code:
 (require 'wisent-bovine)
 
-(defconst wisent-calc-parser-tables
+(defconst wisent-calc-automaton
   (eval-when-compile
-  (wisent-compile-grammar
-   '((NUM NEG NL EQ PLUS MINUS MULT DIV EXP LPAREN RPAREN)
-     ((nonassoc EQ)
-      (left PLUS MINUS)
-      (left MULT DIV)
-      (left NEG)
-      (right EXP))
-     (input
-      (nil
-       (identity ""))
-      ((input line)
-       (format "%s%s" $1 $2)))
-     (line
-      ((NL)
-       (format "%s " $1))
-      ((exp NL)
-       (format "%s%s " $1 $2)))
-     (exp
-      ((NUM))
-      ((exp EQ exp)
-       (progn
-         (if
-             (not
-              (= $1 $3))
-             (message "wisent-calc error: %d != %d" $1 $3))
-         (= $1 $3)))
-      ((exp PLUS exp)
-       (+ $1 $3))
-      ((exp MINUS exp)
-       (- $1 $3))
-      ((exp MULT exp)
-       (* $1 $3))
-      ((exp DIV exp)
-       (/ $1 $3))
-      ((MINUS exp)
-       [NEG]
-       (- $2))
-      ((exp EXP exp)
-       (expt $1 $3))
-      ((LPAREN exp RPAREN)
-       (identity $2))))
-   'nil))
-"Calculator parser tables.")
-
-(defconst wisent-calc-keywords
-  nil
-  "Calculator keywords.
-Semantic BNF converter needs that this variable exists.  But it is not
-used by the calculator.")
+    ;;DO NOT EDIT! Generated from wisent-calc.wy - 2002-02-25 23:43+0100
+    (wisent-compile-grammar
+     '((NUM NL EQ PLUS MINUS MULT DIV EXP LPAREN RPAREN)
+       ((left PLUS MINUS MULT DIV NEG)
+        (right EXP)
+        (nonassoc EQ))
+       (input
+        (nil
+         (identity ""))
+        ((input line)
+         (format "%s%s" $1 $2)))
+       (line
+        ((NL)
+         (format "%s " $1))
+        ((exp NL)
+         (format "%s%s " $1 $2)))
+       (exp
+        ((NUM)
+         (string-to-number $1))
+        ((exp EQ exp)
+         (progn
+           (if
+               (not
+                (= $1 $3))
+               (message "wisent-calc error: %d != %d" $1 $3))
+           (= $1 $3)))
+        ((exp PLUS exp)
+         (+ $1 $3))
+        ((exp MINUS exp)
+         (- $1 $3))
+        ((exp MULT exp)
+         (* $1 $3))
+        ((exp DIV exp)
+         (/ $1 $3))
+        ((MINUS exp)
+         [NEG]
+         (- $2))
+        ((exp EXP exp)
+         (expt $1 $3))
+        ((LPAREN exp RPAREN)
+         (identity $2))))
+     'nil)
+    )
+  "Parser automaton.")
 
 (defconst wisent-calc-tokens
-  '((punctuation
-     (RPAREN . ")")
-     (LPAREN . "(")
-     (EXP . "^")
-     (DIV . "/")
-     (MULT . "*")
-     (MINUS . "-")
-     (PLUS . "+")
-     (EQ . "=")
-     (NL . ";"))
-    (dummy
-     (NEG . ""))
-    (literal
-     (NUM . "")))
-  "Calculator terminals.")
+  (identity
+   ;;DO NOT EDIT! Generated from wisent-calc.wy - 2002-02-25 23:43+0100
+   (wisent-flex-make-token-table
+    '(("close-paren"
+       (RPAREN . ")"))
+      ("open-paren"
+       (LPAREN . "("))
+      ("punctuation"
+       (EXP . "^")
+       (DIV . "/")
+       (MULT . "*")
+       (MINUS . "-")
+       (PLUS . "+")
+       (EQ . "=")
+       (NL . ";"))
+      ("number"
+       (NUM)))
+    '(("punctuation" string t)
+      ("close-paren" string t)
+      ("open-paren" string t)))
+   )
+  "Tokens.")
 
-(defun wisent-calc-default-setup ()
-  "Calculator setup hook.
-Semantic BNF converter needs that this function exists.  But it is not
-used by the calculator."
-  ;; Code generated from wisent-calc.bnf
-  (setq semantic-toplevel-bovine-table wisent-calc-parser-tables
-        semantic-toplevel-bovine-table-source "wisent-calc.bnf")
-  (setq semantic-flex-keywords-obarray wisent-calc-keywords)
- 
- ;; End code generated from wisent-calc.bnf
- )
-
-(defconst wisent-calc-number-regexp
-  (eval-when-compile
-    (concat "^\\("
-            "[0-9]+\\([.][0-9]*\\)?\\([eE][-+]?[0-9]+\\)?"
-            "\\|"
-            "[.][0-9]+\\([eE][-+]?[0-9]+\\)?"
-            "\\)"
-            ))
-  "Lexer regexp to match calculator numbers.")
-
-(defvar wisent-calc-lexer-input-stream nil
-  "Calculator input stream.")
-
-(defun wisent-calc-lexer ()
-  "Calculator lexer."
-  (let* ((is  (or wisent-calc-lexer-input-stream ""))
-         (i   (string-match "[^ ]" is))
-         (lex (list wisent-eoi-term))
-         k)
-    (if (not i)
-        nil
-      (setq is (substring is i))
-      (cond
-       ;; Number
-       ((string-match wisent-calc-number-regexp is)
-        (setq lex (list 'NUM (read (match-string 0 is)))
-              is  (substring is (match-end 0))))
-       ;; Punctuation
-       ((setq k (semantic-flex-token-key
-                 wisent-calc-tokens 'punctuation
-                 (string (aref is 0))))
-        (setq lex (list k (string (aref is 0)))
-              is  (substring is 1)))
-       ;; Invalid input
-       (t
-        (error "Invalid input character %s" is)))
-      (setq wisent-calc-lexer-input-stream is))
-    lex))
+(defun wisent-calc-setup-parser ()
+  "Setup buffer for parse."
+  ;;DO NOT EDIT! Generated from wisent-calc.wy - 2002-02-25 23:43+0100
+  (progn
+    (setq semantic-bovinate-toplevel-override 'wisent-bovinate-toplevel
+          semantic-toplevel-bovine-table wisent-calc-automaton
+          semantic-flex-keywords-obarray nil
+          wisent-flex-tokens-obarray wisent-calc-tokens)
+    (setq semantic-number-expression
+          (concat "\\([0-9]+\\([.][0-9]*\\)?\\([eE][-+]?[0-9]+\\)?"
+                  "\\|[.][0-9]+\\([eE][-+]?[0-9]+\\)?\\)")
+          semantic-flex-depth nil
+          semantic-flex-syntax-modifications '((?\; ".") (?\= ".")
+                                               (?\+ ".") (?\- ".")
+                                               (?\* ".") (?\/ ".")
+                                               (?\^ ".")
+                                               )
+          ))
+  )
 
 (defun wisent-calc (input)
   "Infix desktop calculator.
@@ -179,12 +150,15 @@ Parse INPUT string and output the result of computation."
   (interactive "scalc: ")
   (or (string-match ";\\s-*$" input)
       (setq input (concat input ";")))
-  (let ((wisent-calc-lexer-input-stream input))
-    (message "%s -> %s"
-             input
-             (wisent-parse wisent-calc-parser-tables
-                           #'wisent-calc-lexer
-                           #'error))))
+  (with-temp-buffer
+    (wisent-calc-setup-parser)
+    (insert input)
+    (let ((wisent-flex-istream (semantic-flex-buffer)))
+      (message "%s -> %s"
+               input
+               (wisent-parse wisent-calc-automaton
+                             #'wisent-flex
+                             #'error)))))
 
 (provide 'wisent-calc)
 
