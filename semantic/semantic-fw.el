6312e87c99e078ecf39f9de8637e926f711e043b
zappo <zappo>
2002-07-21 13:05:45 +0000

    Moved compatibility and simple functionality here from semantic.el

---
+++ semantic/semantic-fw.el
@@ -0,0 +1,320 @@
+;;; semantic-fw.el --- Framework for Semantic
+
+;;; Copyright (C) 1999, 2000, 2001, 2002 Eric M. Ludlam
+
+;; X-CVS: $Id: semantic-fw.el,v 1.1 2002/07/21 13:05:45 zappo Exp $
+
+;; This file is not part of GNU Emacs.
+
+;; Semantic is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This software is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+;; 
+;; Semantic has several core features shared across it's lex/parse/util
+;; stages.  This used to clutter semantic.el some.  These routines are all
+;; simple things that are not parser specific, but aid in making
+;; semantic flexible and compatible amongst different Emacs platforms.
+
+;;; No Requirements.
+
+;;; Code:
+;;
+;;; Compatibility
+;;
+(if (featurep 'xemacs)
+    (progn
+      (defalias 'semantic-overlay-live-p 'extent-live-p)
+      (defalias 'semantic-make-overlay 'make-extent)
+      (defalias 'semantic-overlay-put 'set-extent-property)
+      (defalias 'semantic-overlay-get 'extent-property)
+      (defalias 'semantic-overlay-delete 'delete-extent)
+      (defalias 'semantic-overlays-at
+        (lambda (pos) (extent-list nil pos pos)))
+      (defalias 'semantic-overlays-in
+	(lambda (beg end) (extent-list nil beg end)))
+      (defalias 'semantic-overlay-buffer 'extent-buffer)
+      (defalias 'semantic-overlay-start 'extent-start-position)
+      (defalias 'semantic-overlay-end 'extent-end-position)
+      (defalias 'semantic-overlay-next-change 'next-extent-change)
+      (defalias 'semantic-overlay-previous-change 'previous-extent-change)
+      (defalias 'semantic-overlay-lists
+	(lambda () (list (extent-list))))
+      (defalias 'semantic-overlay-p 'extentp)
+      (defun semantic-read-event ()
+        (let ((event (next-command-event)))
+          (if (key-press-event-p event)
+              (let ((c (event-to-character event)))
+                (if (char-equal c (quit-char))
+                    (keyboard-quit)
+                  c)))
+          event))
+      )
+  (defalias 'semantic-overlay-live-p 'overlay-buffer)
+  (defalias 'semantic-make-overlay 'make-overlay)
+  (defalias 'semantic-overlay-put 'overlay-put)
+  (defalias 'semantic-overlay-get 'overlay-get)
+  (defalias 'semantic-overlay-delete 'delete-overlay)
+  (defalias 'semantic-overlays-at 'overlays-at)
+  (defalias 'semantic-overlays-in 'overlays-in)
+  (defalias 'semantic-overlay-buffer 'overlay-buffer)
+  (defalias 'semantic-overlay-start 'overlay-start)
+  (defalias 'semantic-overlay-end 'overlay-end)
+  (defalias 'semantic-overlay-next-change 'next-overlay-change)
+  (defalias 'semantic-overlay-previous-change 'previous-overlay-change)
+  (defalias 'semantic-overlay-lists 'overlay-lists)
+  (defalias 'semantic-overlay-p 'overlayp)
+  (defalias 'semantic-read-event 'read-event)
+  )
+
+(if (and (not (featurep 'xemacs))
+	 (>= emacs-major-version 21))
+    (defalias 'semantic-make-local-hook 'identity)
+  (defalias 'semantic-make-local-hook 'make-local-hook)
+  )
+
+(defun semantic-delete-overlay-maybe (overlay)
+  "Delete OVERLAY if it is a semantic token overlay."
+  (if (semantic-overlay-get overlay 'semantic)
+      (semantic-overlay-delete overlay)))
+
+
+;;; Primitive Token access system:
+;;
+;; Raw tokens in semantic are lists.  Each token/list has a basic structure
+;; for all tokens.  This includes the first two elements, and the last 3.
+;; See `semantic-tfe-*' for details.
+;;
+;; TFE = Token From End
+
+(defconst semantic-tfe-overlay 1
+  "Amount to subtract from the length of the token to get the overlay.")
+(defconst semantic-tfe-properties 2
+  "Amount to subtract from the length of the token to get the property list.")
+(defconst semantic-tfe-docstring 3
+  "Amount to subtract from the length of the token to get the doc string.")
+(defconst semantic-tfe-number 2
+  "The number of required end elements.")
+
+(defmacro semantic-token-token (token)
+  "Retrieve from TOKEN the token identifier.
+ie, the symbol 'variable, 'function, 'type, or other."
+  `(nth 1 ,token))
+
+(defsubst semantic-token-name (token)
+  "Retrieve the name of TOKEN."
+  (car token))
+
+(defun semantic-token-docstring (token &optional buffer)
+  "Retrieve the documentation of TOKEN.
+Optional argument BUFFER indicates where to get the text from.
+If not provided, then only the POSITION can be provided."
+  (let ((p (nth (- (length token) semantic-tfe-docstring) token)))
+    (if (and p buffer)
+	(save-excursion
+	  (set-buffer buffer)
+	  (semantic-flex-text (car (semantic-lex p (1+ p)))))
+      p)))
+
+(defmacro semantic-token-properties (token)
+  "Retrieve the PROPERTIES part of TOKEN.
+The returned item is an ALIST of (KEY . VALUE) pairs."
+  `(nth (- (length ,token) semantic-tfe-properties) ,token))
+
+(defmacro semantic-token-properties-cdr (token)
+  "Retrieve the cons cell for the PROPERTIES part of TOKEN."
+  `(nthcdr (- (length ,token) semantic-tfe-properties) ,token))
+
+(defun semantic-token-put (token key value)
+  "For TOKEN, put the property KEY on it with VALUE.
+If VALUE is nil, then remove the property from TOKEN."
+  (let* ((c (semantic-token-properties-cdr token))
+	 (al (car c))
+	 (a (assoc key (car c))))
+    (if a
+	(if value
+	    (setcdr a value)
+	  (adelete 'al key)
+	  (setcar c al))
+      (if value
+	  (setcar c (cons (cons key value) (car c)))))
+    ))
+
+(defun semantic-token-put-no-side-effect (token key value)
+  "For TOKEN, put the property KEY on it with VALUE without side effects.
+If VALUE is nil, then remove the property from TOKEN.
+All cons cells in the property list are replicated so that there
+are no side effects if TOKEN is in shared lists."
+  (let* ((c (semantic-token-properties-cdr token))
+	 (al (copy-sequence (car c)))
+	 (a (assoc key (car c))))
+    ;; This removes side effects
+    (setcar c a)
+    (if a
+	(if value
+	    (setcdr a value)
+	  (adelete 'al key)
+	  (setcar c al))
+      (if value
+	  (setcar c (cons (cons key value) (car c)))))
+    ))
+
+(defsubst semantic-token-get (token key)
+  "For TOKEN, get the value for property KEY."
+  (cdr (assoc key (semantic-token-properties token))))
+
+(defmacro semantic-token-overlay (token)
+  "Retrieve the OVERLAY part of TOKEN.
+The returned item may be an overlay or an unloaded buffer representation."
+  `(nth (- (length ,token) semantic-tfe-overlay) ,token))
+
+(defmacro semantic-token-overlay-cdr (token)
+  "Retrieve the cons cell containing the OVERLAY part of TOKEN."
+  `(nthcdr (- (length ,token) semantic-tfe-overlay) ,token))
+
+(defmacro semantic-token-extent (token)
+  "Retrieve the extent (START END) of TOKEN."
+  `(let ((o (semantic-token-overlay ,token)))
+     (if (semantic-overlay-p o)
+	 (list (semantic-overlay-start o) (semantic-overlay-end o))
+       (list (aref o 0) (aref o 1)))))
+
+(defsubst semantic-token-start (token)
+  "Retrieve the start location of TOKEN."
+  (let ((o (semantic-token-overlay token)))
+    (if (semantic-overlay-p o)
+        (semantic-overlay-start o)
+      (aref o 0))))
+
+(defsubst semantic-token-end (token)
+  "Retrieve the end location of TOKEN."
+  (let ((o (semantic-token-overlay token)))
+    (if (semantic-overlay-p o)
+        (semantic-overlay-end o)
+      (aref o 1))))
+
+(defsubst semantic-token-buffer (token)
+  "Retrieve the buffer TOKEN resides in."
+  (let ((o (semantic-token-overlay token)))
+    (if (semantic-overlay-p o)
+        (semantic-overlay-buffer o)
+      ;; We have no buffer for this token (It's not in Emacs right now.)
+      nil)))
+
+(defsubst semantic-token-p (token)
+  "Return non-nil if TOKEN is most likely a semantic token."
+  (and (listp token)
+       (stringp (car token))
+       (car (cdr token))
+       (symbolp (car (cdr token)))))
+
+(defun semantic-token-with-position-p (token)
+  "Return non-nil if TOKEN is a semantic token with positional information."
+  (and (semantic-token-p token)
+       (let ((o (semantic-token-overlay token)))
+	 (or (semantic-overlay-p o)
+	     (and (arrayp o)
+		  (not (stringp o)))))))
+
+
+;;; Behavioral APIs
+;;
+;; Each major mode will want to support a specific set of behaviors.
+;; Usually generic behaviors that need just a little bit of local
+;; specifics.  This section permits the setting of override functions
+;; for tasks of that nature, and also provides reasonable defaults.
+
+(defvar semantic-override-table nil
+  "Buffer local semantic function overrides obarray.
+These overrides provide a hook for a `major-mode' to override specific
+behaviors with respect to generated semantic toplevel nonterminals and
+things that these non-terminals are useful for.  Use the function
+`semantic-install-function-overrides' to install overrides.
+
+Use `semantic-list-overrides' to get a list of override functions.")
+(make-variable-buffer-local 'semantic-override-table)
+
+(defun semantic-install-function-overrides (overrides &optional transient)
+  "Install function OVERRIDES in `semantic-override-table'.
+If optional TRANSIENT is non-nil installed overrides can in turn be
+overridden by next installation.  OVERRIDES must be an alist.  Each
+element must be of the form: (SYM . FUN) where SYM is the symbol to
+override, and FUN is the function to override it with."
+  (if (not (arrayp semantic-override-table))
+      (setq semantic-override-table (make-vector 13 nil)))
+  (let (sym sym-name fun override)
+    (while overrides
+      (setq override  (car overrides)
+            overrides (cdr overrides)
+            sym-name  (symbol-name (car override))
+            fun       (cdr override))
+      (if (setq sym (intern-soft sym-name semantic-override-table))
+          (if (get sym 'override)
+              (set sym fun)
+            (or (equal (symbol-value sym) fun)
+                (message "Current `%s' function #'%s not overrode by #'%s"
+                         sym (symbol-value sym) fun)))
+        (setq sym (intern sym-name semantic-override-table))
+        (set sym fun))
+      (put sym 'override transient))))
+
+(defun semantic-fetch-overload (sym)
+  "Find and return the overload function for SYM.
+Return nil if not found."
+  (symbol-value
+   (and (arrayp semantic-override-table)
+        (intern-soft (symbol-name sym) semantic-override-table))))
+
+(defmacro semantic-defoverload (name arguments docstring &rest code)
+  "Define a new function, as with `defun' which can be overloaded.
+NAME is the name of the function to create.  If the name is of
+the form `semantic-NAME'.  The function will strip `semantic-' from
+the front as the name of the symbol creawted.
+ARGUMENTS are the arguments to the function.
+DOCSTRING is a documentation string to describe the function.
+The docstring will automatically had details about its overload symbol
+appended to the end.
+CODE is code that would be run as a default if this method is not
+overloaded for a specific mode.
+If you want the main function of an overloadable function to do extra
+work, write that yourself without `semantic-defoverload'.  See the info
+manual for details."
+  (let* ((fastargs (delq '&rest (delq '&optional (copy-sequence arguments))))
+	 (sym (if (string-match "^semantic-" (symbol-name name))
+		  (intern (substring (symbol-name name) (match-end 0)))
+		name))
+	 (codepart
+	  (if (not code)
+	      (list (cons (intern (concat (symbol-name name) "-default"))
+			  fastargs))
+	    code))
+	 )
+    `(eval-and-compile
+       (defun ,name ,arguments
+	 ,(concat docstring "
+
+This function can be overloaded using the symbol
+`" (symbol-name sym) "'.")
+	 (let ((s (semantic-fetch-overload ',sym)))
+	   (if s
+	       (funcall s ,@fastargs)
+	     ;; Else, perform some default behaviors
+	     ,@codepart)))
+       (put ',name 'semantic-overload ',sym))))
+(put 'semantic-defoverload 'lisp-indent-function 2)
+
+(provide 'semantic-fw)
+
+;;; semantic-fw.el ends here
