fe4d3381b95b54edcf13d9ca7a1d4ee747cbbd9f
berndl <berndl>
2003-01-24 12:32:10 +0000

    Added a some new examples related to the modifications in c.bnf

--- semantic/tests/templates.cpp
+++ semantic/tests/templates.cpp
@@ -1,5 +1,23 @@
 // C++ examples and requests from Klaus Berndl
 
+// template in a unnamed namespace
+namespace
+{
+  template<typename Target, typename Source>
+  Target lexical_cast(Source arg)
+  {
+    std::stringstream interpreter;
+    Target result;
+
+    if(!(interpreter << arg) || !(interpreter >> result) ||
+       !(interpreter >> std::ws).eof())
+      throw bad_lexical_cast();
+    
+    return result;
+  }
+}
+
+
 template <class T, FOO> class Vector
 {
 private:
@@ -29,6 +47,10 @@ public:
   //...
 };
 
+// template contains namespace
+typedef vector<CzkUtilities::Action*> ActionList;
+
+
 template <class T> class Vector <T*> : private Vector <void*>
 {
 public:
@@ -39,6 +61,48 @@ public:
 	T*& elem(int i) {return static_cast<T*&>(Base::elem(i));}
 	//...
 };
+
+// outside method implementation of a template-class
+template<class T> T*& Vector<T*>::elem(int i)
+{
+  return C;
+}
+
+// same but qualified with a namespace Testnamespace
+template<class T> T*& Testnamespace::Vector<T*>::elem(int i)
+{
+  return C;
+}
+
+// function templates with keyword typename
+template<typename Target, typename Source>
+Target lexical_cast(Source arg)
+{
+  std::stringstream interpreter;
+  Target result;
+
+  if(!(interpreter << arg) || !(interpreter >> result) ||
+     !(interpreter >> std::ws).eof())
+    throw bad_lexical_cast();
+    
+  return result;
+}
+
+template<class T>
+static
+typename T::_ptr_type
+getService(const std::string& pServiceName, const int pRetries=20)
+{
+  return T::_narrow(getServiceObject(pServiceName, pRetries));
+}
+
+// function template declaration
+template<class T> void sort(vector<T>&);
+// function template definition
+template<class T, class S, int i> void sort(vector<T>& v)
+{
+  return;
+}
 
 
 class SomeName;
