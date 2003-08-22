cfdfd47c3d2ef455612c2552a9c047981dc5c779
zappo <zappo>
2003-08-22 18:18:27 +0000

    (semantic-grammar): move require. (semantic-ede-proj-target-grammar): autoload cookie (semantic-ede-grammar-compiler): Remove extra rules.  Use one single batch of actions. (ede-compile-intermediate-objects-p, ede-compiler-intermediate-object-variable): Removed. (ede-proj-makefile-insert-variables): changed extra variable to be the .el files. (ede-proj-makefile-insert-rules): Removed intermediate rules used before. (end of file): Fixed provide statement, and method for adding this project class.

--- semantic/semantic-ede-grammar.el
+++ semantic/semantic-ede-grammar.el
@@ -4,7 +4,7 @@
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: project, make
-;; RCS: $Id: semantic-ede-grammar.el,v 1.1 2003/08/17 02:48:05 zappo Exp $
+;; RCS: $Id: semantic-ede-grammar.el,v 1.2 2003/08/22 18:18:27 zappo Exp $
 
 ;; This software is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -29,9 +29,9 @@
 (require 'ede-pmake)
 (require 'ede-pconf)
 (require 'ede-proj-elisp)
-(require 'semantic-grammar)
 
 ;;; Code:
+;;;###autoload
 (defclass semantic-ede-proj-target-grammar (ede-proj-target-makefile)
   ((menu :initform nil)
    (keybindings :initform nil)
@@ -60,55 +60,23 @@ parsing different languages.")
    :name "emacs"
    :variables '(("EMACS" . "emacs"))
    :commands
-   '("@")
+   '(
+     "@echo \"(add-to-list 'load-path nil)\" > grammar-make-script"
+     "@for loadpath in . ${LOADPATH}; do \\"
+     "   echo \"(add-to-list 'load-path \\\"$$loadpath\\\")\" >> grammar-make-script; \\"
+     "done;"
+     "@echo \"(require 'semantic-load)\" >> grammar-make-script"
+     "@echo \"(require 'semantic-grammar)\" >> grammar-make-script"
+     ;; "@echo \"(setq debug-on-error t)\" >> grammar-make-script"
+     "${EMACS} -batch -q -l grammar-make-script -f semantic-grammar-batch-build-packages $^"
+     )
    ;; :autoconf '("AM_PATH_LISPDIR")
    :sourcetype '(semantic-ede-source-grammar)
    :objectextention "-wy.elc"
-   :rules (list (ede-makefile-rule
-		 "wisent-inference-rule"
-		 :target "%-wy.el"
-		 :dependencies "%.wy"
-		 :rules
-		 '(
-		   "@echo \"(add-to-list 'load-path nil)\" > grammar-make-script"
-		   "@for loadpath in . ${LOADPATH}; do \\"
-		   "   echo \"(add-to-list 'load-path \\\"$$loadpath\\\")\" >> grammar-make-script; \\"
-		   "done;"
-		   "@echo \"(require 'semantic-load)\" >> grammar-make-script"
-		   "@echo \"(require 'semantic-grammar)\" >> grammar-make-script"
-		   "@echo \"(setq debug-on-error t)\" >> grammar-make-script"
-		   "${EMACS} -batch -q -l wisent-make-script $< -f semantic-grammar-create-package -f save-buffer"
-		   )
-		 )
-		(ede-makefile-rule
-		 "wisent-inference-emacslisp-rule"
-		 :target "%-wy.elc"
-		 :dependencies "%-wy.el"
-		 :rules
-		 '(
-		   "@echo \"(add-to-list 'load-path nil)\" > $@-compile-script"
-		   "@for loadpath in . ${LOADPATH}; do \\"
-		   "   echo \"(add-to-list 'load-path \\\"$$loadpath\\\")\" >> $@-compile-script; \\"
-		   "done;"
-		   "@echo \"(setq debug-on-error t)\" >> $@-compile-script"
-		   "$(EMACS) -batch -l $@-compile-script -f batch-byte-compile $^"
-		   )
-		 )
-		)
    )
   "Compile Emacs Lisp programs.")
 
-;;; Specialized compiler options
-(defmethod ede-compiler-intermediate-objects-p
-  ((this semantic-ede-grammar-compiler-class))
-  "We have intermediate files for this class of target."
-  t)
-
-(defmethod ede-compiler-intermediate-object-variable ((this semantic-ede-grammar-compiler-class)
-						      targetname)
-  "Return a string based on THIS representing a make object variable.
-TARGETNAME is the name of the target that these objects belong to."
-  (concat targetname "_SEMANTIC_GRAMMAR_ELC"))
+(require 'semantic-grammar)
 
 ;;; Target options.
 (defmethod project-compile-target ((obj semantic-ede-proj-target-grammar))
@@ -141,12 +109,12 @@ TARGETNAME is the name of the target that these objects belong to."
 		       " "))
     )
   (ede-pmake-insert-variable-shared
-      (concat (ede-pmake-varname this) "_SAMENATIC_GRAMMAR_ELC")
+      (concat (ede-pmake-varname this) "_SAMENATIC_GRAMMAR_EL")
     (insert
      (mapconcat (lambda (src)
 		  (save-excursion
 		    (set-buffer (find-file-noselect src))
-		    (concat (semantic-grammar-package) ".elc")))
+		    (concat (semantic-grammar-package) ".el")))
 		(oref this source)
 		" ")))
   )
@@ -154,12 +122,12 @@ TARGETNAME is the name of the target that these objects belong to."
 (defmethod ede-proj-makefile-insert-rules ((this semantic-ede-proj-target-grammar))
   "Insert rules needed by THIS target."
   ;; Add in some dependencies.
-  (mapc (lambda (src)
-	  (let ((nm (file-name-sans-extension src)))
-	    (insert nm "-wy.el: " src "\n"
-		    nm "-wy.elc: " nm "-wy.el\n\n")
-	    ))
-	(oref this source))
+;;  (mapc (lambda (src)
+;;	  (let ((nm (file-name-sans-extension src)))
+;;	    (insert nm "-wy.el: " src "\n"
+;;		    nm "-wy.elc: " nm "-wy.el\n\n")
+;;	    ))
+;;	(oref this source))
   ;; Call the normal insertion of rules.
   (call-next-method)
   )
@@ -171,10 +139,12 @@ TARGETNAME is the name of the target that these objects belong to."
 ;;;###autoload
 (eval-after-load "ede-proj"
     (quote
-     (ede-proj-register-target "semantic grammar"
-			       semantic-ede-proj-target-grammar)
+     (require 'semantic-ede-grammar)
      ))
 
-(provide 'ede-proj-elisp)
+(ede-proj-register-target "semantic grammar"
+			  semantic-ede-proj-target-grammar)
 
-;;; ede-proj-elisp.el ends here
+(provide 'semantic-ede-grammar)
+
+;;; semantic-ede-grammar.el ends here
