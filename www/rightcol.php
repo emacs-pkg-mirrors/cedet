e2cb23c97c5d485060bb0acfcd5d72647b199ac7
zappo <zappo>
2003-10-23 01:47:37 +0000

    Revamped to look sort of like speedbar.

--- www/rightcol.php
+++ www/rightcol.php
@@ -1,46 +1,27 @@
 <body bgcolor=white text=black>
 
-<table width=100 align=right border=0 cellspacing=0 cellpadding=0 bgcolor=cyan>
-<tr><td>
-<table>
-
-<tr><td align=center>&nbsp</td></tr>
+<tt>
+<table border=2 align=right><tr><td>
+<table bgcolor=white>
 
 <tr><td align=center>&nbsp<a href=/     ><b><font size=+1>CEDET</font></b></a>&nbsp</td></tr>
 
-<tr><td align=center>&nbsp</td></tr>
-
-<tr><td align=center>&nbsp<a href=http://www.sourceforge.net/projects/cedet><b>*Project*</b></a>&nbsp</td></tr>
-<tr><td align=center>&nbsp<a href=http://sourceforge.net/project/showfiles.php?group_id=17886><b>*Releases*</b></a>&nbsp</td></tr>
-<tr><td align=center>&nbsp<a href=http://sourceforge.net/mail/?group_id=17886><b>*MailingLists*</b></a>&nbsp</td></tr>
-<tr><td align=center>&nbsp<a href=http://sourceforge.net/projects/rte><b>*RPMs*</b></a>&nbsp</td></tr>
-<tr><td align=center>&nbsp<a href=ftpgate.shtml><font size=-1>*Files*</font></a>&nbsp</td></tr>
-
-<tr><td align=center>&nbsp</td></tr>
-</table>
-
-</td><td>
-
-<table>
-<tr><td align=center bgcolor=lightgreen>&nbsp <u><b>Libraries</b></u>&nbsp</td></tr>
-<tr><td align=center>&nbsp<a href=eieio.shtml   ><b>EIEIO    </b></a>&nbsp</td></tr>
-<tr><td align=center>&nbsp<a href=semantic.shtml><b>Semantic </b></a>&nbsp</td></tr>
-
-<!-- <tr><td align=center>&nbsp</td></tr> -->
-
-<tr><td align=center bgcolor=lightgreen>&nbsp <u><b>Tools    </b></u>&nbsp</td></tr>
-<tr><td align=center>&nbsp<a href=speedbar.shtml><b>Speedbar </b></a>&nbsp</td></tr>
-<tr><td align=center>&nbsp<a href=ede.shtml     ><b>EDE      </b></a>&nbsp</td></tr>
-<tr><td align=center>&nbsp<a href=cogre.shtml   ><b>COGRE    </b></a>&nbsp</td></tr>
-
-<!-- <tr><td align=center>&nbsp</td></tr> -->
-
-<tr><td align=center bgcolor=lightgreen>&nbsp <u><b>Other   </b></u>&nbsp</td></tr>
-<tr><td align=center>&nbsp<a href=checkdoc.shtml><b>Checkdoc</b></a>&nbsp</td></tr>
-
-<!-- <tr><td align=center>&nbsp</td></tr> -->
+<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp<u><b>Libraries</b></u>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=eieio.shtml   ><b>EIEIO    </b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=semantic.shtml><b>Semantic </b></a>&nbsp</td></tr>
+<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <u><b>Tools    </b></u>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=speedbar.shtml><b>Speedbar </b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=ede.shtml     ><b>EDE      </b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=cogre.shtml   ><b>COGRE    </b></a>&nbsp</td></tr>
+<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <u><b>Other   </b></u>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=checkdoc.shtml><b>Checkdoc</b></a>&nbsp</td></tr>
+<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <u><b>Project Info </b></u>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://www.sourceforge.net/projects/cedet><b>Project</b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://sourceforge.net/project/showfiles.php?group_id=17886><b>Releases</b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://sourceforge.net/mail/?group_id=17886><b>Mailing Lists</b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://sourceforge.net/projects/rte><b>RPMs</b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a href=ftpgate.shtml><font size=-1>Files</font></a>&nbsp</td></tr>
 
 </table>
-</td></tr>
-
-</table>
+</td></tr></table>
+</tt>
