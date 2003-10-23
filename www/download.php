63508784fa5e197022bba68d681358a260effe2e
zappo <zappo>
2003-10-23 01:48:14 +0000

    new generic download section for all pages.

---
+++ www/download.php
@@ -0,0 +1,14 @@
+<h2>Downloading CEDET</h2>
+
+<p>CEDET tools including EIEIO, Semantic, Speedbar, EDE, and COGRE are
+now distributed together in a single file.  This simplifies
+installation and version management.
+
+<p>While Some individual CEDET packages have active stable releases, the
+CEDET bundle is currently only available in Beta.  CEDET Betas are
+the only place to get the latest versions of individual tools.
+
+<p>Try out
+<a href="http://sourceforge.net/project/showfiles.php?group_id=17886">
+cedet-1.0beta1.tar.gz</a>.
+
