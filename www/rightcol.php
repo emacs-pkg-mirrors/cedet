9bbd03bbde86ac513043db1bfc7711de4a2391d2
zappo <zappo>
2003-10-25 01:49:22 +0000

    Removed underlining of title parts.

--- www/rightcol.php
+++ www/rightcol.php
@@ -6,16 +6,16 @@
 
 <tr><td align=center>&nbsp<a href=/     ><b><font size=+1>CEDET</font></b></a>&nbsp</td></tr>
 
-<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp<u><b>Libraries</b></u>&nbsp</td></tr>
+<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp<b>Libraries</b>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a href=eieio.shtml   ><b>EIEIO    </b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a href=semantic.shtml><b>Semantic </b></a>&nbsp</td></tr>
-<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <u><b>Tools    </b></u>&nbsp</td></tr>
+<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <b>Tools    </b>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a href=speedbar.shtml><b>Speedbar </b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a href=ede.shtml     ><b>EDE      </b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a href=cogre.shtml   ><b>COGRE    </b></a>&nbsp</td></tr>
-<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <u><b>Other   </b></u>&nbsp</td></tr>
+<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <b>Other   </b>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a href=checkdoc.shtml><b>Checkdoc</b></a>&nbsp</td></tr>
-<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <u><b>Project Info </b></u>&nbsp</td></tr>
+<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <b>Project Info </b>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://www.sourceforge.net/projects/cedet><b>Project</b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://sourceforge.net/project/showfiles.php?group_id=17886><b>Releases</b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://sourceforge.net/mail/?group_id=17886><b>Mailing Lists</b></a>&nbsp</td></tr>
