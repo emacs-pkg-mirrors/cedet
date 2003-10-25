00f8cda3749bd7595c73cd053cd4a0e21c8e3b2e
zappo <zappo>
2003-10-25 03:08:11 +0000

    Added Fancy hover features to URLs.

--- www/rightcol.php
+++ www/rightcol.php
@@ -1,4 +1,13 @@
 <body bgcolor=white text=black>
+<style>
+<!--
+A:visited { color: #000070 ; text-decoration: none }
+A:link { color: #0000B0 ; text-decoration: none }
+A:active { color: red ; text-decoration: underline overline }
+A[href]:hover.SB { background-color: orange ; text-decoration: none}
+A[href]:hover { text-decoration: underline overline }
+-->
+</style>
 
 <tt>
 <table border=2 align=right><tr><td>
@@ -7,19 +16,19 @@
 <tr><td align=center>&nbsp<a href=/     ><b><font size=+1>CEDET</font></b></a>&nbsp</td></tr>
 
 <tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp<b>Libraries</b>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a href=eieio.shtml   ><b>EIEIO    </b></a>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a href=semantic.shtml><b>Semantic </b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=eieio.shtml   ><b>EIEIO    </b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=semantic.shtml><b>Semantic </b></a>&nbsp</td></tr>
 <tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <b>Tools    </b>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a href=speedbar.shtml><b>Speedbar </b></a>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a href=ede.shtml     ><b>EDE      </b></a>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a href=cogre.shtml   ><b>COGRE    </b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=speedbar.shtml><b>Speedbar </b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=ede.shtml     ><b>EDE      </b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=cogre.shtml   ><b>COGRE    </b></a>&nbsp</td></tr>
 <tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <b>Other   </b>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a href=checkdoc.shtml><b>Checkdoc</b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=checkdoc.shtml><b>Checkdoc</b></a>&nbsp</td></tr>
 <tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <b>Project Info </b>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://www.sourceforge.net/projects/cedet><b>Project</b></a>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://sourceforge.net/project/showfiles.php?group_id=17886><b>Releases</b></a>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://sourceforge.net/mail/?group_id=17886><b>Mailing Lists</b></a>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a href=http://sourceforge.net/projects/rte><b>RPMs</b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=http://www.sourceforge.net/projects/cedet><b>Project</b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=http://sourceforge.net/project/showfiles.php?group_id=17886><b>Releases</b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=http://sourceforge.net/mail/?group_id=17886><b>Mailing Lists</b></a>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=http://sourceforge.net/projects/rte><b>RPMs</b></a>&nbsp</td></tr>
 <tr><td >&nbsp</td></tr>
 
 <tr><td bgcolor=lightgreen>&lt;&lt;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=ftpgate.shtml>Files</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&gt;&gt;</td></tr>
