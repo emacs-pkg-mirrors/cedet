22b020443c0ea5ec956caa36d362b3eb8929d35e
zappo <zappo>
2004-02-16 03:41:36 +0000

    Part of new format for web pages.

---
+++ www/pagestart.php
@@ -0,0 +1,9 @@
+<!-- -*- html -*- -->
+<table><tr><td width="90%"><img src="cedetlogo.png" ></td>
+<td><img src="writecode.png" align=right></td></tr></table>
+<table width=100% bgcolor="lightgreen"><tr><td><font size=-2> &nbsp </font></td></tr></table>
+
+<table border=0>
+<tr><td valign=top>
+<?php include ("rightcol.php") ?></td>
+<td valign=top>
