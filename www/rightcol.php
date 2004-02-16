aae48f1f2edac5abae292d64a7b23feff8d808f5
zappo <zappo>
2004-02-16 03:42:21 +0000

    Added download section.

--- www/rightcol.php
+++ www/rightcol.php
@@ -1,3 +1,4 @@
+<!-- -*- html -*- -->
 <body bgcolor=white text=black>
 <style>
 <!--
@@ -11,19 +12,26 @@ A[href]:hover { text-decoration: underline overline }
 
 <tt>
 <table border=2 align=right><tr><td>
-<table bgcolor=white>
+<table bgcolor=white cellspacing=0 cellpadding=1>
 
-<tr><td align=center>&nbsp<a href=/     ><b><font size=+1>CEDET</font></b></a>&nbsp</td></tr>
+<tr><td align=center>&nbsp<a href="/"    ><b><font size=+1>CEDET</font></b></a>&nbsp</td></tr>
 
 <tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp<b>Libraries</b>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=eieio.shtml   ><b>EIEIO    </b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=semantic.shtml><b>Semantic </b></a>&nbsp</td></tr>
+
 <tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <b>Tools    </b>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=speedbar.shtml><b>Speedbar </b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=ede.shtml     ><b>EDE      </b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=cogre.shtml   ><b>COGRE    </b></a>&nbsp</td></tr>
+
+<tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <b>Download</b>&nbsp</td></tr>
+<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=192965l><b>1.0beta1c</b></a>&nbsp</td></tr>
+<tr><td >&nbsp</td></tr>
+
 <tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <b>Other   </b>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=checkdoc.shtml><b>Checkdoc</b></a>&nbsp</td></tr>
+
 <tr><td  bgcolor=lightgreen><img src=dir-minus.gif>&nbsp <b>Project Info </b>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=http://www.sourceforge.net/projects/cedet><b>Project</b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href=http://sourceforge.net/project/showfiles.php?group_id=17886><b>Releases</b></a>&nbsp</td></tr>
