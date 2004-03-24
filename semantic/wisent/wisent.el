9c6e14fd25cbd80dab848ef7d0c6d29963b82f44
ponced <ponced>
2004-03-24 13:34:31 +0000

    (wisent-char-p): Move here from wisent-comp.el. (wisent-escape-sequence-strings): New constant. (wisent-region) (wisent-pre-parse-hook, wisent-post-parse-hook) (wisent-loop): Move declaration. (wisent-parse-verbose-flag): New variable. (wisent-item-to-string, wisent-token-to-string) (wisent-parse-toggle-verbose-flag) (wisent-message): New function. (wisent-parse-max-stack-size) (wisent-parse-max-recover) (wisent-parse-lexer-function) (wisent-parse-error-function) (wisent-input, wisent-recovering) (wisent-errok, wisent-clearin, wisent-abort) (wisent-parse-action): Doc fix. ($region, $nterm): Remove default value and doc string. ($action): New. (wisent-skip-token): No more inlined. Doc fix. Inform about skipped token. (wisent-skip-block): Doc fix.  Return nil.  Inform about skipped tokens. (wisent-parse): Use `wisent-message' as default error reporting function.  Report expected tokens on syntax error.  Inform about skipped tokens.  Simplify (and fix?) error recovery code.

--- semantic/wisent/wisent.el
+++ semantic/wisent/wisent.el
@@ -1,12 +1,12 @@
 ;;; wisent.el --- GNU Bison for Emacs - Runtime
 
-;; Copyright (C) 2002, 2003 David Ponce
+;; Copyright (C) 2002, 2003, 2004 David Ponce
 
 ;; Author: David Ponce <david@dponce.com>
 ;; Maintainer: David Ponce <david@dponce.com>
-;; Created: 30 Janvier 2002
+;; Created: 30 January 2002
 ;; Keywords: syntax
-;; X-RCS: $Id: wisent.el,v 1.28 2003/06/29 12:48:42 ponced Exp $
+;; X-RCS: $Id: wisent.el,v 1.29 2004/03/24 13:34:31 ponced Exp $
 
 ;; This file is not part of GNU Emacs.
 
@@ -54,29 +54,61 @@
               ` ~ ,¨
                `\"\""
   :group 'semantic)
-
-;;;; ----------------
-;;;; Common constants
-;;;; ----------------
 
-;; Special terminal symbols
+
+;;;; -------------
+;;;; Runtime stuff
+;;;; -------------
+
+;;; Compatibility
+(if (fboundp 'char-valid-p)
+    (defalias 'wisent-char-p 'char-valid-p)
+  (defalias 'wisent-char-p 'char-or-char-int-p))
+
+;;; Printed representation of terminals and nonterminals
+(defconst wisent-escape-sequence-strings
+  '(
+    (?\a . "'\\a'")                     ; C-g
+    (?\b . "'\\b'")                     ; backspace, BS, C-h
+    (?\t . "'\\t'")                     ; tab, TAB, C-i
+    (?\n  . "'\\n'")                    ; newline, C-j
+    (?\v . "'\\v'")                     ; vertical tab, C-k
+    (?\f . "'\\f'")                     ; formfeed character, C-l
+    (?\r . "'\\r'")                     ; carriage return, RET, C-m
+    (?\e . "'\\e'")                     ; escape character, ESC, C-[
+    (?\\ . "'\\'")                      ; backslash character, \
+    (?\d . "'\\d'")                     ; delete character, DEL
+    )
+  "Printed representation of usual escape sequences.")
+
+(defsubst wisent-item-to-string (item)
+  "Return a printed representation of ITEM.
+ITEM can be a nonterminal or terminal symbol, or a character literal."
+  (if (wisent-char-p item)
+        (or (cdr (assq item wisent-escape-sequence-strings))
+            (format "'%c'" item))
+    (symbol-name item)))
+
+(defsubst wisent-token-to-string (token)
+  "Return a printed representation of lexical token TOKEN."
+  (format "%s%s(%S)" (wisent-item-to-string (car token))
+          (if (nth 2 token) (format "@%s" (nth 2 token)) "")
+          (nth 1 token)))
+
+;;; Special symbols
 (defconst wisent-eoi-term '$EOI
   "End Of Input token.")
 
 (defconst wisent-error-term 'error
   "Error recovery token.")
 
-;; Special parser action tags
 (defconst wisent-accept-tag 'accept
   "Accept result after input successfully parsed.")
 
 (defconst wisent-error-tag 'error
   "Process a syntax error.")
-
-;;;; --------------
-;;;; Runtime stuff.
-;;;; --------------
 
+;;; Special functions
 (defun wisent-automaton-p (obj)
   "Return non-nil if OBJ is a LALR automaton.
 If OBJ is a symbol check its value."
@@ -86,18 +118,47 @@ If OBJ is a symbol check its value."
        (vectorp (aref obj 0)) (vectorp (aref obj 1))
        (= (length (aref obj 0)) (length (aref obj 1)))
        (listp (aref obj 2)) (vectorp (aref obj 3))))
+
+(defsubst wisent-region (&rest positions)
+  "Return the start/end positions of the region including POSITIONS.
+Each element of POSITIONS is a pair (START-POS . END-POS) or nil.  The
+returned value is the pair (MIN-START-POS . MAX-END-POS) or nil if no
+POSITIONS are available."
+  (let ((pl (delq nil positions)))
+    (if pl
+        (cons (apply #'min (mapcar #'car pl))
+              (apply #'max (mapcar #'cdr pl))))))
+
+;;; Reporting
+(defvar wisent-parse-verbose-flag nil
+  "*non-nil means to issue more messages while parsing.")
+
+;;;###autoload
+(defun wisent-parse-toggle-verbose-flag ()
+  "Toggle whether to issue more messages while parsing."
+  (interactive)
+  (setq wisent-parse-verbose-flag (not wisent-parse-verbose-flag))
+  (when (interactive-p)
+    (message "More messages while parsing %sabled"
+             (if wisent-parse-verbose-flag "en" "dis"))))
+
+(defsubst wisent-message (string &rest args)
+  "Print a one-line message if `wisent-parse-verbose-flag' is set.
+Pass STRING and ARGS arguments to `message'."
+  (and wisent-parse-verbose-flag
+       (apply 'message string args)))
 
 ;;;; --------------------
 ;;;; The LR parser engine
 ;;;; --------------------
 
 (defcustom wisent-parse-max-stack-size 500
-  "*The parser stack size."
+  "The parser stack size."
   :type 'integer
   :group 'wisent)
 
 (defcustom wisent-parse-max-recover 3
-  "*Number of tokens to shift before turning off error status."
+  "Number of tokens to shift before turning off error status."
   :type 'integer
   :group 'wisent)
 
@@ -112,6 +173,17 @@ semantic action calls the function `wisent-skip-token' or
 For language specific hooks, make sure you define this as a local
 hook.")
 
+(defvar wisent-pre-parse-hook nil
+  "Normal hook run just before entering the LR parser engine.")
+
+(defvar wisent-post-parse-hook nil
+  "Normal hook run just after the LR parser engine terminated.")
+
+(defvar wisent-loop nil
+  "The current parser action.
+Stop parsing when set to nil.
+This variable only has meaning in the scope of `wisent-parse'.")
+
 (defvar wisent-nerrs nil
   "The number of parse errors encountered so far.")
 
@@ -120,61 +192,53 @@ hook.")
 This value is non-nil if the parser terminated because of an
 unrecoverable error.")
 
-;;;; Internal stuff
-
-;; The current parser action.  Stop parsing when set to nil.
-(defvar wisent-loop nil)
-
-;;;; Variables and macros that are useful in grammar actions.
-
+;; Variables and macros that are useful in semantic actions.
 (defvar wisent-parse-lexer-function nil
   "The user supplied lexer function.
-This function don't have arguments.  The actual value of this variable
-is local to the parser.")
+This function don't have arguments.
+This variable only has meaning in the scope of `wisent-parse'.")
 
 (defvar wisent-parse-error-function nil
   "The user supplied error function.
-This function must accept one argument, a message string.  The actual
-value of this variable is local to the parser.")
+This function must accept one argument, a message string.
+This variable only has meaning in the scope of `wisent-parse'.")
 
 (defvar wisent-input nil
   "The last token read.
-The actual value of this variable is local to the parser.")
+This variable only has meaning in the scope of `wisent-parse'.")
 
 (defvar wisent-recovering nil
-  "Non nil when the parser is recovering.
-The actual value of this variable is local to the parser.")
+  "non-nil means that the parser is recovering.
+This variable only has meaning in the scope of `wisent-parse'.")
 
-(defvar $region nil
-  "Unused global definition to avoid compiler warnings.
-The actual value of this variable is local to each semantic action.")
-
-(defvar $nterm nil
-  "Unused global definition to avoid compiler warnings.
-The actual value of this variable is local to each semantic action.")
+;; Variables that only have meaning in the scope of a semantic action.
+;; These global definitions avoid byte-compiler warnings.
+(defvar $region)
+(defvar $nterm)
+(defvar $action)
 
 (defmacro wisent-lexer ()
   "Obtain the next terminal in input."
   '(funcall wisent-parse-lexer-function))
 
 (defmacro wisent-error (msg)
-  "Call the user supplied error reporting function with messsage MSG."
+  "Call the user supplied error reporting function with message MSG."
   `(funcall wisent-parse-error-function ,msg))
 
 (defmacro wisent-errok ()
   "Resume generating error messages immediately for subsequent syntax errors.
-This is useful primarily in error rules."
+This is useful primarily in error recovery semantic actions."
   '(setq wisent-recovering nil))
 
 (defmacro wisent-clearin ()
-  "Discard the current look-ahead token.
-This will cause a new token to be read.
-This is useful primarily in error rules."
+  "Discard the current lookahead token.
+This will cause a new lexical token to be read.
+This is useful primarily in error recovery semantic actions."
   '(setq wisent-input nil))
 
 (defmacro wisent-abort ()
-  "Abort parsing and save the look-ahead token.
-This is useful primarily in error rules."
+  "Abort parsing and save the lookahead token.
+This is useful primarily in error recovery semantic actions."
   '(setq wisent-lookahead wisent-input
          wisent-loop nil))
 
@@ -187,74 +251,64 @@ is set to nil."
                       (number-or-marker-p ,end)
                       (cons ,start ,end))))
 
-(defsubst wisent-skip-token ()
-  "Skip an invalid token and try to continue parsing.
-To be used in grammar recovery actions."
-  (wisent-error (format "Skipping invalid '%s'" $nterm))
-  ;; Clear the lookahead token
+(defun wisent-skip-token ()
+  "Skip the lookahead token in order to resume parsing.
+Return nil.
+Must be used in error recovery semantic actions."
   (if (eq (car wisent-input) wisent-eoi-term)
-      ;; does nothing at EOI to avoid infinite recovery loop
+      ;; Does nothing at EOI to avoid infinite recovery loop.
       nil
+    (wisent-message "%s: skip %s" $action
+                    (wisent-token-to-string wisent-input))
     (run-hook-with-args
      'wisent-discarding-token-functions wisent-input)
     (wisent-clearin)
     (wisent-errok)))
 
 (defun wisent-skip-block ()
-  "Safely skip a parenthesized block and try to continue parsing.
-To be used in grammar recovery actions."
+  "Safely skip a parenthesized block in order to resume parsing.
+Return nil.
+Must be used in error recovery semantic actions."
   (let ((start (car $region))
         end input block)
     (if (not (number-or-marker-p start))
-        ;; No nonterminal region available, skip
-        ;; token and try to continue?
+        ;; No nonterminal region available, skip the lookahead token.
         (wisent-skip-token)
-
-      ;; Try to skip a block
+      ;; Try to skip a block.
       (if (not (setq end (save-excursion
                            (goto-char start)
                            (and (looking-at "\\s(")
                                 (condition-case nil
                                     (1- (scan-lists (point) 1 0))
                                   (error nil))))))
-          ;; Not actually a block, skip token and try to continue?
+          ;; Not actually a block, skip the lookahead token.
           (wisent-skip-token)
-        
-        ;; Ok to safely skip the block
-        (wisent-error (format "Skipping invalid '%s' from %s to %s"
-                              $nterm start end))
-        ;; read input until matching close paren or EOI
+        ;; OK to safely skip the block, so read input until a matching
+        ;; close paren or EOI is encountered.
         (setq input wisent-input)
         (while (and (not (eq (car input) wisent-eoi-term))
                     (< (nth 2 input) end))
           (run-hook-with-args
            'wisent-discarding-token-functions input)
           (setq input (wisent-lexer)))
-        ;; Clear the lookahead token
+        (wisent-message "%s: in enclosing block, skip from %s to %s"
+                        $action
+                        (wisent-token-to-string wisent-input)
+                        (wisent-token-to-string input))
         (if (eq (car wisent-input) wisent-eoi-term)
-            ;; does nothing at EOI to avoid infinite recovery loop
+            ;; Does nothing at EOI to avoid infinite recovery loop.
             nil
           (wisent-clearin)
           (wisent-errok))
-        ;; Return a nil value with adjusted start/end positions
-        (cons nil (wisent-set-region start (1+ end)))))))
+        ;; Adjust $region to block bounds.
+        (wisent-set-region start (1+ end))
+        nil))))
 
-;;;; Other parser stuff
-
-(defsubst wisent-region (&rest positions)
-  "Return the start/end positions of the region including POSITIONS.
-Each element of POSITIONS is a pair (START-POS . END-POS) or nil.  The
-returned value is the pair (MIN-START-POS . MAX-END-POS) or nil if no
-POSITIONS are available."
-  (let ((pl (delq nil positions)))
-    (if pl
-        (cons (apply #'min (mapcar #'car pl))
-              (apply #'max (mapcar #'cdr pl))))))
-
+;;; Core parser engine
 (defmacro wisent-parse-action (i al)
   "Return the next parser action.
 I is a token item number and AL is the list of (item . action)
-availables at current state.  The first element of AL contains the
+available at current state.  The first element of AL contains the
 default action for this state."
   `(cdr (or (assq ,i ,al) (car ,al))))
 
@@ -274,12 +328,6 @@ automaton has only one entry point."
           (list token (symbol-name token))
         (error "Invalid start symbol %s" start)))))
 
-(defvar wisent-pre-parse-hook nil
-  "Normal hook run just before entering the LR parser engine.")
-
-(defvar wisent-post-parse-hook nil
-  "Normal hook run just after the LR parser engine terminated.")
-
 (defun wisent-parse (automaton lexer &optional error start)
   "Parse input using the automaton specified in AUTOMATON.
 
@@ -291,7 +339,7 @@ automaton has only one entry point."
 
 - ERROR is an optional reporting function called when a parse error
   occurs.  It receives a message string to report.  It defaults to the
-  function `error'.
+  function `wisent-message'.
 
 - START specify the start symbol (nonterminal) used by the parser as
   its goal.  It defaults to the start symbol defined in the grammar
@@ -303,7 +351,7 @@ automaton has only one entry point."
          (stack (make-vector wisent-parse-max-stack-size nil))
          (sp 0)
          (wisent-loop t)
-         (wisent-parse-error-function (or error #'error))
+         (wisent-parse-error-function (or error 'wisent-message))
          (wisent-parse-lexer-function lexer)
          (wisent-recovering nil)
          (wisent-input (wisent-parse-start start starts))
@@ -317,8 +365,8 @@ automaton has only one entry point."
             wisent-loop (wisent-parse-action tokid (aref actions state)))
       (cond
        
-       ;; Input succesfully parsed
-       ;; ------------------------
+       ;; Input successfully parsed
+       ;; -------------------------
        ((eq wisent-loop wisent-accept-tag)
         (setq wisent-loop nil))
        
@@ -326,21 +374,24 @@ automaton has only one entry point."
        ;; ---------------------
        ((eq wisent-loop wisent-error-tag)
         ;; Report this error if not already recovering from an error.
+        (setq choices (aref actions state))
         (or wisent-recovering
             (wisent-error
-             (format "Parse error - unexpected token %s(%S)%s"
-                     tokid
-                     (cadr wisent-input)
-                     (if (cddr wisent-input)
-                         (format " at %s" (cddr wisent-input))
-                       ""))))
+             (format "Syntax error, unexpected %s, expecting %s"
+                     (wisent-token-to-string wisent-input)
+                     (mapconcat 'wisent-item-to-string
+                                (delq wisent-error-term
+                                      (mapcar 'car (cdr choices)))
+                                ", "))))
         ;; Increment the error counter
         (setq wisent-nerrs (1+ wisent-nerrs))
         ;; If just tried and failed to reuse lookahead token after an
         ;; error, discard it.
         (if (eq wisent-recovering wisent-parse-max-recover)
             (if (eq tokid wisent-eoi-term)
-                (setq wisent-loop nil) ;; Terminate if at end of input.
+                (wisent-abort) ;; Terminate if at end of input.
+              (wisent-message "Error recovery: skip %s"
+                              (wisent-token-to-string wisent-input))
               (run-hook-with-args
                'wisent-discarding-token-functions wisent-input)
               (setq wisent-input (wisent-lexer)))
@@ -352,7 +403,6 @@ automaton has only one entry point."
           (setq wisent-recovering wisent-parse-max-recover)
           ;; Pop the value/state stack to see if an action associated
           ;; to special terminal symbol 'error exists.
-          (setq choices (aref actions state))
           (while (and (>= sp 0)
                       (not (and (setq state   (aref stack sp)
                                       choices (aref actions state)
@@ -363,30 +413,23 @@ automaton has only one entry point."
           (if (not choice)
               ;; No 'error terminal was found.  Just terminate.
               (wisent-abort)
-            
             ;; Try to recover and continue parsing.
             ;; Shift the error terminal.
             (setq state (cdr choice)    ; new state
                   sp    (+ sp 2))
             (aset stack (1- sp) nil)    ; push value
             (aset stack sp state)       ; push new state
-            ;; Try to adjust input to error recovery state.
-            (setq choices (aref actions state))
-            ;; If 'error is followed by an action continue parsing
-            ;; (that is do the action).  Otherwise read until the
-            ;; input token matches the terminal symbol following
-            ;; 'error or EOI is reached.
-            (if (cdr choices)
-                (while
-                    (progn
-                      (or wisent-input
-                          (setq wisent-input (wisent-lexer)
-                                choice (car wisent-input)))
-                      (not (or (eq wisent-eoi-term choice)
-                               (assq choice choices))))
+            ;; Adjust input to error recovery state.  Unless 'error
+            ;; triggers a reduction, eat the input stream until an
+            ;; expected terminal symbol is found, or EOI is reached.
+            (if (cdr (setq choices (aref actions state)))
+                (while (not (or (eq (car wisent-input) wisent-eoi-term)
+                                (assq (car wisent-input) choices)))
+                  (wisent-message "Error recovery: skip %s"
+                                  (wisent-token-to-string wisent-input))
                   (run-hook-with-args
                    'wisent-discarding-token-functions wisent-input)
-                  (setq wisent-input nil))))))
+                  (setq wisent-input (wisent-lexer)))))))
        
        ;; Shift current token on top of the stack
        ;; ---------------------------------------
