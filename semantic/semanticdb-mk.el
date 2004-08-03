85bbf62116a39329e12571abc36a3919829ce16b
zappo <zappo>
2004-08-03 01:38:18 +0000

    (semanticdb-mk-find-eieio): Deleted. ("Old setup code"): Replaced with ("New setup code"): load cedet.el from the usual place for a full seutp. ("Argument parser"): Stop if there are no files found.

--- semantic/semanticdb-mk.el
+++ semantic/semanticdb-mk.el
@@ -1,10 +1,10 @@
 ;;; semanticdb-mk.el --- Command line database builder
 
-;;; Copyright (C) 2002 Eric M. Ludlam
+;;; Copyright (C) 2002, 2004 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: tags
-;; X-RCS: $Id: semanticdb-mk.el,v 1.1 2002/08/11 01:47:36 zappo Exp $
+;; X-RCS: $Id: semanticdb-mk.el,v 1.2 2004/08/03 01:38:18 zappo Exp $
 
 ;; This file is not part of GNU Emacs.
 
@@ -30,39 +30,18 @@
 
 ;;; Code
 ;;
-(defun semanticdb-mk-find-eieio ()
-  "Find EIEIO in the usual places."
-  (cond 
-   ((locate-library "eieio")
-    nil)
-   ((getenv "EIEIO_LOADPATH")
-    (add-to-list 'load-path (getenv "EIEIO_LOADPATH")))
-   (t;; For now, lets hope it was installed in their .emacs file.
-    (let ((de (find-file-noselect "~/.emacs"))
-	  (path nil))
-      (save-excursion
-	(set-buffer de)
-	(goto-char (point-min))
-	(while (not path)
-	  (re-search-forward "eieio")
-	  (if (re-search-backward "\"" nil t)
-	      (progn
-		(mark-sexp 1)
-		(setq path
-		      (buffer-substring-no-properties (point) (mark))))))
-	(add-to-list 'load-path (read path)))))))
-
 (if (not noninteractive)
     (error "You should not load semanticdb-mk interactivly."))
 
-;; Force semantic onto the load path
-(add-to-list 'load-path (file-name-directory load-file-name))
+;; Find our source directory
+(let (fname semanticdir cedetdir loadfile)
 
-;; Add eieio to the path.
-(semanticdb-mk-find-eieio)
+  (setq fname load-file-name)
+  (setq semanticdir (file-name-directory fname))
+  (setq cedetdir (expand-file-name (concat semanticdir "../common/")))
+  (setq loadfile (concat cedetdir "cedet.el"))
 
-;; Initialize semantic
-(require 'semantic-load)
+  (load-file loadfile))
 
 ;; Turn on semanticdb
 (global-semanticdb-minor-mode 1)
@@ -70,19 +49,22 @@
 ;; Process loaded buffers from the command line.
 (let ((args command-line-args))
   ;; Move past this load file being loaded.
-  (while (not
-	  (progn
-	    ;;(message "Compare %s to %s" (car args) load-file-name)
-	    (string= (car args) load-file-name)))
+  (while (and args
+	      (not
+	       (progn
+		 (message "Compare %s to %s" (car args) "-l")
+		 (string= (car args) "-l"))))
     (setq args (cdr args)))
-  (setq args (cdr args))
-  ;; Grab the rest of the file names.
-  ;; For each file, load it in, let semantic evaluate it for tags.
-  (while args
-    (message "Loading %s" (car args))
-    (find-file (car args))
-    (setq args (cdr args)))
-  )
+  (when args
+    (setq args (cdr (cdr args)))
+    ;; Grab the rest of the file names.
+    ;; For each file, load it in, let semantic evaluate it for tags.
+    (while args
+      (message "Loading %s" (car args))
+      (find-file (car args))
+      (semantic-fetch-tags)
+      (setq args (cdr args)))
+    ))
 
 ;; Save the databases.
 (semanticdb-save-all-db)
