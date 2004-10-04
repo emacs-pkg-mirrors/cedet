506fb9993a095e839702d4babde4352815b2d563
ponced <ponced>
2004-10-04 10:10:45 +0000

    (semantic-tag-p): Return nil if an error occurs.

--- semantic/semantic-tag.el
+++ semantic/semantic-tag.el
@@ -2,7 +2,7 @@
 
 ;;; Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004 Eric M. Ludlam
 
-;; X-CVS: $Id: semantic-tag.el,v 1.27 2004/03/25 10:43:06 ponced Exp $
+;; X-CVS: $Id: semantic-tag.el,v 1.28 2004/10/04 10:10:45 ponced Exp $
 
 ;; This file is not part of GNU Emacs.
 
@@ -248,12 +248,15 @@ Return nil if that information can't be obtained."
 ;;
 (defsubst semantic-tag-p (tag)
   "Return non-nil if TAG is most likely a semantic tag."
-  (and (consp tag)
-       (stringp (car tag))                ; NAME
-       (symbolp (nth 1 tag)) (nth 1 tag)  ; TAG-CLASS
-       (listp (nth 2 tag))                ; ATTRIBUTES
-       (listp (nth 3 tag))                ; PROPERTIES
-       ))
+  (condition-case nil
+      (and (consp tag)
+	   (stringp (car tag))                ; NAME
+	   (symbolp (nth 1 tag)) (nth 1 tag)  ; TAG-CLASS
+	   (listp (nth 2 tag))                ; ATTRIBUTES
+	   (listp (nth 3 tag))                ; PROPERTIES
+	   ))
+  ;; If an error occurs, then it most certainly is not a tag.
+  (error nil))
 
 (defsubst semantic-tag-of-class-p (tag class)
   "Return non-nil if class of TAG is CLASS."
