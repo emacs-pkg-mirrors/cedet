d6ef54930c1ca03dcdd5fa290596541ba7517694
zappo <zappo>
2005-01-29 04:34:06 +0000

    (semantic-load-enable-minimum-features): Fix doc

--- semantic/semantic-load.el
+++ semantic/semantic-load.el
@@ -1,9 +1,9 @@
 ;;; semantic-load.el --- Autoload definitions for Semantic
 
-;;; Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004 Eric M. Ludlam
+;;; Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
-;; X-RCS: $Id: semantic-load.el,v 1.42 2004/06/24 01:05:25 zappo Exp $
+;; X-RCS: $Id: semantic-load.el,v 1.43 2005/01/29 04:34:06 zappo Exp $
 
 ;; Semantic is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -61,7 +61,7 @@ other criteria.")
 This includes:
  `semantic-idle-scheduler-mode' - Keeps a buffer's parse tree up to date.
  `semanticdb-minor-mode' - Stores tags when a buffer is not in memory.
- `semantic-load-system-caches' - Loads any systemdbs created earlier."
+ `semanticdb-load-system-caches' - Loads any systemdbs created earlier."
   (interactive)
 
   (global-semantic-idle-scheduler-mode 1)
