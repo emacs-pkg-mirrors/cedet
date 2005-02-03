0dee683d541ec2999e3f0967910b4188ee7bf6e4
zappo <zappo>
2005-02-03 18:57:48 +0000

    Folding decoration/mode.

--- semantic/semantic-fold.el
+++ semantic/semantic-fold.el
@@ -1,5 +1,28 @@
 ;;; semantic-fold.el --- Folding support for Semantic Tags.
 
+;;; Copyright (C) 2005 Eric M. Ludlam
+
+;; Author: Eric M. Ludlam <zappo@gnu.org>
+;; Keywords: syntax
+;; X-RCS: $Id: semantic-fold.el,v 1.2 2005/02/03 18:57:48 zappo Exp $
+
+;; This file is not part of GNU Emacs.
+
+;; Semantic is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This software is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
 ;;; Commentary:
 ;;
 ;; Using semantic-decoration-mode, apply folding icons onto tags.
