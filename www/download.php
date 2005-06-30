a343efc8fc5975c81736b24bbaac1186b62d6ad2
zappo <zappo>
2005-06-30 02:49:57 +0000

    Added pre-release links. Moved betas into a BETA section. Added comments on each new section.

--- www/download.php
+++ www/download.php
@@ -8,14 +8,39 @@ now distributed together in a single file.  This simplifies
 installation and version management.
 
 <p>While Some individual CEDET packages have active stable releases, the
-CEDET bundle is currently only available in Beta.  CEDET Betas are
+CEDET bundle is currently only available in Beta or Pre release.  CEDET Betas are
 the only place to get the latest versions of individual tools.
 
+<H3>PRE RELEASES</H3>
+
+<p>CEDET is currently driving toward a 1.0 release, and small changes are
+going onto each pre-release.  Try out a pre-release and send in bug
+reports on the build process, or anything else.
+
 <p>Try out
+<a href="http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=326500">
+cedet-1.0pre2.tar.gz</a>.
+
+<p>Try out
+<a href="http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=322793">
+cedet-1.0pre1.tar.gz</a>.
+
+<h3>BETAS</h3>
+
+The betas each helped debug new features.  The most recent bundles are
+the pre-releases.
+
+<p>Download
+<a href="http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=254753">
+cedet-1.0beta3b.tar.gz</a>.
+
+<p>Download
+<a href="http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=227377">
+cedet-1.0beta2b.tar.gz</a>.
+
+<p>Download
 <a href="http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=192965">
 cedet-1.0beta1c.tar.gz</a>.
 
-<p>Try out
-<a href="http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=227377">
-cedet-1.0beta2.tar.gz</a>.
+
 
