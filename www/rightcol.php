d8c60e1e81d49a22360490b83f167e9bc6ce8c0d
zappo <zappo>
2005-06-30 01:42:15 +0000

    Removed RPMs list. Add mailing lists.

--- www/rightcol.php
+++ www/rightcol.php
@@ -30,7 +30,10 @@
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href="http://www.sourceforge.net/projects/cedet"><b>Project</b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href="http://sourceforge.net/project/showfiles.php?group_id=17886"><b>Releases</b></a>&nbsp</td></tr>
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href="http://sourceforge.net/mail/?group_id=17886"><b>Mailing Lists</b></a>&nbsp</td></tr>
-<tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href="http://sourceforge.net/projects/rte"><b>RPMs</b></a>&nbsp</td></tr>
+<tr><td >&nbsp&nbsp<img src=tag.png>&nbsp<a class=SB href="http://lists.sourceforge.net/lists/listinfo/cedet-devel"><b><font size=-1>cedet-devel</font></b></a>&nbsp</td></tr>
+<tr><td >&nbsp&nbsp<img src=tag.png>&nbsp<a class=SB href="http://lists.sourceforge.net/lists/listinfo/cedet-semantic"><b><font size=-1>cedet-semantic</font></b></a>&nbsp</td></tr>
+<tr><td >&nbsp&nbsp<img src=tag.png>&nbsp<a class=SB href="http://lists.sourceforge.net/lists/listinfo/cedet-eieio"><b><font size=-1>cedet-eieio</font></b></a>&nbsp</td></tr>
+<!-- tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href="http://sourceforge.net/projects/rte"><b>RPMs</b></a>&nbsp</td></tr -->
 <tr><td >&nbsp<img src=page.gif>&nbsp<a class=SB href="http://sourceforge.net/donate/index.php?group_id=17886"><b>Donate</b></a>&nbsp</td></tr>
 <tr><td >&nbsp</td></tr>
 
