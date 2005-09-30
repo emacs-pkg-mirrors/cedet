1e6311144ad43ff748ea0e42af4b504fa586963e
zappo <zappo>
2005-09-30 20:31:46 +0000

    Update all GPL headers with script from savannah.gnu.org.

--- common/cedet.el
+++ common/cedet.el
@@ -6,7 +6,7 @@
 ;; Maintainer: CEDET developers <http://sf.net/projects/cedet>
 ;; Created: 09 Dec 2002
 ;; Keywords: syntax
-;; X-RCS: $Id: cedet.el,v 1.16 2005/06/30 02:28:59 zappo Exp $
+;; X-RCS: $Id: cedet.el,v 1.17 2005/09/30 20:31:46 zappo Exp $
 
 ;; This file is not part of Emacs
 
@@ -22,8 +22,8 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 ;;
