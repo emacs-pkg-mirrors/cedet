df1b6ef74e15f533a1fdd4d99ee78cd1c87438a2
zappo <zappo>
2006-02-16 01:11:17 +0000

    (HTTPServer.server_bind): Uncomment.

--- semantic/tests/test.py
+++ semantic/tests/test.py
@@ -1,6 +1,6 @@
 # Test file for Python language.
 #
-# $Id: test.py,v 1.2 2002/06/22 10:01:39 emacsman Exp $
+# $Id: test.py,v 1.3 2006/02/16 01:11:17 zappo Exp $
 
 # Simle class compount statement with blank lines sprinkled.
 class Foo(Bar):
@@ -50,13 +50,13 @@ s = "a" "b" "c"
 
 # wisent-python.wy chokes on this! -ryk 6/17/02
 
-#class HTTPServer(xxx.yyy):
-#    allow_reuse_address = 1    # Seems to make sense in testing environment
-#    def server_bind(self):
-#        SocketServer.TCPServer.server_bind(self)
-#        host, port = self.socket.getsockname()
-#        self.server_name = socket.getfqdn(host)
-#        self.server_port = port
+class HTTPServer(xxx.yyy):
+    allow_reuse_address = 1    # Seems to make sense in testing environment
+    def server_bind(self):
+        SocketServer.TCPServer.server_bind(self)
+        host, port = self.socket.getsockname()
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
 
 
 #########################################################################
