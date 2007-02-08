633ec5133cb220c7d85d47b7dd3ccec87e9c1ac4
zappo <zappo>
2007-02-08 16:44:59 +0000

    (compat vars): Do not use compatibility variables in batch mode.

--- semantic/semantic-load.el
+++ semantic/semantic-load.el
@@ -3,7 +3,7 @@
 ;;; Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
-;; X-RCS: $Id: semantic-load.el,v 1.52 2007/01/21 18:11:10 zappo Exp $
+;; X-RCS: $Id: semantic-load.el,v 1.53 2007/02/08 16:44:59 zappo Exp $
 
 ;; Semantic is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -190,15 +190,16 @@ These modes include:
 
   )
 
-;; Old style variables
-(cond
- (semantic-load-turn-everything-on
-  (semantic-load-enable-excessive-code-helpers))
- (semantic-load-turn-useful-things-on
-  (semantic-load-enable-code-helpers))
- (t
-  (semantic-load-enable-minimum-features)
-  ))
+;; Old style variables.  Use only if we are not in batch mode.
+(when (not noninteractive)
+  (cond
+   (semantic-load-turn-everything-on
+    (semantic-load-enable-excessive-code-helpers))
+   (semantic-load-turn-useful-things-on
+    (semantic-load-enable-code-helpers))
+   (t
+    (semantic-load-enable-minimum-features)
+    )))
 
 (provide 'semantic-load)
 
