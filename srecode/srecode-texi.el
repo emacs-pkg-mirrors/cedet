cb9f4caa2eb4e4da2c037bb1898bec4b952bb4b3
zappo <zappo>
2008-01-26 02:44:32 +0000

    (srecode-texi-add-menu): Implement. (srecode-semantic-handle-:texi): Add "top" as a level.

--- srecode/srecode-texi.el
+++ srecode/srecode-texi.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2008 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: srecode-texi.el,v 1.1 2008/01/22 22:53:28 zappo Exp $
+;; X-RCS: $Id: srecode-texi.el,v 1.2 2008/01/26 02:44:32 zappo Exp $
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -27,13 +27,72 @@
 ;; Contains some handlers, and a few simple texinfo srecoder applications.
 
 (require 'semantic)
+(require 'semantic-texi)
 
 ;;; Code:
 (defun srecode-texi-add-menu ()
   "Add an item into the current menu.  Add @node statements as well."
   (interactive)
-  
-  )
+  (let ((currnode (reverse (semantic-find-tag-by-overlay)))
+	(nodebounds nil))
+    (when (not currnode)
+      (error "Cannot find node to put menu item into"))
+    (setq currnode (car currnode))
+    (setq nodebounds (semantic-tag-texi-section-text-bounds currnode))
+    ;; Step 1:
+    ;;   Limit search within this node.
+    ;; Step 2:
+    ;;   Find the menu.  If there isn't one, add one to the end.
+    ;; Step 3:
+    ;;   Add new item to end of menu list.
+    ;; Step 4:
+    ;;   Find correct node new item should show up after, and stick
+    ;;   the new node there.
+    (if (string= (semantic-texi-current-environment) "menu")
+	;; We are already in a menu, so insert the new item right here.
+	(beginning-of-line)
+      ;; Else, try to find a menu item to append to.
+      (goto-char (car nodebounds))
+      (if (not (re-search-forward "^@menu" (car (cdr nodebounds)) t))
+	  (progn
+	    (goto-char (car (cdr nodebounds)))
+	    (if (not (y-or-n-p "Add menu here? "))
+		(error "Abort"))
+	    (srecode-insert "declaration:menu"))
+	;; Else, find the end
+	(re-search-forward "@end menu")
+	(beginning-of-line)))
+    ;; At this point, we are in a menu... or not.
+    ;; If we are, do stuff, else error.
+    (when (string= (semantic-texi-current-environment) "menu")
+      (let ((menuname (read-string "Name of new node: "))
+	    (returnpoint nil))
+	(srecode-insert "declaration:menuitem" "NAME" menuname)
+	(setq returnpoint (make-marker))
+	;; Update the bound since we added text
+	(setq nodebounds (semantic-tag-texi-section-text-bounds currnode))
+	(beginning-of-line)
+	(forward-char -1)
+	(beginning-of-line)
+	(let ((end nil))
+	  (if (not (looking-at "\\* \\([[^:]+\\):"))
+	      (setq end (car (cdr nodebounds)))
+	    (let* ((nname (match-string 1))
+		   (tag
+		    (semantic-find-first-tag-by-name nname (current-buffer))))
+	      (when tag
+		(setq end (semantic-tag-end tag)))
+	      ))
+	  (when (not end)
+	    (goto-char returnpoint)
+	    (error "Could not find location for new node" ))
+	  (when end
+	    (goto-char end)
+	    (insert "\n")
+	    (srecode-insert "declaration:node" "NAME" menuname)
+	    )
+	  )))
+    ))
 
 ;;;###autoload
 (defun srecode-semantic-handle-:texi (dict)
@@ -52,7 +111,8 @@ Adds the following:
 	  (setq level (match-string 1))
 	  )))
     (srecode-dictionary-set-value dict "LEVEL" (or level "chapter"))
-    (let ((nl (assoc level '( ( nil . "chapter" )
+    (let ((nl (assoc level '( ( nil . "top" )
+			      ("top" . "chapter")
 			      ("chapter" . "section")
 			      ("section" . "subsection")
 			      ("subsection" . "subsubsection")
