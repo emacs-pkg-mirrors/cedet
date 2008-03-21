728dd2a04ccb3d258918699af004410e0ccc3e9d
zappo <zappo>
2008-03-21 18:00:45 +0000

    Macro replacement for C/SPP.

---
+++ semantic/tests/testsppreplace.c
@@ -0,0 +1,36 @@
+/* Test CPP/SPP Replacement
+ */
+
+/* TEST: The EMU keyword doesn't screw up the function defn. */
+#define EMU
+#define EMU2 /*comment*/
+char EMU parse_around_emu EMU2 (EMU)
+{
+}
+
+/* TEST: A simple word can be replaced in a definition. */
+#define SUBFLOAT /* Some Float */ float
+SUBFLOAT returnanfloat()
+{
+}
+
+/* TEST: Punctuation an be replaced in a definition. */
+#define COLON :
+int foo COLON COLON bar ()
+{
+}
+
+/* TEST: Multiple lexical characters in a definition */
+#define SUPER mysuper::
+int SUPER baz ()
+{
+}
+
+/* TEST: Macro replacement  -- NOT IMPLEMENTED YET. */
+#if 0
+#define INT_FCN(name) int name (int in)
+
+INT_FCN(increment) {
+  return in+1;
+}
+#endif
