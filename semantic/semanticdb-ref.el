706d537a347e0fe24a173e71819787318a3810bd
zappo <zappo>
2008-03-27 02:53:17 +0000

    (semanticdb-ref-test): Use new data-debug instead of semantic-debug.

--- semantic/semanticdb-ref.el
+++ semantic/semanticdb-ref.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2007, 2008 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: semanticdb-ref.el,v 1.6 2008/03/16 17:13:36 zappo Exp $
+;; X-RCS: $Id: semanticdb-ref.el,v 1.7 2008/03/27 02:53:17 zappo Exp $
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -155,9 +155,9 @@ refreshed before dumping the result."
 				      :i-depend-on myrefs
 				      :local-table tab
 				      :i-include myinc))
-	 (ab (semantic-adebug-new-buffer "*References ADEBUG*"))
+	 (ab (data-debug-new-buffer "*References ADEBUG*"))
 	 )
-    (semantic-adebug-insert-object-fields adbc "!"))
+    (data-debug-insert-object-fields adbc "!"))
   )
 
 (provide 'semanticdb-ref)
