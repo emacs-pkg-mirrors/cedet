2ef156aa2b7f5ad15e30dc0e1a64af6799fed68d
zappo <zappo>
2008-06-17 16:21:56 +0000

    Add todo comments.

--- ede/ede-simple.el
+++ ede/ede-simple.el
@@ -1,9 +1,9 @@
 ;;; ede-simple.el --- Overlay an EDE structure on an existing project
 
-;; Copyright (C) 2007 Eric M. Ludlam
+;; Copyright (C) 2007, 2008 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: ede-simple.el,v 1.4 2007/09/02 14:32:25 zappo Exp $
+;; X-RCS: $Id: ede-simple.el,v 1.5 2008/06/17 16:21:56 zappo Exp $
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -33,6 +33,9 @@
 ;;
 ;; It will also support a the minimal EDE UI for compilation and
 ;; configuration.
+
+;; @todo - Add support for cpp-root as an ede-simple project.
+;; @todo - Allow ede-simple to store locally.
 
 (require 'ede)
 (require 'cedet-files)
