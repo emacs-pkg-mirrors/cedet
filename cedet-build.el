e3b5de2e6b1d06577f70ac7c4d7228bf86fee120
zappo <zappo>
2008-08-23 23:51:08 +0000

    Patch from Lennart Borgman (cedet-build-msg): Print to stdout if noninteractive. (cedet-build): Fix a notice to be consistent

--- cedet-build.el
+++ cedet-build.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2008 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: cedet-build.el,v 1.6 2008/08/22 23:56:36 zappo Exp $
+;; X-RCS: $Id: cedet-build.el,v 1.7 2008/08/23 23:51:08 zappo Exp $
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -80,10 +80,12 @@ This only works if EIEIO does not need to be compiled."
 
 (defun cedet-build-msg (fmt &rest args)
   "Show a build message."
-  (switch-to-buffer "*CEDET BYTECOMPILE*" t)
-  (goto-char (point-max))
-  (insert (apply 'format fmt args))
-  (sit-for 0))
+  (if noninteractive
+      (princ (apply 'format fmt args) t)
+    (switch-to-buffer "*CEDET BYTECOMPILE*" t)
+    (goto-char (point-max))
+    (insert (apply 'format fmt args))
+    (sit-for 0)))
 
 (defun cedet-build (&optional override-check)
   "Build CEDET via EDE.
@@ -99,7 +101,7 @@ OVERRIDE-CHECK to override cedet short-cicuit."
   (delete-other-windows)
   (erase-buffer)
   (cedet-build-msg "CEDET BYTE COMPILATION STATUS:\n\n")
-  (cedet-build-msg "STEP 1: Byte compile EIEIO...")
+  (cedet-build-msg "Step 1: Byte compile EIEIO...")
 
   ;; Get EIEIO built first.
   (save-excursion
