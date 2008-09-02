3ba1663bcb6ff19304c525857b1b4d5bc29c1b23
safanaj <safanaj>
2008-09-02 00:29:10 +0000

    added copyright statement

--- tests/cit-gnustep.el
+++ tests/cit-gnustep.el
@@ -1,4 +1,30 @@
-;;;; Test EDE GNUstep Project
+;;; cit-gnustep.el --- Test EDE GNUstep Project
+
+;; Copyright (C) 2008 "Marco (Bj) Bardelli"
+
+;; Author: Marco (Bj) Bardelli <bardelli.marco@gmail.com>
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; EDE GNUstep for the cedet integration tests.
+
+;;; Code:
+
 ;(require 'ede-pmake "../ede/ede-pmake.el" t)
 (require 'ede-gnustep (concat cit-src-dir "../contrib/ede-gnustep.el") t)
 ;(defvar cit-ede-step-dir (concat cedet-integ-target "/edegnustep"))
