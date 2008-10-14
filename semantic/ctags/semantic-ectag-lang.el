f876d655f834a60a4b1903c2f4dea906152a506e
zappo <zappo>
2008-10-14 23:48:04 +0000

    (c++-mode::semantic-ectag-lang) (c++-mode::semantic-ectag-lang-kind): Added c++ version.

--- semantic/ctags/semantic-ectag-lang.el
+++ semantic/ctags/semantic-ectag-lang.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2008 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: semantic-ectag-lang.el,v 1.2 2008/10/14 12:17:07 zappo Exp $
+;; X-RCS: $Id: semantic-ectag-lang.el,v 1.3 2008/10/14 23:48:04 zappo Exp $
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -36,11 +36,14 @@
 (defvar-mode-local c-mode semantic-ectag-lang "c"
   "Language name for Exuberent CTags.")
 
-(defvar-mode-local c-mode semantic-ectag-lang-kind "cdegmnpsufvti"
+(defvar-mode-local c++-mode semantic-ectag-lang "c++"
+  "Language name for Exuberent CTags.")
+
+(defvar-mode-local c-mode semantic-ectag-lang-kind "cdegmnpsufvt"
   "Kinds of Exuberent CTags available.")
 
 (defvar-mode-local c-mode semantic-ectag-lang-extra-flags
-  '("--regex-c+=/^[ \t]*#[ \t]*include[ \t]*[<\"]([a-zA-Z0-9_.-]+)[>\"]/\\1/i,include/")
+  '("--regex-c=/^[ \t]*#[ \t]*include[ \t]*[<\"]([a-zA-Z0-9_.-]+)[>\"]/\\1/i,include/")
   "Add support for include files.")
 
 (defvar-mode-local c++-mode semantic-ectag-lang-extra-flags
