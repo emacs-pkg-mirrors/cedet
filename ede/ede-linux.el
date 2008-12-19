8fd65b9ce395db08f39185a11fdac9ef651760cb
zappo <zappo>
2008-12-19 22:51:51 +0000

    (ede-linux-project::ede-expand-filename-impl): Renamed from ede-expand-filename.

--- ede/ede-linux.el
+++ ede/ede-linux.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2008 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: ede-linux.el,v 1.2 2008/12/17 03:17:10 zappo Exp $
+;; X-RCS: $Id: ede-linux.el,v 1.3 2008/12/19 22:51:51 zappo Exp $
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -218,7 +218,7 @@ All files need the macros from lisp.h!"
   (let ((F (expand-file-name name (expand-file-name subdir root))))
     (when (file-exists-p F) F)))
 
-(defmethod ede-expand-filename ((proj ede-linux-project) name)
+(defmethod ede-expand-filename-impl ((proj ede-linux-project) name)
   "Within this project PROJ, find the file NAME.
 Knows about how the Linux source tree is organized."
   (let* ((ext (file-name-extension name))
