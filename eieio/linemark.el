baa40f482ada3c68dbb3a3d406d36ce2eb9708d4
zappo <zappo>
2008-12-29 02:21:08 +0000

    (linemark-group::linemark-display) (linemark-group::linemark-delete): Fix byte-comp warnings about mapcar.

--- eieio/linemark.el
+++ eieio/linemark.el
@@ -5,7 +5,7 @@
 ;; Created: Dec 1999
 ;; Keywords: lisp
 ;;
-;; Copyright (C) 1999, 2001, 2002, 2003, 2004, 2005, 2007 Eric M. Ludlam
+;; Copyright (C) 1999, 2001, 2002, 2003, 2004, 2005, 2007, 2008 Eric M. Ludlam
 ;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -277,7 +277,7 @@ Call the new entrie's activate method."
 
 (defmethod linemark-display ((g linemark-group) active-p)
   "Set object G to be active or inactive."
-  (mapcar (lambda (g) (linemark-display g active-p)) (oref g marks))
+  (mapc (lambda (g) (linemark-display g active-p)) (oref g marks))
   (oset g active active-p))
 
 (defmethod linemark-display ((e linemark-entry) active-p)
@@ -318,7 +318,7 @@ Call the new entrie's activate method."
 
 (defmethod linemark-delete ((g linemark-group))
   "Remove group G from linemark tracking."
-  (mapcar 'linemark-delete (oref g marks))
+  (mapc 'linemark-delete (oref g marks))
   (setq linemark-groups (delete g linemark-groups)))
 
 (defmethod linemark-delete ((e linemark-entry))
