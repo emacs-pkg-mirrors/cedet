0d99e79459e45656fb5fb2554aafc225d8bcf8ef
zappo <zappo>
2008-12-29 04:44:43 +0000

    (srecode-map-update-map): Fix bytecomp warnings about copy-list.

--- srecode/srecode-map.el
+++ srecode/srecode-map.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2008 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: srecode-map.el,v 1.11 2008/08/23 23:52:57 zappo Exp $
+;; X-RCS: $Id: srecode-map.el,v 1.12 2008/12/29 04:44:43 zappo Exp $
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -293,14 +293,14 @@ if that file is NEW, otherwise assume the mode has not changed."
   ;;
   (let ((dirty nil))
     ;; 3) - Purge dead files from the file list.
-    (dolist (entry (copy-list (oref srecode-current-map files)))
+    (dolist (entry (copy-sequence (oref srecode-current-map files)))
       (when (not (srecode-map-file-still-valid-p
 		  (car entry) srecode-current-map))
 	(srecode-map-delete-file-entry srecode-current-map (car entry))
 	(setq dirty t)
 	))
-    (dolist (app (copy-list (oref srecode-current-map apps)))
-      (dolist (entry (copy-list (cdr app)))
+    (dolist (app (copy-sequence (oref srecode-current-map apps)))
+      (dolist (entry (copy-sequence (cdr app)))
 	(when (not (srecode-map-file-still-valid-p
 		    (car entry) srecode-current-map))
 	  (srecode-map-delete-file-entry-from-app
