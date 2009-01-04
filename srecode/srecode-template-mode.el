56f5a31a0ec9a2af63eb9398a986a36300dea7d1
zappo <zappo>
2009-01-04 13:15:25 +0000

    (srecode-template-mode-syntax-table): Add : and $ as punctuation. (srecode-template-mode-font-lock-macro-helper) (srecode-template-mode-macro-escape-match) (srecode-macro-help): Use helper for getting escape start/end. (srecode-template-get-mode): Fix typo in name. (srecode-up-context-get-name, srecode-parse-this-macro): New util (semantic-up-context, semantic-beginning-of-context) (semantic-end-of-context, semantic-get-local-variables) (semantic-get-local-arguments, semantic-ctxt-current-symbol) (semantic-analyze-current-context): New overrides for srecode-template-mode (srecode-template-get-escape-start, srecode-template-get-escape-end) (srecode-template-current-context) (srecode-template-find-templates-of-context): New utility functions.

--- srecode/srecode-template-mode.el
+++ srecode/srecode-template-mode.el
@@ -29,7 +29,8 @@
   (let ((table (make-syntax-table (standard-syntax-table))))
     (modify-syntax-entry ?\; ". 12"  table) ;; SEMI, Comment start ;;
     (modify-syntax-entry ?\n ">"     table) ;; Comment end
-    (modify-syntax-entry ?$  "."     table) ;; Comment end
+    (modify-syntax-entry ?$  "."     table) ;; Punctuation
+    (modify-syntax-entry ?:  "."     table) ;; Punctuation
     (modify-syntax-entry ?\" "\""    table) ;; String
     (modify-syntax-entry ?\- "_"     table) ;; Symbol
     (modify-syntax-entry ?\: "_"     table) ;; Symbol
@@ -120,10 +121,8 @@ Don't scan past LIMIT.  Match with EXPRESSION."
   (let* ((done nil)
 	 (md nil)
 	 (tags (semantic-fetch-available-tags))
-	 (est (semantic-find-first-tag-by-name "escape_start" tags))
-	 (eet (semantic-find-first-tag-by-name "escape_end" tags))
-	 (es (regexp-quote (if est (car (semantic-tag-variable-default est)) "{{")))
-	 (ee (regexp-quote (if eet (car (semantic-tag-variable-default eet)) "}}")))
+	 (es (regexp-quote (srecode-template-get-escape-start)))
+	 (ee (regexp-quote (srecode-template-get-escape-end)))
 	 (regex (concat es expression ee))
 	 )
     (while (not done)
@@ -143,10 +142,8 @@ Don't scan past LIMIT."
   (let* ((done nil)
 	 (md nil)
 	 (tags (semantic-fetch-available-tags))
-	 (est (semantic-find-first-tag-by-name "escape_start" tags))
-	 (eet (semantic-find-first-tag-by-name "escape_end" tags))
-	 (es (regexp-quote (if est (car (semantic-tag-variable-default est)) "{{")))
-	 (ee (regexp-quote (if eet (car (semantic-tag-variable-default eet)) "}}")))
+	 (es (regexp-quote (srecode-template-get-escape-start)))
+	 (ee (regexp-quote (srecode-template-get-escape-end)))
 	 (regex (concat "\\(" es "\\|" ee "\\)"))
 	 )
     (while (not done)
@@ -207,12 +204,8 @@ we can tell font lock about them.")
   (let* ((root 'srecode-template-inserter)
 	 (myname (symbol-name root))
 	 (chl (aref (class-v root) class-children))
-	 (es (semantic-find-first-tag-by-name "escape_start" (current-buffer)))
-	 (ee (semantic-find-first-tag-by-name "escape_end" (current-buffer)))
-	 (ess (if es (car (semantic-tag-get-attribute es :default-value))
-		"{{"))
-	 (ees (if ee (car (semantic-tag-get-attribute ee :default-value))
-		"}}"))
+	 (ess (srecode-template-get-escape-start))
+	 (ees (srecode-template-get-escape-end))
 	 )
     (with-output-to-temp-buffer "*SRecode Macros*"
       (princ "Description of known SRecode Template Macros.")
@@ -268,12 +261,348 @@ we can tell font lock about them.")
 	))))
 
 
+;;; Local Context Parsing.
+;;
+(eval-when-compile
+  (require 'semantic-analyze))
+
+(defun srecode-up-context-get-name (&optional point)
+  "Move up one context as for `semantic-up-context', and return the name.
+Moves point to the opening characters of the section macro text.
+If there is no upper context, return nil.
+Starts at POINT if provided."
+  (when point (goto-char (point)))
+  (let* ((tag (semantic-current-tag))
+	 (es (regexp-quote (srecode-template-get-escape-start)))
+	 (start (concat es "[#<]\\(\\w+\\)"))
+	 (orig (point))
+	 (name nil)
+	 (res nil))
+    (when (semantic-tag-of-class-p tag 'function)
+      (while (and (not res)
+		  (re-search-backward start (semantic-tag-start tag) t))
+	(when (save-excursion
+		(setq name (match-string 1))
+		(let ((endr (concat es "/" name)))
+		  (if (re-search-forward endr (semantic-tag-end tag) t)
+		      (< orig (point))
+		    (error "Unmatched Section Template"))))
+	  (setq res (point)))
+	)
+      ;; Restore in no result found.
+      (goto-char (or res orig))
+      name)))
+
+(define-mode-local-override semantic-up-context
+  srecode-template-mode (&optional point)
+  "Move up one context in the current code.
+Moves out one named section."
+  (not (srecode-up-context-get-name point)))
+
+(define-mode-local-override semantic-beginning-of-context
+  srecode-template-mode (&optional point)
+  "Move to the beginning of the current context.
+Moves the the beginning of one named section."
+  (if (semantic-up-context point)
+      t
+    (let ((es (regexp-quote (srecode-template-get-escape-start)))
+	  (ee (regexp-quote (srecode-template-get-escape-end))))
+      (re-search-forward es) ;; move over the start chars.
+      (re-search-forward ee) ;; Move after the end chars.
+      nil)))
+
+(define-mode-local-override semantic-end-of-context
+  srecode-template-mode (&optional point)
+  "Move to the beginning of the current context.
+Moves the the beginning of one named section."
+  (let ((name (srecode-up-context-get-name point))
+	(tag (semantic-current-tag))
+	(es  (regexp-quote (srecode-template-get-escape-start))))
+  (if (not name)
+      t
+    (unless (re-search-forward (concat es "/" name) (semantic-tag-end tag) t)
+      (error "Section %s has no end" name))
+    (goto-char (match-beginning 0))
+    nil)))
+
+(define-mode-local-override semantic-get-local-variables
+  srecode-template-mode (&optional point)
+  "Get local variables from an SRecode template."
+  (save-excursion
+    (when point (goto-char (point)))
+    (let* ((tag (semantic-current-tag))
+	   (name (save-excursion
+		   (srecode-up-context-get-name (point))))
+	   (subdicts (semantic-tag-get-attribute tag :dictionaries))
+	   (global nil)
+	   )
+      (dolist (D subdicts)
+	(setq global (cons (semantic-tag-new-variable (car D) nil)
+			   global)))
+      (if name
+	  ;; Lookup any subdictionaries in TAG.
+	  (let ((res nil))
+	    
+	    (while (and (not res) subdicts)
+	      ;; Find the subdictionary with the same name.  Those variables
+	      ;; are now local to this section.
+	      (when (string= (car (car subdicts)) name)
+		(setq res (cdr (car subdicts))))
+	      (setq subdicts (cdr subdicts)))
+	    ;; Pre-pend our global vars.
+	    (append global res))
+	;; If we aren't in a subsection, just do the global variables
+	global	    
+	))))
+
+(define-mode-local-override semantic-get-local-arguments
+  srecode-template-mode (&optional point)
+  "Get local arguments from an SRecode template."
+  (save-excursion
+    (when point (goto-char (point)))
+    (let* ((tag (semantic-current-tag))
+	   (args (semantic-tag-function-arguments tag))
+	   (argsym (mapcar 'intern args))
+	   (argvars nil)
+	   ;; Create a temporary dictionary in which the
+	   ;; arguments can be resolved so we can extract
+	   ;; the results.
+	   (dict (srecode-create-dictionary t))
+	   )
+      ;; Resolve args into our temp dictionary
+      (srecode-resolve-argument-list argsym dict)
+
+      (maphash 
+       (lambda (key entry)
+	 (setq argvars
+	       (cons (semantic-tag-new-variable key nil entry)
+		     argvars)))
+       (oref dict namehash))
+
+      argvars)))
+
+(define-mode-local-override semantic-ctxt-current-symbol
+  srecode-template-mode (&optional point)
+  "Return the current symbol under POINT.
+Return nil if point is not on/in a template macro."
+  (let ((macro (srecode-parse-this-macro point)))
+    (cdr macro))
+  )
+
+(defun srecode-parse-this-macro (&optional point)
+  "Return the current symbol under POINT.
+Return nil if point is not on/in a template macro.
+The first element is the key for the current macro, such as # for a
+section or ? for an ask variable."
+  (save-excursion
+    (if point (goto-char point))
+    (let ((tag (semantic-current-tag))
+	  (es (regexp-quote (srecode-template-get-escape-start)))
+	  (ee (regexp-quote (srecode-template-get-escape-end)))
+	  (symbolend nil)
+	  (start (point))
+	  (macrostart nil)
+	  (macroend nil)
+	  (raw nil)
+	  )
+      (when (and tag (semantic-tag-of-class-p tag 'function)
+		 (re-search-backward es (semantic-tag-start tag) t))
+	(setq macrostart (match-end 0))
+	(goto-char macrostart)
+	;; We have a match
+	(if (not (re-search-forward ee (semantic-tag-end tag) t))
+	    (setq symbol-end start) ;; Pretend we are ok for completion
+	  (if (> start (point))
+	      ;; If our starting point is after the found point, that
+	      ;; means we are not inside the macro.  Retur nil.
+	      nil
+	    ;; We are inside the macro, extract the text so far.
+	    (let* ((macroend (match-beginning 0))
+		   (raw (buffer-substring-no-properties
+			 macrostart macroend))
+		   (STATE (srecode-compile-state "TMP"))
+		   (inserter (condition-case nil
+				 (srecode-compile-parse-inserter
+				  raw STATE)
+			       (error nil)))
+		   )
+	      (when inserter
+		(let ((base
+		       (cons (oref inserter :object-name)
+			     (if (and (slot-boundp inserter :secondname)
+				      (oref inserter :secondname))
+				 (split-string (oref inserter :secondname)
+					       ":")
+			       nil)))
+		      (key (oref inserter key)))
+		  (cond ((null key)
+			 ;; A plain variable
+			 (cons nil base))
+			(t
+			 ;; A complex variable thingy.
+			 (cons (format "%c" key)
+			       base)))))
+	      )
+	    ))))
+    ))
+
+(define-mode-local-override semantic-analyze-current-context
+  srecode-template-mode (point)
+  "Provide a Semantic analysis in SRecode template mode."
+    (let* ((context-return nil)
+	   (startpoint (point))
+	   (prefixandbounds (semantic-ctxt-current-symbol-and-bounds))
+	   (prefix (car prefixandbounds))
+	   (endsym (nth 1 prefixandbounds))
+	   (bounds (nth 2 prefixandbounds))
+	   (key (car (srecode-parse-this-macro (point))))
+	   (prefix-var nil)
+	   (prefix-context nil)
+	   (prefix-subtemplate nil)
+	   (prefix-function nil)
+	   (prefixclass (semantic-ctxt-current-class-list))
+	   (localvar (semantic-get-all-local-variables point))
+	   (globalvar (semantic-find-tags-by-class 'variable (current-buffer)))
+	   )
+      
+      (when prefix
+	;; First, try to find the variable for the first
+	;; entry in the prefix list.
+	(setq prefix-var (or (semantic-find-first-tag-by-name
+			      (car prefix) localvar)
+			     (semantic-find-first-tag-by-name
+			      (car prefix) globalvar)))
+	
+	(cond
+	 ((and (or (not key) (string= key "?"))
+	       (> (length prefix) 1))
+	  ;; Variables can have lisp function names.
+	  (with-mode-local 'emacs-lisp-mode
+	    (let ((fcns (semanticdb-find-tags-by-name (last prefix))))
+	      (setq prefix-function (semanticdb-find-result-nth fcns 0))))
+	  )
+	 ((or (string= key "<") (string= key ">"))
+	  ;; Includes have second args that is the template name.
+	  (if (= (length prefix) 3)
+	      (let ((contexts (semantic-find-tags-by-class
+			       'context (current-buffer))))
+		(setq prefix-context
+		      (or (semantic-find-first-tag-by-name
+			   (nth 1 prefix) contexts)
+			  ;; Calculate from location
+			  (semantic-tag
+			   (symbol-name
+			    (srecode-template-current-context))
+			   'context))))
+	    (setq prefix-context
+		  ;; Calculate from location
+		  (semantic-tag
+		   (symbol-name (srecode-template-current-context))
+		   'context))
+	    )
+	  ;; The last one?
+	  (when (> (length prefix) 1)
+	    (let ((toc (srecode-template-find-templates-of-context
+			(read (semantic-tag-name prefix-context))))
+		  )
+	      (setq prefix-function 
+		    (or (semantic-find-first-tag-by-name
+			(car (last prefix)) toc)
+			;; Not in this buffer?  Search the master
+			;; templates list.
+			nil))
+	      ))
+	  )
+	 )
+
+	(setq prefixsym
+	      (cond ((= (length prefix) 3)
+		     (list prefix-var prefix-context prefix-function))
+		    ((= (length prefix) 2)
+		     (list prefix-var prefix-function))
+		    ((= (length prefix) 1)
+		     (list prefix-var))))
+
+	(setq context-return
+	      (semantic-analyze-context
+	       "context-for-srecode"
+	       :buffer (current-buffer)
+	       :scope nil
+	       :bounds bounds
+	       :prefix (or prefixsym
+			   prefix)
+	       :prefixtypes nil
+	       :prefixclass prefixclass
+	       :errors nil
+	       ))
+
+	context-return)))
+
+
 ;;; Utils
 ;;
-(defun srecode-tmeplate-get-mode ()
+(defun srecode-template-get-mode ()
   "Get the supported major mode for this template file."
   (let ((m (semantic-find-first-tag-by-name "mode" (current-buffer))))
     (when m (read (semantic-tag-variable-default m)))))
+
+(defun srecode-template-get-escape-start ()
+  "Get the current escape_start characters."
+  (let ((es (semantic-find-first-tag-by-name "escape_start" (current-buffer)))
+	)
+     (if es (car (semantic-tag-get-attribute es :default-value))
+       "{{")))
+
+(defun srecode-template-get-escape-end ()
+  "Get the current escape_end characters."
+  (let ((ee (semantic-find-first-tag-by-name "escape_end" (current-buffer)))
+	)
+    (if ee (car (semantic-tag-get-attribute ee :default-value))
+      "}}")))
+
+(defun srecode-template-current-context (&optional point)
+  "Calculate the context encompassing POINT."
+  (save-excursion
+    (when point (goto-char (point)))
+    (let ((ct (semantic-current-tag)))
+      (when (not ct)
+	(setq ct (semantic-find-tag-by-overlay-prev)))
+
+      ;; Loop till we find the context.
+      (while (and ct (not (semantic-tag-of-class-p ct 'context)))
+	(setq ct (semantic-find-tag-by-overlay-prev
+		  (semantic-tag-start ct))))
+
+      (if ct
+	  (read (semantic-tag-name ct))
+	'declaration))))
+
+(defun srecode-template-find-templates-of-context (context &optional buffer)
+  "Find all the templates belonging to a particular CONTEXT.
+When optional BUFFER is provided, search that buffer."
+  (save-excursion
+    (when buffer (set-buffer buffer))
+    (let ((tags (semantic-fetch-available-tags))
+	  (cc 'declaration)
+	  (scan nil)
+	  (ans nil))
+
+      (when (eq cc context)
+	(setq scan t))
+
+      (dolist (T tags)
+	;; Handle contexts
+	(when (semantic-tag-of-class-p T 'context)
+	  (setq cc (read (semantic-tag-name T)))
+	  (when (eq cc context)
+	    (setq scan t)))
+
+	;; Scan
+	(when (and scan (semantic-tag-of-class-p T 'function))
+	  (setq ans (cons T ans)))
+	)
+
+      (nreverse ans))))
 
 
 ;;; MMM-Mode support ??
