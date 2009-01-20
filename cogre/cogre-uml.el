e068806f1b4eff83ca5951f7cf65e93e91a086e5
zappo <zappo>
2009-01-20 03:41:03 +0000

    (cogre-uml-enable-unicode): Support unicode UML symbols.

--- cogre/cogre-uml.el
+++ cogre/cogre-uml.el
@@ -4,7 +4,7 @@
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: oop, uml
-;; X-RCS: $Id: cogre-uml.el,v 1.12 2009/01/05 23:41:03 zappo Exp $
+;; X-RCS: $Id: cogre-uml.el,v 1.13 2009/01/20 03:41:03 zappo Exp $
 
 ;; This file is not part of GNU Emacs.
 
@@ -145,6 +145,22 @@ The `start' node is the owner of the aggregation, the `end' node is
 the item being aggregated.
 This is supposed to infer that START contains END.")
 
+(defun cogre-uml-enable-unicode ()
+  "Enable use of UNICODE symbols to create COGRE graphs."
+  (interactive)
+  (oset-default cogre-inherit end-glyph [ ("\u25b3") ("\u25bd")
+					  ("\u25c1") ("\u25b7") ])
+  (oset-default cogre-aggrigate start-glyph
+		[ ("\u25c6") ("\u25c6") ("\u25c6") ("\u25c6") ] )
+
+  ;; "\u25c7" - open box like "\u25c6"
+  (setq picture-rectangle-v ?\u2502)
+  (setq picture-rectangle-h ?\u2500)
+  (setq picture-rectangle-ctl ?\u250C)
+  (setq picture-rectangle-ctr ?\u2510)
+  (setq picture-rectangle-cbl ?\u2514)
+  (setq picture-rectangle-cbr ?\u2518)
+  )
 
 (provide 'cogre-uml)
 
