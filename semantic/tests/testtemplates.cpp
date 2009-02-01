50bd3791af07d503e0ee8174eb2397d36bbbb7ce
zappo <zappo>
2009-02-01 16:11:38 +0000

    New test file for dereferencing smart pointers.

---
+++ semantic/tests/testtemplates.cpp
@@ -0,0 +1,70 @@
+// Templates Test file:
+// Written by 'Raf'
+
+template <class T, int U, class V>
+class read_ref {
+public:
+  const T* read_ref_member_one( T);
+  const V* read_ref_member_two();
+};
+
+template <class T, int U, class V>
+class ref {
+public:  
+  read_ref<T,10,V> operator->() {
+    m_// -1-
+      ;
+    // #1# ( "m_datas" )
+  }  
+
+private:
+  T m_datas[U];
+};
+
+class FooOne {
+public:
+  int fooOneMember();
+};
+
+class FooTwo {
+public:
+  int fooTwoMember();
+};
+
+class FooThree {
+public:
+  int fooThreeMember();
+
+  FooOne * operator->();
+};
+
+typedef ref<FooOne, 10,FooTwo> Test;
+
+void
+main(void) {
+  ref<FooOne, 10, FooTwo> v;
+  
+  v->read_ref_member_one()-> // -2-
+    ;
+  // #2# ( "fooOneMember" )
+
+  v->read_ref_member_two()-> // -3-
+    ;
+  // #3# ( "fooTwoMember" )
+
+  v-> // -4-
+    ;
+  // #4# ( "read_ref_member_one" "read_ref_member_two" )
+
+  Test t;
+
+  t->read_ref_member_two()-> // -5-
+    ;
+  // #5# ( "fooTwoMember" )
+
+  ref<FooOne, 10, FooThree> v2;
+  
+  v2->read_ref_member_two()-> // -6- 
+    ;
+  // #6# ( "fooOneMember" )
+}
