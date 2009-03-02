9f0db849091137c61a86334613ef2be264bd55c3
zappo <zappo>
2009-03-02 02:17:36 +0000

    Remove old stuff. Add 1.0pre6.

--- www/download.php
+++ www/download.php
@@ -1,29 +1,23 @@
 <!-- -*-html-*- -->
+<br clear=all>
+<p>
 <table width=100% class=BAR><tr><td>
 <h2>Downloading CEDET</h2>
 </td></tr></table>
 
-<p>CEDET tools including EIEIO, Semantic, Speedbar, EDE, and COGRE are
-now distributed together in a single file.  This simplifies
-installation and version management.
+<p>All the <b>CEDET</b> tools are available from a single distribution file.</p>
 
-<p>While Some individual CEDET packages have active stable releases, the
-CEDET bundle is currently only available in Beta or Pre release.  CEDET Betas are
-the only place to get the latest versions of individual tools.
-
-<H3>PRE RELEASES</H3>
-
-<p>CEDET is currently driving toward a 1.0 release, and small changes are
-going onto each pre-release.  Try out a pre-release and send in bug
-reports on the build process, or anything else.
+<p>CEDET is currently driving toward a 1.0 release.  Try out a
+pre-release and send in bug reports on the build process, or anything
+else to the <a href="http://lists.sourceforge.net/lists/listinfo/cedet-devel">mailing list</a>.
 
 <p>Try out
-<a href="http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=513873">
-cedet-1.0pre4.tar.gz</a>.
+<a href="http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=664893">
+cedet-1.0pre6.tar.gz</a>.
 
-<p>Try out
-<a href="http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=338764">
-cedet-1.0pre3.tar.gz</a>.
+<p>After building CEDET, consider adding your results to the prerelease
+<a href="http://cedet.wiki.sourceforge.net/1.0+pre6+Tested+Configurations">
+tested configuration page</a> by adding your own platform information.
 
 
 
