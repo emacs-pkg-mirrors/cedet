f5ae69a9b905af6fb1018da771fc38ed932d4888
zappo <zappo>
2009-03-02 02:20:56 +0000

    Change default colors. Add fancy new header.

--- www/pagestart.php
+++ www/pagestart.php
@@ -3,12 +3,15 @@
 <style>
 <!--
 ol,ul,p,body,td,tr,th,form {font-family : helvetica,sans-serif; } 
-table.BAR { background-color: lightgreen; border-top: medium solid black; border-bottom: medium solid black}
+
+table.BAR { background-color: #bfbfbf; border-top: medium solid black; border-bottom: medium solid black}
+td.BAR { background-color: #bfbfbf; }
+
 table.SPEEDBAR { background-color: none; border: medium solid black}
-td.BAR { background-color: lightgreen; }
+
 A:visited { color: #0000A0 ; text-decoration: none }
 A:link { color: #0000F0 ; text-decoration: none }
-A:active { color: red ; text-decoration: underline overline }
+A:active { color: #FF0000 ; text-decoration: underline overline }
 A[href]:visited.sb { color: #0000A0 ; text-decoration: none; font-family : courier }
 A[href]:link.SB { text-decoration: none; font-family : courier }
 A[href]:hover.SB { background-color: orange ; text-decoration: none ; font-family : courier}
@@ -16,9 +19,17 @@ A[href]:hover { text-decoration: underline overline }
 -->
 </style>
 
-<table><tr><td width="90%"><img src="cedetlogo.png" ></td>
-<td><img src="writecode.png" align=right></td></tr></table>
-<table width=100% class=BAR><tr><td><font size=-2> &nbsp </font></td></tr></table>
+<!--table><tr><td width="90%"><img src="cedetlogo.png" ></td>
+<td><img src="writecode.png" align=right></td></tr></table-->
+<table cellspacing=0 border=0 width=100%><tr>
+   <td background="img-gen/logo-left.jpg" width="885" height="152">&nbsp;</td>
+   <td background="img-gen/logo-mid.jpg" height="152">&nbsp;</td>
+   <td background="img-gen/logo-right.jpg" width="69" height="152">&nbsp;</td>
+</table>
+
+<table border=0 cellspacing=0><tr>
+<td background="img-gen/box-left.jpg" width="5">&nbsp;</td>
+<td>
 
 <table border=0>
 <tr><td valign=top>
