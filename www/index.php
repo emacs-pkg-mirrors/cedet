13d9ce31699a9a55a581361660f3220b4eb89b8d
zappo <zappo>
2009-03-02 02:19:11 +0000

    Complete re-write:  * Remove all old summary info.  * Add in USERS VIEW section.

--- www/index.php
+++ www/index.php
@@ -5,145 +5,175 @@
   include ("pagestart.php")
 ?>
 
-<P>Welcome to the CEDET homepage.  CEDET is a collection of tools
+<table width=100% class=BAR><tr><td>
+<H3>What is <b>CEDET</b> ?</h3>
+</td></tr></table>
+
+<!-- The below paragraph gets sucked into aggregators.  Include some
+     nice URLs in it to get folks back here. -->
+<p><b>CEDET</b> is a <em><b>C</b>ollection
+   of <b>E</b>macs <b>D</b>evelopment <b>E</b>nvironment <b>T</b>ools</em>
    written with the end goal of creating an advanced development
-   environment in Emacs.  CEDET is hosted at
-   <a href=http://www.sourceforge.net>Source Forge</a>.  You can view
-   CEDET's CVS archive, and project summary
-   <a href=http://www.sourceforge.net/projects/cedet>here.</a>
+   environment in Emacs.  <b>CEDET</b> is hosted at
+   <a href=http://www.sourceforge.net>Source Forge</a> and
+   is <a href="http://www.gnu.org/philosophy/free-sw.html">Free
+   Software</a>.  You can view <b>CEDET</b>'s CVS archive, project
+   summary, and mailing lists at
+   the <a href="http://www.sourceforge.net/projects/cedet"><b>CEDET</b>
+   Project page.</a>
 </p>
+
+<h4>Why is <b>CEDET</b> needed?</h4>
 
 <P>Emacs already is a great environment for writing software, but
    there are additional areas that need improvement.  Many new ideas
    for integrated environments have been developed in newer products,
-   such as Microsoft's Visual environment, JBuilder, Eclipse, or
-   KDevelop.  CEDET is a project which brings together several
-   different tools needed to implement advanced features.
+   such as Microsoft's Visual environment, JBuilder, or Eclipse.
+   CEDET is a project which implements several advanced features
+   developers have come to expect from an Editor.
 </p>
 
-
-<P>Please visit individual project pages for additional information and
-   downloads.
-</p>
 
 <table width=100% class=BAR><tr><td>
-<H3>Articles</h3>
+<H3>CEDET: A User's View</h3>
 </td></tr></table>
 
-<P>An
-   <a href=http://www-106.ibm.com/developerworks/library/j-emacs/?n-j-5241>
-   article</a>
-   about the
-   <a href=http://jde.sunsite.dk/>JDE</a>
-   includes some pointers to CEDET, and mentions some of these tools.</p>
+<p>So what does an Emacs User get out of <b>CEDET</b>?  <b>CEDET</b>
+   includes the following major user features:
 
-<table width=100% class=BAR><tr><td>
-<H3>Base Tools are:</h3>
-</td></tr></table>
+<h4>Projects</h4>
 
-<P><A HREF="eieio.shtml"> <B>EIEIO: Enhanced Implementation of Emacs
-   Interpreted Objects</B></A><br> is a package which implements a
-   <B>CLOS</B> subset for Emacs.  It includes examples which can draw
-   simple tree graphs, and bar charts.
-</p>
+<p><img src="img-gen/project-menu.jpg" align=right> 
+   The CEDET <a href="projects.shtml">Project management system</a>
+   provides a few simple keystrokes for organizing your files,
+   building Makefiles or Automake files, and compiling your sources.
 
-<P><A HREF="semantic.shtml"><B>Semantic</B></A><br> is a
-   Infrastructure for parser based text analysis in Emacs.  It creates
-   parsers in Emacs Lisp.  Includes interfaces to all common tag-like
-   features in Emacs.
-</P>
+<p>If you don't want <b>CEDET</b> to manage your Makefiles, CEDET will
+   still be able to identify some types of projects based on
+   pre-existing build configurations, such as the Emacs sources, the
+   Linux kernel, or any project built using Automake.
 
-<P><A HREF="srecode.shtml"> <B>SRecode: Semantic Recoder</B></A><br>
-  is a template authoring and cataloging system.  Includes base
-  templates for several languages and sample SRecode applications.
-  <em>Currently available from CVS only.</em>
-</p>
+<p>The entire <b>CEDET</b> Makefile tree was built with <b>CEDET</b>'s
+   project management system, so when you download and build the
+   distribution package, you will be using a <b>CEDET</b> project.
 
-<P><B>CEDET/common:</B><br>
-   The CEDET common subpackage contains small utilities recently drawn
-   out of some of the other tools.  Some example tools include:
-   <ul>
-     <li>working - A busy meter.
-     <li>sformat - Souped up format.
-     <li>cedet-autogen - Autoload generators extended for eieio and
-       semantic.
-     <li>inversion - Package level versioning system.
-     <li>ezimage - Simple way to declare and display images in all
-       versions of Emacs.
-     <li>pulse - Fancy overlay color pulsing.
-     <li>pprint - Pretty printer for Emacs Lisp values.
-   </ul>
-</p>
+<p>The image to the right shows a part of the Project management menu.
+<br clear=all>
 
-<table width=100% class=BAR><tr><td>
-<h3>User Interface Tools are:</h3>
-</td></tr></table>
+<h4>Smart Completion</h4>
 
-<P><A HREF="speedbar.shtml"><B>Speedbar</B></A><br>
-   Speedbar is an Everything Browser. It creates special skinny frame
-   to display hierarchical data.  Speedbar supports file/directory
-   trees, Info manuals, EIEIO class browsing, project browsing,
-   context analysis/autocomplete, EIRC groups, xshtml, and VHDL.  It
-   can be rigged to display just about anything.
-</P>
+<p><img src="img-gen/smartcomplete.jpg" border=1 align=right> Write
+   code with <a href="intellisense.shtml">smart code completion</a>,
+   sometimes known as "Intellisense".  The CEDET infrastructure for
+   parsing and tagging files, and analyzing source code is one of the
+   most accurate completion tools for C++ anywhere.  It will correctly
+   handle inheritance scoping rules, templates, smart-pointers, and
+   automatically filters based on what the value will be assigned into.
 
-<P><A href="ede.shtml"> <B>EDE: Emacs Development
-   Environment</b></a><br> EDE is a project management system. It
-   implements projects under Emacs making it easy to maintain programs
-   without learning make.  This depends on EIEIO.</p>
+<p>Code completion is not restricted to just C or C++.  The completion
+   engine is generic and works for any language that has a robust
+   tagging parser written for CEDET, and a thin adaptation layer for
+   the language.
 
-<P><A href="cogre.shtml"> <b>COGRE: COnnected GRaph Editor</b></a><br>
-   COGRE, pronounced like <em>cougar</em>, is an interface to managing connected
-   graphs, such as UML class diagrams.</p>  It can display simple
-   graphs, and UML class diagrams.  It can generate simple class diagrams
-   from sources using Semantic.   
-</p>
+<p>The image to the right shows smart completion configured to use
+   tooltips in a CEDET unit test source file going through a templated
+   smart pointer.
+<br clear=all>
+
+<h4>Symbol References</h4>
+
+<p><img src="img-gen/symref.jpg" border=1 align=right>Analyze
+   where <a href="symref.shtml">symbols are referenced</a> in your
+   code base.  The Symbol References tool in <b>CEDET</b> can use
+   external tools such as GNU Global, ID Utils, CScope, or even
+   find/grep to locate usages of your symbols.  Every location is
+   analyzed and displayed hierarchically showing you the file and
+   function the reference occurs in, instead of a flat list of
+   locations.
+
+<p>The image to the right shows integration with GNU Global running in
+   the GNU Global source code.
+
+<br clear=all>
+
+<h4>Code Generation</h4>
+
+<p><img src="img-gen/srecode-fields.jpg" border=1 align=right>
+   Generate code with a
+   powerful <a href="codegen.shtml">language-independent template
+   system</a>.  The template system in <b>CEDET</b> is a framework
+   designed for code-generating application developers.  Even so, the
+   base system and templates can also be used to insert small
+   repetitive code blocks.  The template language is straight-forward,
+   and flexible.  Because it uses the same code analytics as the Smart
+   completion, it often can correctly guess a wide range of values to
+   be used in your templates, saving you typing.
+
+<p>Some pre-existing tools that use <b>CEDET</b>'s code generation
+   system will write texinfo documentation for you, create doxygen
+   style comments fully filled out from the local context, or insert
+   get/set methods for variables in a class.
+
+<p>The image to the right shows a simple class declaration inserted
+   with the experimental "fields" feature enabled.  Typing in one
+   field updates the others that match the same template dictionary
+   macro.
+
+<br clear=all>
+
+<h4>UML Diagrams</h4>
+
+<p><img src="img-gen/cogre-quick-class.jpg" border=1 align=right>
+   Create UML diagrams either by hand, or automatically generate
+   simple 3-tier class diagrams from your sources.  The diagrams are
+   linked to your source-code, so you can browse quickly through you
+   code from the convenience of UML.
+
+<p>The image to the right shows <b>CEDET</b>'s UML tool's source code,
+   called 'COGRE', diagramming and browsing itself.  Optional unicode
+   character support is enabled for the special characters.
+    
+<br clear=all>
+
+<h4>Install and Configure CEDET</h4>
+
+<p><img src="img-gen/setup.jpg" border=1
+   align=right><a href="setup.shtml">Setting up a tool as large and
+   complex as <b>CEDET</b></a> can seem daunting.  Fortunately, a
+   minimal <b>CEDET</b> initialization can take up only a couple lines
+   of code in your <tt>.emacs</tt> file.  If you are brave and enjoy
+   customizing your <b>CEDET</b> past this, then you are in luck as
+   there are more options and small features to play with than anyone
+   sane person might want.
+
+<P>Alex Ott has written a great article
+   called <a href="http://xtalk.msk.su/~ott/en/writings/emacs-devenv/EmacsCedet.html">
+   A Gentle Introduction to CEDET</a> that shows both the
+   simple <b>CEDET</b> configuration process, and a wide array of
+   customizations that helps tune your Emacs for programming the smart
+   way.
+
+<p>The image to the right shows the CEDET internal package and
+   revision testing output.  Making sure all the package revisions
+   needed are accurate is important, and fully automatic.
+
+<br clear=all>
+
+<h4>Contribute to CEDET</h4>
+
+<p>In many ways, <b>CEDET</b> is a big pile of infrastructure with a
+   thin user interface sitting on it.  Consider helping <b>CEDET</b>
+   by using the infrastructure to build better or more user interface
+   tools.  Join
+   the <a href="http://lists.sourceforge.net/lists/listinfo/cedet-devel">cedet-devel</a>
+   mailing list to learn more.
+
+<br clear=all>
+<p>
 
 <?php
   include ("download.php")
 ?>
-</p>
-
-<table width=100% class=BAR><tr><td>
-<h3>Tools hosted elsewhere:</h3>
-</td></tr></table>
-
-<P><a href=http://ecb.sourceforge.net>
-   <b>Emacs Code Browser (ECB)</b></a>
-   lets you browse your files' contents.
-   Uses the Semantic package.</p>
-
-<table width=100% class=BAR><tr><td>
-<h3>Other Miscellaneous Emacs Hacks at this web site:</h3>
-</td></tr></table>
-
-<P><A HREF="checkdoc.shtml"><B>Checkdoc</B></A> is a program which
-   checks the style of your documentation strings and comments.
-   Useful if you want to keep other Emacs Lisp gurus from picking on
-   you.
-</P>
-
-<P><A HREF="cparse.shtml"><B>C-Parse</B></A> is an <B>Emacs Lisp</B>
-   program which can parse a c file, and allows searching for
-   functions, variables, and types.  CParse is no longer supported.
-   It's tools will be ported to Semantic.
-</P>
-
-<P><A HREF="ftp/X-0.3a.tar.gz"><b>X11 lib calls for Emacs, V
-   0.3a</b></A> Imagine the binary network interface for X windows
-   implemented in Emacs Lisp.  Is it useful?  Silly?  I dunno, but it
-   was fun to play with.  No documentation.  Byte compile it, load
-   "xhello.el" and run the function `XX' for the simple demo.<br>
-</p>
-<P>
-   Newer versions of this X package are in CVS, and can be downloaded
-   from the very nifty XWEM project (Window manager written in Emacs Lisp).
-   You can download that from 
-   <a href="http://www.xwem.org">XWEM home page.</a>
-</p>
-
-<P><A HREF="ftp/hangman.el-0.1.gz"><b>Hangman</b></A> game for
-   Emacs.  About as simple as it gets.
 </p>
 
 <?php
