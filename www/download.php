043f5655fb656ec0c7f47e8e731d94b645a49198
zappo <zappo>
2009-03-05 04:21:37 +0000

    Boldify CEDET.

--- www/download.php
+++ www/download.php
@@ -7,7 +7,7 @@
 
 <p>All the <b>CEDET</b> tools are available from a single distribution file.</p>
 
-<p>CEDET is currently driving toward a 1.0 release.  Try out a
+<p><b>CEDET</b> is currently driving toward a 1.0 release.  Try out a
 pre-release and send in bug reports on the build process, or anything
 else to the <a href="http://lists.sourceforge.net/lists/listinfo/cedet-devel">mailing list</a>.
 
@@ -15,7 +15,7 @@ else to the <a href="http://lists.sourceforge.net/lists/listinfo/cedet-devel">ma
 <a href="http://sourceforge.net/project/showfiles.php?group_id=17886&release_id=664893">
 cedet-1.0pre6.tar.gz</a>.
 
-<p>After building CEDET, consider adding your results to the prerelease
+<p>After building <b>CEDET</b>, consider adding your results to the prerelease
 <a href="http://cedet.wiki.sourceforge.net/1.0+pre6+Tested+Configurations">
 tested configuration page</a> by adding your own platform information.
 
