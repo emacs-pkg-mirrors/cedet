e2f95354462c65a129bfb4164e5ae4f26464b123
zappo <zappo>
2009-03-10 00:35:14 +0000

    (semantic-load-enable-all-exuberent-ctags-support): Moved to here from semantic-ectag-lang.  Calls two new fcns for the two classes of ectag support.

--- semantic/semantic-load.el
+++ semantic/semantic-load.el
@@ -3,7 +3,7 @@
 ;;; Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
-;; X-RCS: $Id: semantic-load.el,v 1.66 2009/02/14 16:25:08 zappo Exp $
+;; X-RCS: $Id: semantic-load.el,v 1.67 2009/03/10 00:35:14 zappo Exp $
 
 ;; Semantic is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -198,6 +198,19 @@ These modes include:
   ;; would be better?
   (setq semantic-edits-verbose-flag t)
 
+  )
+
+;;;###autoload
+(defun semantic-load-enable-all-exuberent-ctags-support ()
+  "Enable all exuberent ctags extensions.
+See the functions:
+   `semantic-load-enable-primary-exuberent-ctags-support'
+   `semantic-load-enable-secondary-exuberent-ctags-support'
+If you just want to add new languages, use
+   `semantic-load-enable-primary-exuberent-ctags-support'."
+  (interactive)
+  (semantic-load-enable-primary-exuberent-ctags-support)
+  (semantic-load-enable-secondary-exuberent-ctags-support)
   )
 
 ;; Old style variables.  Use only if we are not in batch mode.
