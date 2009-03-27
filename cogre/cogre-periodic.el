245212aa0770a28147417bac3d0dfb1a195328d0
zappo <zappo>
2009-03-27 03:54:16 +0000

    (cogre-periodic): Use new utils below. Add an arrow link. (cogre-periodic-make-*-at): New.

--- cogre/cogre-periodic.el
+++ cogre/cogre-periodic.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2009 Eric M. Ludlam
 ;;
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: cogre-periodic.el,v 1.1 2009/03/27 01:40:38 zappo Exp $
+;; X-RCS: $Id: cogre-periodic.el,v 1.2 2009/03/27 03:54:16 zappo Exp $
 ;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -31,6 +31,7 @@
 (require 'cogre-utest)
 (require 'cogre-uml)
 
+;;;###autoload
 (defun cogre-periodic ()
   "Create a periodic table of COGRE objects."
   (interactive)
@@ -39,17 +40,36 @@
   (erase-buffer)
   (cogre "Periodic")
   ;; Put out the base items.
-  (cogre-utest-make-node-at 2 1 'cogre-node "cogre-node")
-  (cogre-utest-make-node-at 40 1 'cogre-node "cogre-node (2)")
+  (let ((n1 (cogre-periodic-make-node-at 2 1 'cogre-node "cogre-node"))
+	(n2 (cogre-periodic-make-node-at 40 1 'cogre-node "cogre-node (2)")))
+    (cogre-periodic-link-at n1 n2 'cogre-link))
+  ;; Put out some UML class diagram elements items.
+  (let ((p1 (cogre-periodic-make-node-at 2 6 'cogre-package "cogre-package"))
+	(c1 (cogre-periodic-make-node-at 25 6 'cogre-class "cogre-class"))
+	(c2 (cogre-periodic-make-node-at 23 15 'cogre-class "cogre-class (2)"))
+	)
+    (cogre-periodic-link-at p1 c1 'cogre-aggregate)
+    (cogre-periodic-link-at c2 c1 'cogre-inherit))
+  ;; Instance Diagram
+  (let ((i1 (cogre-periodic-make-node-at 45 6 'cogre-instance "cogre-instance"))
+	(i2 (cogre-periodic-make-node-at 45 20 'cogre-instance "cogre-instance (2)"))
+	)
+    (cogre-periodic-link-at i1 i2 'cogre-arrow))
+  
   (cogre-render-buffer cogre-graph)
-  (cogre-utest-link-at 2 1 40 1 'cogre-link)
-  ;; Put out some UML items.
-  (cogre-utest-make-node-at 2 6 'cogre-package "cogre-package")
-  (cogre-utest-make-node-at 25 6 'cogre-class "cogre-class")
-  (cogre-utest-make-node-at 23 15 'cogre-class "cogre-class (2)")
-  (cogre-render-buffer cogre-graph)
-  (cogre-utest-link-at 2 6 25 6 'cogre-aggregate)
-  (cogre-utest-link-at 23 15 25 6 'cogre-inherit)
+  )
+
+(defun cogre-periodic-make-node-at (x y type name)
+  "Create a node at X,Y with TYPE and NAME."
+  (picture-goto-coordinate x y)
+  (let ((node (cogre-new-node (point) type)))
+    (cogre-set-element-name node name)
+    node))
+
+(defun cogre-periodic-link-at (node1 node2 type)
+  "Create a link between NODE1 and NODE2.
+Link is created with the specified TYPE."
+  (make-instance type :start node1 :end node2)
   )
 
 
