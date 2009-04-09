3610d0757f51474f743b14245ee1123b8240d085
zappo <zappo>
2009-04-09 01:12:54 +0000

    Support for Semantic Tag based node peers.

---
+++ cogre/cogre-semantic.el
@@ -0,0 +1,138 @@
+;;; cogre-semantic.el --- Convert Semantic TAGS to COGRE nodes.
+;;
+;; Copyright (C) 2009 Eric M. Ludlam
+;;
+;; Author: Eric M. Ludlam <eric@siege-engine.com>
+;; X-RCS: $Id: cogre-semantic.el,v 1.1 2009/04/09 01:12:54 zappo Exp $
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; Define a set of Semantic COGRE element peers based on Semantic objects.
+;;
+;; Convert tags into COGRE nodes, or update existing COGRE nodes
+;; with data from TAGS.
+;;
+;; Used for cut/paste, or updating graphs linked to code from changes
+;; made in the code.
+;;
+;; To convert nodes into tags, see `cogre-convert.el'.
+
+(require 'semantic)
+(require 'cogre)
+
+;;; Code:
+
+;;; PEERS
+;;
+;; The peers can represent a Semantic tag, and keep it up to date.
+
+(defclass cogre-peer-semantic (cogre-element-peer)
+  ((tag :initarg :tag
+	:initform nil
+	:type (or null semantic-tag)
+	:documentation
+	"The Semantic Tag managed by this peer.")
+   )
+  "A peer containing a Semantic class.")
+
+(defclass cogre-peer-semantic-class (cogre-peer-semantic)
+  (
+   )
+  "A peer containing a Semantic class.")
+
+(defmethod cogre-peer-update-from-source ((peer cogre-peer-semantic-class) node)
+  "Update the PEER object, and NODE from environment."
+  (let ((tag (oref peer tag))
+	)
+    ;; Find the original tag and get it.
+
+    ;; Update node based one what we learned.
+    (save-excursion
+      ;; This makes sure that our methods are setup correctly.
+      (semantic-go-to-tag tag)
+      (let ((slots (semantic-tag-type-members tag))
+	    (extmeth (semantic-tag-external-member-children tag t))
+	    attrib method)
+	;; Bin them up
+	(while slots
+	  (cond
+	   ;; A plain string, a simple language, just do attributes.
+	   ((stringp (car slots))
+	    (setq attrib (cons (list (car slots) 'variable nil)
+			       attrib))
+	    )
+	   ;; Variable decl is an attribute
+	   ((eq (semantic-tag-class (car slots)) 'variable)
+	    (setq attrib (cons (semantic-tag-copy (car slots) nil t) attrib)))
+	   ;; A function decle is a method.
+	   ((eq (semantic-tag-class (car slots)) 'function)
+	    (setq method (cons (semantic-tag-copy (car slots) nil t) method)))
+	   )
+	  (setq slots (cdr slots)))
+	;; Add in all those extra methods	
+	(when (semanticdb-find-results-p extmeth)
+	  (setq extmeth (semanticdb-strip-find-results extmeth t)))
+	(while extmeth
+	  (when (eq (semantic-tag-class (car extmeth)) 'function)
+	    (setq method (cons (semantic-tag-copy (car extmeth) nil t) method)))
+	  (setq extmeth (cdr extmeth)))
+	;; Put them into the class.
+	(oset node object-name (semantic-tag-name tag))
+	(oset node attributes (nreverse attrib))
+	(oset node methods (nreverse method))
+	;; Tada!
+	))))
+
+(defmethod cogre-peer-update-from-element ((peer cogre-peer-semantic-class) element)
+  "Update the PEER object, from the ELEMENT data, changing the environment."
+  (message "Cannot update source from graph yet.")
+   nil)
+
+;;; NEW NODE FROM SOURCE
+;;
+;; Handle conversion from a Semantic Tag (source) to a new node.
+;;
+;;;###autoload
+(defun cogre-semantic-tag-to-node (tag)
+  "Convert the Semantic tag TAG into a COGRE node.
+Only handles data types nodes.
+To convert function/variables into methods or attributes in
+an existing COGRE node, see @TODO - do that."
+  (cond
+   ((and tag (semantic-tag-of-class-p tag 'type)
+	 (or (string= (semantic-tag-type tag) "class")
+	     (string= (semantic-tag-type tag) "struct")))
+    ;; A type from a typed language, make the peer and the class,
+    ;; then perform the update.
+    (let* ((peer (cogre-peer-semantic-class
+		  (semantic-tag-name tag)
+		  :tag (semantic-tag-copy tag nil t)))
+	   (node (cogre-class (semantic-tag-name tag)
+			      :peer peer)))
+      (cogre-peer-update-from-source peer node)
+      node))
+
+   ((semantic-tag-of-class-p tag 'node)
+    ;; A node from a dot file.
+    (error "COGRE can't import dot files yet")
+    )
+   (t
+    (error "COGRE can only convert language datatypes into class nodes"))))
+
+(provide 'cogre-semantic)
+;;; cogre-semantic.el ends here
